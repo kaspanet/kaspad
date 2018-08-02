29569a2b9b97d7d73993bce1ef52462f00278c23
Mike Zak <mike@daglabs.com>
2018-08-02 15:32:07 +0300

    [DEV-81] Added tests for handleRollback error-cases

--- database/ffldb/blockio_test.go
+++ database/ffldb/blockio_test.go
@@ -1,9 +1,11 @@
 package ffldb
 
 import (
+	"errors"
 	"os"
 	"testing"
 
+	"github.com/bouk/monkey"
 	"github.com/daglabs/btcd/dagconfig/daghash"
 	"github.com/daglabs/btcd/database"
 	"github.com/daglabs/btcd/wire"
@@ -45,6 +47,69 @@ func TestDeleteFile(t *testing.T) {
 				if _, err := os.Stat(filePath); !os.IsNotExist(err) {
 					t.Errorf("TestDeleteFile: %d: File %s still exists", test.fileNum, filePath)
 				}
+			}
+		}()
+	}
+}
+
+// TestHandleRollbackErrors tests all error-cases in *blockStore.handleRollback().
+// The non-error-cases are tested in the more general tests.
+// Since handleRollback just logs errors, this test simply causes all error-cases to be hit,
+// and makes sure no panic occurs, as well as ensures the writeCursor was updated correctly.
+func TestHandleRollbackErrors(t *testing.T) {
+	testBlock := btcutil.NewBlock(wire.NewMsgBlock(
+		wire.NewBlockHeader(1, []daghash.Hash{}, &daghash.Hash{}, 0, 0)))
+
+	testBlockSize := uint32(testBlock.MsgBlock().SerializeSize())
+	tests := []struct {
+		name        string
+		fileNum     uint32
+		offset      uint32
+		target      interface{}
+		replacement interface{}
+	}{
+		// offset should be size of block + 12 bytes for block network, size and checksum
+		{"Nothing to rollback", 1, testBlockSize + 12, nil, nil},
+		{"deleteFile fails", 0, 0, (*blockStore).deleteFile,
+			func(*blockStore, uint32) error { return errors.New("error in blockstore.deleteFile") }},
+		{"openWriteFile fails", 0, 0, (*blockStore).openWriteFile,
+			func(*blockStore, uint32) (filer, error) { return nil, errors.New("error in blockstore.openWriteFile") }},
+		{"file.Truncate fails", 0, 0, (*os.File).Truncate,
+			func(*os.File, int64) error { return errors.New("error in file.Truncate") }},
+		{"file.Sync fails", 0, 0, (*os.File).Sync,
+			func(*os.File) error { return errors.New("error in file.Sync") }},
+	}
+
+	for _, test := range tests {
+		func() {
+			pdb := newTestDb("TestHandleRollbackErrors", t)
+			defer pdb.Close()
+
+			// Set maxBlockFileSize to testBlockSize so that writeCursor.curFileNum increments
+			pdb.store.maxBlockFileSize = testBlockSize
+
+			err := pdb.Update(func(tx database.Tx) error {
+				return tx.StoreBlock(testBlock)
+			})
+			if err != nil {
+				t.Fatalf("TestHandleRollbackErrors: %s: Error adding test block to dabase: %s", test.name, err)
+			}
+
+			if test.target != nil && test.replacement != nil {
+				patch := monkey.Patch(test.target, test.replacement)
+				defer patch.Unpatch()
+			}
+
+			pdb.store.handleRollback(test.fileNum, test.offset)
+
+			if pdb.store.writeCursor.curFileNum != test.fileNum {
+				t.Errorf("TestHandleRollbackErrors: %s: Expected fileNum: %d, but got: %d",
+					test.name, test.fileNum, pdb.store.writeCursor.curFileNum)
+			}
+
+			if pdb.store.writeCursor.curOffset != test.offset {
+				t.Errorf("TestHandleRollbackErrors: %s: offset fileNum: %d, but got: %d",
+					test.name, test.offset, pdb.store.writeCursor.curOffset)
 			}
 		}()
 	}
