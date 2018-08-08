e12191b3986bfd39b596e8738e8e1ced7d5df0c5
Stas Boutenko <stasb@freshub.com>
2018-08-08 15:39:39 +0300

    [DEV-75] Cleaned up virtualblock.go a bit.

--- blockdag/virtualblock.go
+++ blockdag/virtualblock.go
@@ -27,21 +27,36 @@ func newVirtualBlock(tips blockSet, phantomK uint32) *virtualBlock {
 	return &virtual
 }
 
+// setTips replaces the tips of the virtual block with the blocks in the
+// given blockSet. This only differs from the exported version in that it
+// is up to the caller to ensure the lock is held.
+//
+// This function MUST be called with the view mutex locked (for writes).
 func (v *virtualBlock) setTips(tips blockSet) {
 	v.blockNode = *newBlockNode(nil, tips, v.phantomK)
 }
 
+// SetTips replaces the tips of the virtual block with the blocks in the
+// given blockSet.
+//
+// This function is safe for concurrent access.
 func (v *virtualBlock) SetTips(tips blockSet) {
 	v.mtx.Lock()
 	v.setTips(tips)
 	v.mtx.Unlock()
 }
 
+// addTip adds the given tip to the set of tips in the virtual block.
+// All former tips that happen to be the given tips parents are removed
+// from the set. This only differs from the exported version in that it
+// is up to the caller to ensure the lock is held.
+//
+// This function MUST be called with the view mutex locked (for writes).
 func (v *virtualBlock) addTip(newTip *blockNode) {
 	tips := newSet()
 	tips.add(newTip)
 
-	for tipHash, tip := range v.tips() {
+	for tipHash, tip := range v.Tips() {
 		isParent := false
 		for parentHash := range newTip.parents {
 			if tipHash == parentHash {
@@ -57,14 +72,15 @@ func (v *virtualBlock) addTip(newTip *blockNode) {
 	v.setTips(tips)
 }
 
-func (v *virtualBlock) AddTip(tip *blockNode) {
+// addTip adds the given tip to the set of tips in the virtual block.
+// All former tips that happen to be the given tip's parents are removed
+// from the set.
+//
+// This function is safe for concurrent access.
+func (v *virtualBlock) AddTip(newTip *blockNode) {
 	v.mtx.Lock()
-	v.addTip(tip)
+	v.addTip(newTip)
 	v.mtx.Unlock()
-}
-
-func (v *virtualBlock) tips() blockSet {
-	return v.parents
 }
 
 // Tips returns the current tip block nodes for the DAG.  It will return
@@ -72,12 +88,7 @@ func (v *virtualBlock) tips() blockSet {
 //
 // This function is safe for concurrent access.
 func (v *virtualBlock) Tips() blockSet {
-	v.mtx.Lock()
-	defer func() {
-		v.mtx.Unlock()
-	}()
-
-	return v.tips()
+	return v.parents
 }
 
 // SelectedTip returns the current selected tip for the DAG.
