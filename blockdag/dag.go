59254d27fd3965aac2f205b94290f172baa796cf
Stas Boutenko <stasb@freshub.com>
2018-08-09 17:21:17 +0300

    [DEV-89] Moved fastLog2Floor from dag.go to btcutil/btcmath.go.

--- blockdag/dag.go
+++ blockdag/dag.go
@@ -822,7 +822,7 @@ func (dag *BlockDAG) blockLocator(node *blockNode) BlockLocator {
 		// Requested hash itself + previous 10 entries + genesis block.
 		// Then floor(log2(height-10)) entries for the skip portion.
 		adjustedHeight := uint32(node.height) - 10
-		maxEntries = 12 + fastLog2Floor(adjustedHeight)
+		maxEntries = 12 + btcutil.FastLog2Floor(adjustedHeight)
 	}
 	locator := make(BlockLocator, 0, maxEntries)
 
@@ -853,25 +853,6 @@ func (dag *BlockDAG) blockLocator(node *blockNode) BlockLocator {
 	}
 
 	return locator
-}
-
-// log2FloorMasks defines the masks to use when quickly calculating
-// floor(log2(x)) in a constant log2(32) = 5 steps, where x is a uint32, using
-// shifts.  They are derived from (2^(2^x) - 1) * (2^(2^x)), for x in 4..0.
-var log2FloorMasks = []uint32{0xffff0000, 0xff00, 0xf0, 0xc, 0x2}
-
-// fastLog2Floor calculates and returns floor(log2(x)) in a constant 5 steps.
-func fastLog2Floor(n uint32) uint8 {
-	rv := uint8(0)
-	exponent := uint8(16)
-	for i := 0; i < 5; i++ {
-		if n&log2FloorMasks[i] != 0 {
-			rv += exponent
-			n >>= exponent
-		}
-		exponent >>= 1
-	}
-	return rv
 }
 
 // BlockHeightByHash returns the height of the block with the given hash in the
