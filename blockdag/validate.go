4b78fd007df03d5161958d6adcbbb00ccf8488c8
Ori Newman <orinewman1@gmail.com>
2018-08-14 16:15:12 +0300

    [DEV-84] validate block parents order
    * [DEV-84] validate block parents order
    
    * [DEV-84] change AreEqual to look for order equality as well

--- blockdag/validate.go
+++ blockdag/validate.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math"
 	"math/big"
+	"sort"
 	"time"
 
 	"github.com/daglabs/btcd/dagconfig"
@@ -416,6 +417,11 @@ func checkBlockHeaderSanity(header *wire.BlockHeader, powLimit *big.Int, timeSou
 		return err
 	}
 
+	err = checkBlockParentsOrder(header)
+	if err != nil {
+		return err
+	}
+
 	// A block timestamp must not have a greater precision than one second.
 	// This check is necessary because Go time.Time values support
 	// nanosecond precision whereas the consensus rules only apply to
@@ -436,6 +442,21 @@ func checkBlockHeaderSanity(header *wire.BlockHeader, powLimit *big.Int, timeSou
 		return ruleError(ErrTimeTooNew, str)
 	}
 
+	return nil
+}
+
+//checkBlockParentsOrder ensures that the block's parents are ordered by hash
+func checkBlockParentsOrder(header *wire.BlockHeader) error {
+	sortedHashes := make([]daghash.Hash, 0, len(header.PrevBlocks))
+	for _, hash := range header.PrevBlocks {
+		sortedHashes = append(sortedHashes, hash)
+	}
+	sort.Slice(sortedHashes, func(i, j int) bool {
+		return daghash.Less(&sortedHashes[i], &sortedHashes[j])
+	})
+	if !daghash.AreEqual(header.PrevBlocks, sortedHashes) {
+		return ruleError(ErrWrongParentsOrder, "block parents are not ordered by hash")
+	}
 	return nil
 }
 
