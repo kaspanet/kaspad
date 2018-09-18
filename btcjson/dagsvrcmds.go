327dfb8f8e41b58bf483d0c185e72497f2654ec4
Ori Newman <orinewman1@gmail.com>
2018-09-18 14:51:58 +0300

    [DEV-130] convert searchrawtransaction ints to bools

--- btcjson/dagsvrcmds.go
+++ btcjson/dagsvrcmds.go
@@ -609,10 +609,10 @@ func NewReconsiderBlockCmd(blockHash string) *ReconsiderBlockCmd {
 // SearchRawTransactionsCmd defines the searchrawtransactions JSON-RPC command.
 type SearchRawTransactionsCmd struct {
 	Address     string
-	Verbose     *int  `jsonrpcdefault:"1"`
+	Verbose     *bool `jsonrpcdefault:"true"`
 	Skip        *int  `jsonrpcdefault:"0"`
 	Count       *int  `jsonrpcdefault:"100"`
-	VinExtra    *int  `jsonrpcdefault:"0"`
+	VinExtra    *bool `jsonrpcdefault:"false"`
 	Reverse     *bool `jsonrpcdefault:"false"`
 	FilterAddrs *[]string
 }
@@ -622,7 +622,7 @@ type SearchRawTransactionsCmd struct {
 //
 // The parameters which are pointers indicate they are optional.  Passing nil
 // for optional parameters will use the default value.
-func NewSearchRawTransactionsCmd(address string, verbose, skip, count *int, vinExtra *int, reverse *bool, filterAddrs *[]string) *SearchRawTransactionsCmd {
+func NewSearchRawTransactionsCmd(address string, verbose *bool, skip, count *int, vinExtra, reverse *bool, filterAddrs *[]string) *SearchRawTransactionsCmd {
 	return &SearchRawTransactionsCmd{
 		Address:     address,
 		Verbose:     verbose,
