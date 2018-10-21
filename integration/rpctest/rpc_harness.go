db3c41d5c2a6bea9204b3d8b7aba699669cac5e0
Stas Boutenko <stasb@freshub.com>
2018-10-21 17:23:54 +0300

    [DEV-134] Ran gofmt on everything.

--- integration/rpctest/rpc_harness.go
+++ integration/rpctest/rpc_harness.go
@@ -18,8 +18,8 @@ import (
 	"github.com/daglabs/btcd/dagconfig"
 	"github.com/daglabs/btcd/dagconfig/daghash"
 	"github.com/daglabs/btcd/rpcclient"
-	"github.com/daglabs/btcd/wire"
 	"github.com/daglabs/btcd/util"
+	"github.com/daglabs/btcd/wire"
 )
 
 const (
