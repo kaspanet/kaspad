d38794eb880f5a72629fd61eb28115c274601f62
Ori Newman <orinewman1@gmail.com>
2018-11-07 17:22:07 +0200

    [DEV-267] add comment to MsgSyncMe and its methods

--- wire/msgsyncme.go
+++ wire/msgsyncme.go
@@ -9,11 +9,16 @@ import (
 	"github.com/daglabs/btcd/dagconfig/daghash"
 )
 
+// MsgSyncMe implements the Message interface and represents a bitcoin version
+// message.  It is used in order to initialize a sync session from a peer with
+// unknown tip hashes.
 type MsgSyncMe struct {
 	ProtocolVersion     uint32
 	PartialSelectedPath []*daghash.Hash
 }
 
+// BtcDecode decodes r using the bitcoin protocol encoding into the receiver.
+// This is part of the Message interface implementation.
 func (msg *MsgSyncMe) BtcDecode(r io.Reader, pver uint32) error {
 	buf, ok := r.(*bytes.Buffer)
 	if !ok {
@@ -42,6 +47,8 @@ func (msg *MsgSyncMe) BtcDecode(r io.Reader, pver uint32) error {
 	return nil
 }
 
+// BtcEncode encodes the receiver to w using the bitcoin protocol encoding.
+// This is part of the Message interface implementation.
 func (msg *MsgSyncMe) BtcEncode(w io.Writer, pver uint32) error {
 	err := writeElement(w, msg.ProtocolVersion)
 	if err != nil {
@@ -57,17 +64,20 @@ func (msg *MsgSyncMe) BtcEncode(w io.Writer, pver uint32) error {
 
 // Command returns the protocol command string for the message.  This is part
 // of the Message interface implementation.
-func (msg *MsgVersion) Command() string {
+func (msg *MsgSyncMe) Command() string {
 	return CmdSyncMe
 }
 
 // MaxPayloadLength returns the maximum length the payload can be for the
 // receiver.  This is part of the Message interface implementation.
-func (msg *MsgVersion) MaxPayloadLength(pver uint32) uint32 {
+func (msg *MsgSyncMe) MaxPayloadLength(pver uint32) uint32 {
 	// Protocol version 4 bytes + number of hashes 2 bytes + max uint16 * daghash.HashSize
 	return 6 + math.MaxUint16*daghash.HashSize
 }
 
+// NewMsgSyncMe returns a new bitcoin version message that conforms to the
+// Message interface using the passed parameters and defaults for the remaining
+// fields.
 func NewMsgSyncMe(PartialSelectedPath []*daghash.Hash) *MsgSyncMe {
 	return &MsgSyncMe{
 		ProtocolVersion:     ProtocolVersion,
