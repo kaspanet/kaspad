1b7b010a088b1aacbd3aec743abbe26650b33367
Ori Newman <orinewman1@gmail.com>
2019-03-10 17:08:36 +0200

    [NOD-36] Convert util.MaxSatoshi to integer

--- util/const.go
+++ util/const.go
@@ -6,11 +6,11 @@ package util
 
 const (
 	// SatoshiPerBitcent is the number of satoshi in one bitcoin cent.
-	SatoshiPerBitcent = 1e6
+	SatoshiPerBitcent = 1000000
 
 	// SatoshiPerBitcoin is the number of satoshi in one bitcoin (1 BTC).
-	SatoshiPerBitcoin = 1e8
+	SatoshiPerBitcoin = 100000000
 
 	// MaxSatoshi is the maximum transaction amount allowed in satoshi.
-	MaxSatoshi = 21e6 * SatoshiPerBitcoin
+	MaxSatoshi = 21000000 * SatoshiPerBitcoin
 )
