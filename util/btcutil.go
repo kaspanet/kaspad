e2bc7fb7a11f5c20adc2e75819141797fea654b5
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-06-16 13:27:14 +0300

    [NOD-208] Fixed comment in SearchSlice.

--- util/btcutil.go
+++ util/btcutil.go
@@ -4,11 +4,11 @@ import "sort"
 
 // SearchSlice uses binary search to find and return the smallest index i
 // in [0, n) at which f(i) is true, assuming that on the range [0, n),
-// f(i) == true implies f(i+1) == true. That is, Search requires that
+// f(i) == true implies f(i+1) == true. That is, SearchSlice requires that
 // f is false for some (possibly empty) prefix of the input range [0, n)
-// and then true for the (possibly empty) remainder; Search returns
+// and then true for the (possibly empty) remainder; SearchSlice returns
 // the first true index.
-// Search calls f(i) only for i in the range [0, n).
+// SearchSlice calls f(i) only for i in the range [0, n).
 func SearchSlice(sliceLength int, searchFunc func(int) bool) (foundIndex int, ok bool) {
 	result := sort.Search(sliceLength, searchFunc)
 	if result == sliceLength {
