a7f6ed2362be388b53bf762f04dac6854ac323f8
Ori Newman <orinewman1@gmail.com>
2019-07-02 16:05:38 +0300

    [NOD-225] Refactor updateFinalityPoint

--- blockdag/dag.go
+++ blockdag/dag.go
@@ -722,29 +722,25 @@ func (dag *BlockDAG) checkFinalityRules(newNode *blockNode) error {
 // updateFinalityPoint updates the dag's last finality point if necessary.
 func (dag *BlockDAG) updateFinalityPoint() {
 	selectedTip := dag.selectedTip()
-	var newFinalityPoint *blockNode
 	// if the selected tip is the genesis block - it should be the new finality point
 	if selectedTip.isGenesis() {
-		newFinalityPoint = selectedTip
-		dag.lastFinalityPoint = newFinalityPoint
+		dag.lastFinalityPoint = selectedTip
 		return
-	} else {
-		// We are looking for a new finality point only if the new block's finality score is higher
-		// by 2 than the existing finality point's
-		if selectedTip.finalityScore() < dag.lastFinalityPoint.finalityScore()+2 {
-			return
-		}
-
-		var currentNode *blockNode
-		for currentNode = selectedTip.selectedParent; ; currentNode = currentNode.selectedParent {
-			// We look for the first node in the selected parent chain that has a higher finality score than the last finality point.
-			if currentNode.selectedParent.finalityScore() == dag.lastFinalityPoint.finalityScore() {
-				break
-			}
-		}
-		newFinalityPoint = currentNode
 	}
-	dag.lastFinalityPoint = newFinalityPoint
+	// We are looking for a new finality point only if the new block's finality score is higher
+	// by 2 than the existing finality point's
+	if selectedTip.finalityScore() < dag.lastFinalityPoint.finalityScore()+2 {
+		return
+	}
+
+	var currentNode *blockNode
+	for currentNode = selectedTip.selectedParent; ; currentNode = currentNode.selectedParent {
+		// We look for the first node in the selected parent chain that has a higher finality score than the last finality point.
+		if currentNode.selectedParent.finalityScore() == dag.lastFinalityPoint.finalityScore() {
+			break
+		}
+	}
+	dag.lastFinalityPoint = currentNode
 	spawn(func() {
 		dag.finalizeNodesBelowFinalityPoint(true)
 	})
