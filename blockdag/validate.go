38e73c3eb09ea28a1869b693ac4bb710568f0b53
Ori Newman <orinewman1@gmail.com>
2019-07-02 11:44:03 +0300

    [NOD-225] change comment in validateParents

--- blockdag/validate.go
+++ blockdag/validate.go
@@ -610,13 +610,9 @@ func validateParents(blockHeader *wire.BlockHeader, parents blockSet) error {
 	queue := newDownHeap()
 	visited := newSet()
 	for _, parent := range parents {
-		// Once parent.isFinalized is set to true, the value won't change again, but
-		// because the isFinalized field can be set to true in finalizeNodesBelowFinalityPoint
-		// in another goroutine, there's no guarantee that the parent won't be marked as
-		// finalized later on. That is to say, if isFinalized is true, we're 100% sure it's
-		// true, but if it's false, we can't be sure that it's not actually finalized.
-		// This is why later the block is checked more thoroughly on the finality rules
-		// in dag.checkFinalityRules.
+		// isFinalized might be false-negative because node finality status is
+		// updated in a separate goroutine. This is why later the block is
+		// checked more thoroughly on the finality rules in dag.checkFinalityRules.
 		if parent.isFinalized {
 			return ruleError(ErrFinality, fmt.Sprintf("block %s is a finalized parent of block %s", parent.hash, blockHeader.BlockHash()))
 		}
