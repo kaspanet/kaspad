501cf7b202c5cbcb5c17ba541189110e62eb9cc6
Ori Newman <orinewman1@gmail.com>
2019-08-22 17:38:04 +0300

    [NOD-285] Add accepting block to transactions and blocks models, and remove accepting block model

--- apiserver/models/models.go
+++ apiserver/models/models.go
@@ -5,84 +5,79 @@ import (
 	"time"
 )
 
-type Block struct{
+type Block struct {
 	gorm.Model
-	ID uint
-	BlockHash string
-	Version int32
-	HashMerkleRoot string
-	AcceptedIDMerkleRoot	string
-	UTXOCommitment	string
-	Timestamp	time.Time
-	Bits	uint32
-	Nonce	uint64
-	BlueScore	uint64
-	IsChainBlock	bool
+	ID                   uint
+	BlockHash            string
+	AcceptingBlockID     uint64
+	Version              int32
+	HashMerkleRoot       string
+	AcceptedIDMerkleRoot string
+	UTXOCommitment       string
+	Timestamp            time.Time
+	Bits                 uint32
+	Nonce                uint64
+	BlueScore            uint64
+	IsChainBlock         bool
 }
 
-type ParentBlock struct{
+type ParentBlock struct {
 	gorm.Model
-	BlockID uint64
+	BlockID       uint64
 	ParentBlockID uint64
 }
 
-type AcceptingBlock struct{
+type RawBlock struct {
 	gorm.Model
-	BlockID uint64
-	ParentBlockID uint64
-}
-
-type RawBlock struct{
-	gorm.Model
-	BlockID uint64
+	BlockID   uint64
 	BlockData []byte
 }
 
-type Subnetwork struct{
+type Subnetwork struct {
 	gorm.Model
-	ID uint64
+	ID           uint64
 	SubnetworkID []byte
 }
 
 type Transactions struct {
 	gorm.Model
-	ID              uint64
-	BlockID         uint64
-	TransactionHash string
-	TransactionID   string
-	LockTime   uint64
-	SubnetworkID   uint64
-	Gas   uint64
-	PayloadHash   string
-	Payload   []byte
-}
-
-type TransactionsToBlocks struct{
-	gorm.Model
-	TransactionID uint64
-	BlockID uint64
-	LocationInBlock uint32
-}
-
-type TransactionOutputs struct{
-	gorm.Model
-	TransactionID uint64
-	Index uint32
-	Value uint64
-	PkScript []byte
-}
-
-type TransactionInputs struct{
-	gorm.Model
-	TransactionID uint64
-	TransactionOutputID uint64
-	Index uint32
-	SignatureScript []byte
-	Sequence uint64
-}
-
-type UTXO struct{
-	gorm.Model
-	TransactionOutputID uint64
+	ID               uint64
 	AcceptingBlockID uint64
+	TransactionHash  string
+	TransactionID    string
+	LockTime         uint64
+	SubnetworkID     uint64
+	Gas              uint64
+	PayloadHash      string
+	Payload          []byte
+}
+
+type TransactionsToBlocks struct {
+	gorm.Model
+	TransactionID uint64
+	BlockID       uint64
+	Index         uint32
+}
+
+type TransactionOutputs struct {
+	gorm.Model
+	TransactionID uint64
+	Index         uint32
+	Value         uint64
+	PkScript      []byte
+}
+
+type TransactionInputs struct {
+	gorm.Model
+	TransactionID       uint64
+	TransactionOutputID uint64
+	Index               uint32
+	SignatureScript     []byte
+	Sequence            uint64
+}
+
+type UTXO struct {
+	gorm.Model
+	TransactionOutputID uint64
+	AcceptingBlockID    uint64
 }
