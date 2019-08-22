2c2af3584603109ac2792ba4e112c45710f4dbb5
Ori Newman <orinewman1@gmail.com>
2019-08-22 17:15:17 +0300

    [NOD-280] Rename location_in_block to index

--- apiserver/db/migrations/000006_create_transactions_to_blocks_table.up.sql
+++ apiserver/db/migrations/000006_create_transactions_to_blocks_table.up.sql
@@ -2,9 +2,9 @@ CREATE TABLE `transactions_to_blocks`
 (
     `transaction_id`    BIGINT UNSIGNED NOT NULL,
     `block_id`          BIGINT UNSIGNED NOT NULL,
-    `location_in_block` INT UNSIGNED    NOT NULL,
+    `index` INT UNSIGNED    NOT NULL,
     PRIMARY KEY (`transaction_id`, `block_id`),
-    INDEX `idx_transactions_to_blocks_location_in_block` (`location_in_block`),
+    INDEX `idx_transactions_to_blocks_index` (`index`),
     CONSTRAINT `fk_transactions_to_blocks_block_id`
         FOREIGN KEY (`block_id`)
             REFERENCES `blocks` (`id`),
