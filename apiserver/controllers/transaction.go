90357acfbd5fb98e213644db42dfe45bd20caa3f
Ori Newman <orinewman1@gmail.com>
2019-09-05 16:46:43 +0300

    [NOD-304] change db var to query

--- apiserver/controllers/transaction.go
+++ apiserver/controllers/transaction.go
@@ -18,8 +18,8 @@ func GetTransactionByIDHandler(txID string) (interface{}, *utils.HandlerError) {
 		return nil, utils.NewHandlerError(http.StatusUnprocessableEntity, fmt.Sprintf("The given txid is not a hex-encoded %d-byte hash.", daghash.TxIDSize))
 	}
 	tx := &models.Transaction{}
-	db := database.DB.Where(&models.Transaction{TransactionID: txID})
-	addTxPreloadedFields(db).First(&tx)
+	query := database.DB.Where(&models.Transaction{TransactionID: txID})
+	addTxPreloadedFields(query).First(&tx)
 	if tx.ID == 0 {
 		return nil, utils.NewHandlerError(http.StatusNotFound, "No transaction with the given txid was found.")
 	}
@@ -32,9 +32,9 @@ func GetTransactionByHashHandler(txHash string) (interface{}, *utils.HandlerErro
 		return nil, utils.NewHandlerError(http.StatusUnprocessableEntity, fmt.Sprintf("The given txhash is not a hex-encoded %d-byte hash.", daghash.HashSize))
 	}
 	tx := &models.Transaction{}
-	db := database.DB.
+	query := database.DB.
 		Where(&models.Transaction{TransactionHash: txHash})
-	addTxPreloadedFields(db).First(&tx)
+	addTxPreloadedFields(query).First(&tx)
 	if tx.ID == 0 {
 		return nil, utils.NewHandlerError(http.StatusNotFound, "No transaction with the given txhash was found.")
 	}
@@ -48,7 +48,7 @@ func GetTransactionsByAddressHandler(address string, skip uint64, limit uint64) 
 		return nil, utils.NewHandlerError(http.StatusUnprocessableEntity, fmt.Sprintf("The maximum allowed value for the limit is %d", maximumGetTransactionsLimit))
 	}
 	txs := []*models.Transaction{}
-	db := database.DB.
+	query := database.DB.
 		Joins("LEFT JOIN `transaction_outputs` ON `transaction_outputs`.`transaction_id` = `transactions`.`id`").
 		Joins("LEFT JOIN `addresses` AS `out_addresses` ON `out_addresses`.`id` = `transaction_outputs`.`address_id`").
 		Joins("LEFT JOIN `transaction_inputs` ON `transaction_inputs`.`transaction_id` = `transactions`.`id`").
@@ -58,7 +58,7 @@ func GetTransactionsByAddressHandler(address string, skip uint64, limit uint64) 
 		Or("`in_addresses`.`address` = ?", address).
 		Limit(limit).
 		Offset(skip)
-	addTxPreloadedFields(db).Find(&txs)
+	addTxPreloadedFields(query).Find(&txs)
 	txResponses := make([]*transactionResponse, len(txs))
 	for i, tx := range txs {
 		txResponses[i] = convertTxModelToTxResponse(tx)
@@ -66,8 +66,8 @@ func GetTransactionsByAddressHandler(address string, skip uint64, limit uint64) 
 	return txResponses, nil
 }
 
-func addTxPreloadedFields(db *gorm.DB) *gorm.DB {
-	return db.Preload("AcceptingBlock").
+func addTxPreloadedFields(query *gorm.DB) *gorm.DB {
+	return query.Preload("AcceptingBlock").
 		Preload("Subnetwork").
 		Preload("TransactionOutputs").
 		Preload("TransactionOutputs.Address").
