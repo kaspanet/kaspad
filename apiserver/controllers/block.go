377dde833d6023986804c29fc3702425c9dc58e1
Ori Newman <orinewman1@gmail.com>
2019-09-15 15:19:36 +0300

    [NOD-325] Enable separate error messages for logging and client

--- apiserver/controllers/block.go
+++ apiserver/controllers/block.go
@@ -20,7 +20,7 @@ func GetBlockByHashHandler(blockHash string) (interface{}, *utils.HandlerError) 
 
 	db, err := database.DB()
 	if err != nil {
-		return nil, utils.NewHandlerError(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return nil, utils.NewInternalServerHandlerError(err.Error())
 	}
 
 	block := &models.Block{}
