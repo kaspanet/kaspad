f4850b9e7a9d23878d7fc68178e8c8338660139c
Ori Newman <orinewman1@gmail.com>
2019-09-18 11:47:54 +0300

    [NOD-330] Use BTCD logs for gorm (#410)

---
+++ apiserver/database/log.go
@@ -0,0 +1,9 @@
+package database
+
+import "github.com/daglabs/btcd/util/panics"
+import "github.com/daglabs/btcd/apiserver/logger"
+
+var (
+	log   = logger.BackendLog.Logger("DTBS")
+	spawn = panics.GoroutineWrapperFunc(log, logger.BackendLog)
+)
