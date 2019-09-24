3126b5252fe137f7b7b243c1325f03a2d481c4f3
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-09-24 10:48:42 +0300

    [NOD-337] Added a comment for MaxMassPerTx.

--- wire/msgblock.go
+++ wire/msgblock.go
@@ -24,6 +24,7 @@ const defaultTransactionAlloc = 2048
 // MaxMassPerBlock is the maximum total transaction mass a block may contain.
 const MaxMassPerBlock = 10000000
 
+// MaxMassPerTx is the maximum total mass a transaction may contain.
 const MaxMassPerTx = MaxMassPerBlock / 2
 
 // maxTxPerBlock is the maximum number of transactions that could
