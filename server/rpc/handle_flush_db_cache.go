40ad9c5d2b0c3ada2b1e51e08559876c28387733
Dan Aharoni <dan@daglabs.com>
2019-10-07 14:07:20 +0300

    [NOD-355] remove 'flushDbCache' and 'getBlockHash' rpc commands (#425)
    * [NOD-355] remove 'flushDbCache' and 'getBlockHash' rpc commands
    
    * [NOD-355] remove 'flushDbCache' and 'getBlockHash' from rpc server help

--- server/rpc/handle_flush_db_cache.go
+++
@@ -1,8 +0,0 @@
-package rpc
-
-// handleFlushDBCache flushes the db cache to the disk.
-// TODO: (Ori) This is a temporary function for dev use. It needs to be removed.
-func handleFlushDBCache(s *Server, cmd interface{}, closeChan <-chan struct{}) (interface{}, error) {
-	err := s.cfg.DB.FlushCache()
-	return nil, err
-}
