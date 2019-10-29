6da5d45911e4fb080e3cdf32c980f9b4b2a52d35
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-10-29 14:07:43 +0200

    [NOD-376] Add a test to make sure that bad child blocks don't invalidate valid parent blocks.

--- blockdag/process_test.go
+++ blockdag/process_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/daglabs/btcd/dagconfig"
 	"github.com/daglabs/btcd/util"
+	"github.com/daglabs/btcd/util/daghash"
+	"path/filepath"
 	"testing"
 	"time"
 )
@@ -63,5 +65,66 @@ func TestProcessBlock(t *testing.T) {
 	expectedErrMsg := fmt.Sprintf("already have block %s", dagconfig.SimNetParams.GenesisHash)
 	if err == nil || err.Error() != expectedErrMsg {
 		t.Errorf("ProcessBlock: Expected error \"%s\" but got \"%s\"", expectedErrMsg, err)
+	}
+}
+
+func TestProcessOrphans(t *testing.T) {
+	dag, teardownFunc, err := DAGSetup("TestProcessOrphans", Config{
+		DAGParams: &dagconfig.SimNetParams,
+	})
+	if err != nil {
+		t.Errorf("Failed to setup dag instance: %v", err)
+		return
+	}
+	defer teardownFunc()
+
+	dag.TestSetCoinbaseMaturity(0)
+
+	blocksFile := "blk_0_to_4.dat"
+	blocks, err := LoadBlocks(filepath.Join("testdata/", blocksFile))
+	if err != nil {
+		t.Fatalf("TestProcessOrphans: "+
+			"Error loading file '%s': %s\n", blocksFile, err)
+	}
+
+	// Get a reference to a parent block
+	parentBlock := blocks[1]
+
+	// Get a reference to a child block and mess with it to make sure it gets rejected
+	childBlock := blocks[2]
+	childBlock.MsgBlock().Header.Nonce = 3
+	childBlock.MsgBlock().Header.UTXOCommitment = &daghash.ZeroHash
+
+	// Process the child block so that it gets added to the orphan pool
+	isOrphan, delay, err := dag.ProcessBlock(childBlock, BFNone)
+	if err != nil {
+		t.Fatalf("TestProcessOrphans: child block unexpectedly returned an error: %s", err)
+	}
+	if delay != 0 {
+		t.Fatalf("TestProcessOrphans: child block is too far in the future")
+	}
+	if !isOrphan {
+		t.Fatalf("TestProcessOrphans: incorrectly returned that child block is not an orphan")
+	}
+
+	// Process the parent block
+	isOrphan, delay, err = dag.ProcessBlock(parentBlock, BFNone)
+	if err != nil {
+		t.Fatalf("TestProcessOrphans: parent block unexpectedly returned an error: %s", err)
+	}
+	if delay != 0 {
+		t.Fatalf("TestProcessOrphans: parent block is too far in the future")
+	}
+	if isOrphan {
+		t.Fatalf("TestProcessOrphans: incorrectly returned that parent block is an orphan")
+	}
+
+	// Make sure that the child block had been rejected
+	node := dag.index.LookupNode(childBlock.Hash())
+	if node == nil {
+		t.Fatalf("TestProcessOrphans: child block missing from block index")
+	}
+	if !dag.index.NodeStatus(node).KnownInvalid() {
+		t.Fatalf("TestProcessOrphans: child block erroneously not marked as invalid")
 	}
 }
