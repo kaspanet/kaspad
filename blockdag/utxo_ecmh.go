40657a83f5c5d591d199a69e64f3e6309ccfbf6e
Ori Newman <orinewman1@gmail.com>
2019-11-03 12:29:55 +0200

    [NOD-344] Cache ECMH (#445)
    * [NOD-134] Change newConnMtx to newConnReqMtx
    
    * [NOD-344] Change ECMH cache size to 4e6
    
    * [NOD-344] Refactor
    
    * [NOD-344] Fix go.mod

---
+++ blockdag/utxo_ecmh.go
@@ -0,0 +1,29 @@
+package blockdag
+
+import (
+	"bytes"
+	"github.com/daglabs/btcd/btcec"
+	"github.com/daglabs/btcd/util/daghash"
+	"github.com/daglabs/btcd/wire"
+	"github.com/golang/groupcache/lru"
+)
+
+const ecmhCacheSize = 4_000_000
+
+var ecmhCache = lru.New(ecmhCacheSize)
+
+func utxoMultiset(entry *UTXOEntry, outpoint *wire.Outpoint) (*btcec.Multiset, error) {
+	w := &bytes.Buffer{}
+	err := serializeUTXO(w, entry, outpoint)
+	if err != nil {
+		return nil, err
+	}
+	serializedUTXO := w.Bytes()
+	utxoHash := daghash.DoubleHashH(serializedUTXO)
+	if cachedMSPoint, ok := ecmhCache.Get(utxoHash); ok {
+		return cachedMSPoint.(*btcec.Multiset), nil
+	}
+	msPoint := btcec.NewMultiset(btcec.S256()).Add(serializedUTXO)
+	ecmhCache.Add(utxoHash, msPoint)
+	return msPoint, nil
+}
