13f06ca29341ea6150b29ead403f2c52c72211a2
stasatdaglabs <39559713+stasatdaglabs@users.noreply.github.com>
2019-11-04 13:04:06 +0200

    [NOD-399] Fix TxGen resending coinbase transactions. (#448)

--- cmd/txgen/txloop.go
+++ cmd/txgen/txloop.go
@@ -419,7 +419,7 @@ func applyConfirmedTransactionsAndResendNonAccepted(client *txgenClient, walletT
 			if isTxMatured(msgTx, *txResult.Confirmations) {
 				walletTx.confirmed = true
 				addTxOutsToUTXOSet(walletUTXOSet, msgTx)
-			} else if *txResult.Confirmations == 0 && !txResult.IsInMempool && blockChainHeight-maxResendDepth > walletTx.chainHeight {
+			} else if !msgTx.IsCoinBase() && *txResult.Confirmations == 0 && !txResult.IsInMempool && blockChainHeight-maxResendDepth > walletTx.chainHeight {
 				log.Infof("Transaction %s was not accepted in the DAG. Resending", txID)
 				txChan <- msgTx
 			}
