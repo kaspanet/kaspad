bc46594c95354b5ee5c8cfd1050104db1291792a
Ori Newman <orinewman1@gmail.com>
2019-11-04 16:16:16 +0200

    [NOD-384] Remove mass from rpc results

--- server/rpc/handle_get_raw_transaction.go
+++ server/rpc/handle_get_raw_transaction.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/hex"
 
-	"github.com/daglabs/btcd/blockdag"
 	"github.com/daglabs/btcd/btcjson"
 	"github.com/daglabs/btcd/database"
 	"github.com/daglabs/btcd/util"
@@ -31,7 +30,6 @@ func handleGetRawTransaction(s *Server, cmd interface{}, closeChan <-chan struct
 	// try the block database.
 	var tx *util.Tx
 	var blkHash *daghash.Hash
-	var txMass uint64
 	isInMempool := false
 	mempoolTx, err := s.cfg.TxMemPool.FetchTransaction(txID)
 	if err != nil {
@@ -68,10 +66,6 @@ func handleGetRawTransaction(s *Server, cmd interface{}, closeChan <-chan struct
 		}
 
 		tx = util.NewTx(&mtx)
-		txMass, err = calcTxMassFromTxIndex(s, tx)
-		if err != nil {
-			return nil, err
-		}
 	} else {
 		// When the verbose flag isn't set, simply return the
 		// network-serialized transaction as a hex-encoded string.
@@ -86,12 +80,6 @@ func handleGetRawTransaction(s *Server, cmd interface{}, closeChan <-chan struct
 				return nil, err
 			}
 			return mtxHex, nil
-		}
-
-		var err error
-		txMass, err = blockdag.CalcTxMassFromUTXOSet(mempoolTx, s.cfg.DAG.UTXOSet())
-		if err != nil {
-			return nil, err
 		}
 
 		tx = mempoolTx
@@ -121,32 +109,11 @@ func handleGetRawTransaction(s *Server, cmd interface{}, closeChan <-chan struct
 		}
 	}
 	rawTxn, err := createTxRawResult(s.cfg.DAGParams, tx.MsgTx(), txID.String(),
-		blkHeader, blkHashStr, nil, &confirmations, isInMempool, txMass)
+		blkHeader, blkHashStr, nil, &confirmations, isInMempool)
 	if err != nil {
 		return nil, err
 	}
 	return *rawTxn, nil
-}
-
-func calcTxMassFromTxIndex(s *Server, tx *util.Tx) (uint64, error) {
-	if tx.IsCoinBase() {
-		return blockdag.CalcTxMass(tx, nil), nil
-	}
-	previousScriptPubKeys := make([][]byte, len(tx.MsgTx().TxIn))
-	for i, txIn := range tx.MsgTx().TxIn {
-		txBytes, _, err := fetchTxBytesAndBlockHashFromTxIndex(s, &txIn.PreviousOutpoint.TxID)
-		if err != nil {
-			return 0, err
-		}
-		var msgTx wire.MsgTx
-		err = msgTx.Deserialize(bytes.NewReader(txBytes))
-		if err != nil {
-			context := "Failed to deserialize transaction"
-			return 0, internalRPCError(err.Error(), context)
-		}
-		previousScriptPubKeys[i] = msgTx.TxOut[txIn.PreviousOutpoint.Index].ScriptPubKey
-	}
-	return blockdag.CalcTxMass(tx, previousScriptPubKeys), nil
 }
 
 func fetchTxBytesAndBlockHashFromTxIndex(s *Server, txID *daghash.TxID) ([]byte, *daghash.Hash, error) {
--- server/rpc/rpcwebsocket.go
+++ server/rpc/rpcwebsocket.go
@@ -735,7 +735,7 @@ func (m *wsNotificationManager) notifyForNewTx(clients map[chan struct{}]*wsClie
 	initializeMarshalledJSONVerbose := func() bool {
 		net := m.server.cfg.DAGParams
 		build := func() ([]byte, bool) {
-			rawTx, err := createTxRawResult(net, mtx, txIDStr, nil, "", nil, nil, true, 0)
+			rawTx, err := createTxRawResult(net, mtx, txIDStr, nil, "", nil, nil, true)
 			if err != nil {
 				return nil, false
 			}
