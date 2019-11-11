afe51a31482757630bfc7a0c9de52055843879e0
Mike Zak <mike@daglabs.com>
2019-11-11 11:31:39 +0200

    [NOD-401] Work in progress for send command in cli-wallet

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/daglabs/btcd/dagconfig"
+)
+
+var activeNetParams = dagconfig.MainNetParams
+
 func main() {
 	subCommand, config := parseCommandLine()
 
@@ -7,8 +16,17 @@ func main() {
 	case "new":
 		new(config.(*newConfig))
 	case "balance":
-		balance(config.(*balanceConfig))
+		err := balance(config.(*balanceConfig))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+			os.Exit(1)
+		}
 	case "send":
-		send(config.(*sendConfig))
+		err := send(config.(*sendConfig))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+			os.Exit(1)
+		}
+
 	}
 }
--- cmd/wallet/send.go
+++ cmd/wallet/send.go
@@ -1,4 +1,37 @@
 package main
 
-func send(conf *sendConfig) {
+import (
+	"encoding/hex"
+
+	"github.com/daglabs/btcd/btcec"
+	"github.com/daglabs/btcd/util"
+	"github.com/pkg/errors"
+)
+
+func parsePrivateKey(privateKeyHex string) (*btcec.PrivateKey, *btcec.PublicKey, error) {
+	privateKeyBytes, err := hex.DecodeString(privateKeyHex)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "Error parsing private key hex")
+	}
+	privateKey, publicKey := btcec.PrivKeyFromBytes(btcec.S256(), privateKeyBytes)
+	return privateKey, publicKey, nil
+}
+
+func send(conf *sendConfig) error {
+	privateKey, publicKey, err := parsePrivateKey(conf.PrivateKey)
+	if err != nil {
+		return err
+	}
+
+	address, err := util.NewAddressPubKeyHashFromPublicKey(publicKey.SerializeCompressed(), activeNetParams.Prefix)
+	if err != nil {
+		return err
+	}
+
+	utxos, err := getUTXOs(conf.APIAddress, address.String())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
