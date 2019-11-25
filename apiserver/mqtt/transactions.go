944997abbcc8668eb9b24581471df09724ac8cde
Dan Aharoni <dan@daglabs.com>
2019-11-25 13:25:33 +0200

    [NOD-426] Load DB in controller

--- apiserver/mqtt/transactions.go
+++ apiserver/mqtt/transactions.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/daglabs/btcd/apiserver/apimodels"
 	"github.com/daglabs/btcd/apiserver/controllers"
-	"github.com/daglabs/btcd/apiserver/database"
 	"github.com/daglabs/btcd/btcjson"
 	"github.com/daglabs/btcd/rpcclient"
 	"github.com/daglabs/btcd/util/daghash"
@@ -75,11 +74,6 @@ func publishTransactionNotificationForAddress(transaction *apimodels.Transaction
 
 // PublishAcceptedTransactionsNotifications publishes notification for each accepted transaction of the given chain-block
 func PublishAcceptedTransactionsNotifications(addedChainBlocks []*rpcclient.ChainBlock) error {
-	db, err := database.DB()
-	if err != nil {
-		return err
-	}
-
 	for _, addedChainBlock := range addedChainBlocks {
 		for _, acceptedBlock := range addedChainBlock.AcceptedBlocks {
 			transactionIDs := make([]string, len(acceptedBlock.AcceptedTxIDs))
@@ -87,7 +81,7 @@ func PublishAcceptedTransactionsNotifications(addedChainBlocks []*rpcclient.Chai
 				transactionIDs[i] = acceptedTxID.String()
 			}
 
-			transactions, err := controllers.GetTransactionsByIDsHandler(db, transactionIDs)
+			transactions, err := controllers.GetTransactionsByIDsHandler(transactionIDs)
 			if err != nil {
 				return err
 			}
