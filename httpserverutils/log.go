d8a34866096c1eb8aa0a808167f9a80deacfda38
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-05 16:28:45 +0200

    [NOD-492] Fix more packages.

--- httpserverutils/log.go
+++ httpserverutils/log.go
@@ -1,7 +1,7 @@
 package httpserverutils
 
 import "github.com/daglabs/btcd/util/panics"
-import "github.com/daglabs/btcd/apiserver/logger"
+import "github.com/daglabs/btcd/kasparov/logger"
 
 var (
 	log   = logger.BackendLog.Logger("UTIL")
