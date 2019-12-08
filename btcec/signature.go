5a624a656d734c87dc4b0dc9cf7c0a5e89329a2a
Elichai Turkel <elichai.turkel@gmail.com>
2019-12-08 20:05:05 +0200

    Update the schnorr signature algorithm Replaced RFC6979 with a simple SHA256 Nicer code and more explanations

--- btcec/signature.go
+++ btcec/signature.go
@@ -6,11 +6,8 @@ package btcec
 
 import (
 	"bytes"
-	"crypto/elliptic"
-	"crypto/hmac"
 	"crypto/sha256"
 	"github.com/pkg/errors"
-	"hash"
 	"math/big"
 )
 
@@ -27,12 +24,9 @@ type Signature struct {
 }
 
 var (
-	// Used in RFC6979 implementation when testing the nonce for correctness
-	one = big.NewInt(1)
-
-	// oneInitializer is used to fill a byte slice with byte 0x01.  It is provided
-	// here to avoid the need to create it multiple times.
-	oneInitializer = []byte{0x01}
+	zero = big.NewInt(0)
+	// Group order in secp256k1
+	n = S256().N
 )
 
 // Serialize returns a serialized signature (R and S concatenated).
@@ -122,34 +116,16 @@ func bigIntTo32Bytes(val *big.Int) []byte {
 	return append(pad, b...)
 }
 
-// hashToInt converts a hash value to an integer. There is some disagreement
-// about how this is done. [NSA] suggests that this is done in the obvious
-// manner, but [SECG] truncates the hash to the bit-length of the curve order
-// first. We follow [SECG] because that's what OpenSSL does. Additionally,
-// OpenSSL right shifts excess bits from the number if the hash is too large
-// and we mirror that too.
-// This is borrowed from crypto/ecdsa.
-func hashToInt(hash []byte, c elliptic.Curve) *big.Int {
-	orderBits := c.Params().N.BitLen()
-	orderBytes := (orderBits + 7) / 8
-	if len(hash) > orderBytes {
-		hash = hash[:orderBytes]
-	}
-
-	ret := new(big.Int).SetBytes(hash)
-	excess := len(hash)*8 - orderBits
-	if excess > 0 {
-		ret.Rsh(ret, uint(excess))
-	}
-	return ret
-}
-
 // sign signs the hash using the schnorr signature algorithm.
 func sign(privateKey *PrivateKey, hash []byte) (*Signature, error) {
-	// The rfc6979 nonce derivation function accepts additional entropy.
-	// See https://github.com/bitcoincashorg/bitcoincash.org/blob/master/spec/2019-05-15-schnorr.md#recommended-practices-for-secure-signature-generation
-	additionalData := []byte{'S', 'c', 'h', 'n', 'o', 'r', 'r', '+', 'S', 'H', 'A', '2', '5', '6', ' ', ' '}
-	k := nonceRFC6979(privateKey.D, hash, additionalData)
+
+	kBytes := sha256.Sum256(append(privateKey.Serialize(), hash...))
+	k := new(big.Int).SetBytes(kBytes[:])
+	// The modulo is fine because n is close enough to 2^256 that this is very very rare
+	k.Mod(k, n)
+	if k.Cmp(zero) == 0 {
+		return nil, errors.New("Something bad happend with sha256. got 0")
+	}
 	// Compute point R = k * G
 	rx, ry := privateKey.Curve.ScalarBaseMult(k.Bytes())
 
@@ -161,107 +137,15 @@ func sign(privateKey *PrivateKey, hash []byte) (*Signature, error) {
 	// Compute scalar e = Hash(R.x || compressed(P) || m) mod N
 	eBytes := sha256.Sum256(append(append(bigIntTo32Bytes(rx), privateKey.PubKey().SerializeCompressed()...), hash...))
 	e := new(big.Int).SetBytes(eBytes[:])
-	e.Mod(e, privateKey.Params().N)
+	// The modulo is fine because n is close enough to 2^256 that this is very very rare
+	e.Mod(e, n)
 
 	// Compute scalar s = (k + e * x) mod N
-	x := new(big.Int).SetBytes(privateKey.Serialize())
-	s := e.Mul(e, x)
+	s := e.Mul(e, privateKey.D)
 	s.Add(s, k)
-	s.Mod(s, privateKey.Params().N)
+	s.Mod(s, n)
 	return &Signature{
 		R: rx,
 		S: s,
 	}, nil
-}
-
-// nonceRFC6979 generates a nonce (`k`) deterministically according to RFC 6979.
-// It takes a 32-byte hash as an input and returns 32-byte nonce to be used in the digital signature algorithm.
-func nonceRFC6979(privkey *big.Int, hash []byte, additionalData []byte) *big.Int {
-
-	// Step A
-	curve := S256()
-	q := curve.Params().N
-	x := privkey
-	alg := sha256.New
-
-	qlen := q.BitLen()
-	holen := alg().Size()
-	rolen := (qlen + 7) >> 3
-	bx := append(int2octets(x, rolen), bits2octets(hash, curve, rolen)...)
-
-	// Step B
-	v := bytes.Repeat(oneInitializer, holen)
-
-	// Step C (Go zeroes the all allocated memory)
-	k := make([]byte, holen)
-
-	// Step D
-	k = mac(alg, k, append(append(append(v, 0x00), bx...), additionalData...))
-
-	// Step E
-	v = mac(alg, k, v)
-
-	// Step F
-	k = mac(alg, k, append(append(append(v, 0x01), bx...), additionalData...))
-
-	// Step G
-	v = mac(alg, k, v)
-
-	// Step H
-	for {
-		// Step H1
-		var t []byte
-
-		// Step H2
-		for len(t)*8 < qlen {
-			v = mac(alg, k, v)
-			t = append(t, v...)
-		}
-
-		// Step H3
-		secret := hashToInt(t, curve)
-		if secret.Cmp(one) >= 0 && secret.Cmp(q) < 0 {
-			return secret
-		}
-		k = mac(alg, k, append(v, 0x00))
-		v = mac(alg, k, v)
-	}
-}
-
-// mac returns an HMAC of the given key and message.
-func mac(alg func() hash.Hash, k, m []byte) []byte {
-	h := hmac.New(alg, k)
-	h.Write(m)
-	return h.Sum(nil)
-}
-
-// https://tools.ietf.org/html/rfc6979#section-2.3.3
-func int2octets(v *big.Int, rolen int) []byte {
-	out := v.Bytes()
-
-	// left pad with zeros if it's too short
-	if len(out) < rolen {
-		out2 := make([]byte, rolen)
-		copy(out2[rolen-len(out):], out)
-		return out2
-	}
-
-	// drop most significant bytes if it's too long
-	if len(out) > rolen {
-		out2 := make([]byte, rolen)
-		copy(out2, out[len(out)-rolen:])
-		return out2
-	}
-
-	return out
-}
-
-func bits2octets(in []byte, curve elliptic.Curve, rolen int) []byte {
-	// https://tools.ietf.org/html/rfc6979#section-2.3.4
-	z1 := hashToInt(in, curve)
-	z2 := new(big.Int).Sub(z1, curve.Params().N)
-	if z2.Sign() < 0 {
-		return int2octets(z1, rolen)
-	}
-	return int2octets(z2, rolen)
 }
