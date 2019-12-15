5426e238545c66912c9405fbd13786949dfd800c
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-15 18:06:25 +0200

    [NOD-514] Remove no-longer-relevant link in doc.

--- util/bech32/doc.go
+++ util/bech32/doc.go
@@ -8,7 +8,5 @@ Package bech32 provides a Go implementation of the bech32 format.
 Bech32 strings consist of a prefix, followed by the
 separator :, then a checksummed data part encoded using the 32 characters
 "qpzry9x8gf2tvdw0s3jn54khce6mua7l".
-
-More info: https://github.com/kaspanet/spec/blob/master/kaspa.pdf
 */
 package bech32
