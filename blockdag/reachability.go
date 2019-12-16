783698156e78015bf00944413f5d62c96b01ac9f
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-16 17:58:47 +0200

    [NOD-540] Add comments.

--- blockdag/reachability.go
+++ blockdag/reachability.go
@@ -9,48 +9,57 @@ type reachabilityInterval struct {
 
 type futureBlocks []*blockNode
 
+// insertFutureBlock inserts the given block into this futureBlocks
+// if required.
 func (fb *futureBlocks) insertFutureBlock(block *blockNode) {
 	start := block.interval.start
-	end := block.interval.end
-	i := fb.bisect(end)
+	i := fb.bisect(block)
 	if i > 0 {
 		candidate := (*fb)[i-1]
+		end := block.interval.end
 		if candidate.interval.start <= end && end <= candidate.interval.end {
 			// candidate is an ancestor of block, no need to insert
 			return
 		}
 		if start <= candidate.interval.end && candidate.interval.end <= end {
-			// block is ancestor of candidate, and can thus replace it
+			// block is an ancestor of candidate, and can thus replace it
 			(*fb)[i-1] = block
 			return
 		}
 	}
 
-	// Insert block in the correct index to maintain futureBlocks as a sorted-by-interval list
-	// (note that i might be equal to len(futureBlocks))
+	// Insert block in the correct index to maintain futureBlocks as
+	// a sorted-by-interval list.
+	// Note that i might be equal to len(futureBlocks)
 	left := (*fb)[:i]
 	right := append([]*blockNode{block}, (*fb)[i:]...)
 	*fb = append(left, right...)
 }
 
+// isFutureBlock resolves whether the given block is in the future of
+// any block in this futureBlocks.
 func (fb futureBlocks) isFutureBlock(block *blockNode) bool {
-	end := block.interval.end
-	i := fb.bisect(end)
+	i := fb.bisect(block)
 	if i == 0 {
 		// No candidate to contain block
 		return false
 	}
 
 	candidate := fb[i-1]
+	end := block.interval.end
 	return candidate.interval.start <= end && end <= candidate.interval.end
 }
 
-func (fb futureBlocks) bisect(intervalEnd uint64) int {
+// bisect finds the appropriate index for the givev block's reachability
+// interval.
+func (fb futureBlocks) bisect(block *blockNode) int {
+	end := block.interval.end
+
 	low := 0
 	high := len(fb)
 	for low < high {
 		middle := (low + high) / 2
-		if intervalEnd < fb[middle].interval.start {
+		if end < fb[middle].interval.start {
 			high = middle
 		} else {
 			low = middle + 1
