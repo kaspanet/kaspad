36755c29d09e798aece65b9ed5682583294480d8
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-26 15:56:57 +0200

    [NOD-517] Update doc.go in rpctest.

--- integration/rpctest/doc.go
+++ integration/rpctest/doc.go
@@ -5,10 +5,5 @@ interface. Each instance of an active harness comes equipped with a simple
 in-memory HD wallet capable of properly syncing to the generated chain,
 creating new addresses, and crafting fully signed transactions paying to an
 arbitrary set of outputs.
-
-This package was designed specifically to act as an RPC testing harness for
-`kaspad`. However, the constructs presented are general enough to be adapted to
-any project wishing to programmatically drive a `kaspad` instance of its
-systems/integration tests.
 */
 package rpctest
