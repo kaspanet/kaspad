72c7b8df593e237b94d7a0abd70a082a10c96468
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-26 15:55:56 +0200

    [NOD-517] Update doc.go in ecc.

--- ecc/doc.go
+++ ecc/doc.go
@@ -1,7 +1,7 @@
 /*
-Package ecc implements support for the elliptic curves needed for bitcoin.
+Package ecc implements support for the elliptic curves needed for kaspa.
 
-Bitcoin uses elliptic curve cryptography using koblitz curves
+Kaspa uses elliptic curve cryptography using koblitz curves
 (specifically secp256k1) for cryptographic functions. See
 http://www.secg.org/collateral/sec2_final.pdf for details on the
 standard.
@@ -10,8 +10,7 @@ This package provides the data structures and functions implementing the
 crypto/elliptic Curve interface in order to permit using these curves
 with the standard crypto/ecdsa package provided with go. Helper
 functionality is provided to parse signatures and public keys from
-standard formats. It was designed for use with kaspad, but should be
-general enough for other uses of elliptic curve crypto. It was originally based
-on some initial work by ThePiachu, but has significantly diverged since then.
+standard formats. It was originally based on some initial work by
+ThePiachu, but has significantly diverged since then.
 */
 package ecc
