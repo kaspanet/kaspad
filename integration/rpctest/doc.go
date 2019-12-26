a740e2d527ffb1c389914d484c3a1ee60c2332d5
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-26 12:28:16 +0200

    [NOD-517] Make all doc.go use the same style of comment.

--- integration/rpctest/doc.go
+++ integration/rpctest/doc.go
@@ -1,12 +1,14 @@
-// Package rpctest provides a kaspad-specific RPC testing harness crafting and
-// executing integration tests by driving a `kaspad` instance via the `RPC`
-// interface. Each instance of an active harness comes equipped with a simple
-// in-memory HD wallet capable of properly syncing to the generated chain,
-// creating new addresses, and crafting fully signed transactions paying to an
-// arbitrary set of outputs.
-//
-// This package was designed specifically to act as an RPC testing harness for
-// `kaspad`. However, the constructs presented are general enough to be adapted to
-// any project wishing to programmatically drive a `kaspad` instance of its
-// systems/integration tests.
+/*
+Package rpctest provides a kaspad-specific RPC testing harness crafting and
+executing integration tests by driving a `kaspad` instance via the `RPC`
+interface. Each instance of an active harness comes equipped with a simple
+in-memory HD wallet capable of properly syncing to the generated chain,
+creating new addresses, and crafting fully signed transactions paying to an
+arbitrary set of outputs.
+
+This package was designed specifically to act as an RPC testing harness for
+`kaspad`. However, the constructs presented are general enough to be adapted to
+any project wishing to programmatically drive a `kaspad` instance of its
+systems/integration tests.
+*/
 package rpctest
