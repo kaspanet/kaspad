a6d2d4fd53d23aaca6f8699ba56a85261bad79c0
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2019-12-30 11:26:45 +0200

    [NOD-517] Update doc.go in bech32.

--- util/bech32/doc.go
+++ util/bech32/doc.go
@@ -1,8 +1,8 @@
 /*
 Package bech32 provides a Go implementation of the bech32 format.
 
-Bech32 strings consist of a prefix, followed by the
-separator :, then a checksummed data part encoded using the 32 characters
+Bech32 strings consist of a prefix, followed by the separator :,
+then a checksummed data part encoded using the 32 characters
 "qpzry9x8gf2tvdw0s3jn54khce6mua7l".
 */
 package bech32
