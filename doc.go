4498df16c1672d04b7cd1447b7c0cc80285c2a1d
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2020-01-02 16:51:51 +0200

    [NOD-517] Add Conformal to the license notices.

--- doc.go
+++ doc.go
@@ -2,6 +2,7 @@
 Copyright (c) 2018-2019 The kaspanet developers
 Copyright (c) 2013-2018 The btcsuite developers
 Copyright (c) 2015-2016 The Decred developers
+Copyright (c) 2013-2014 Conformal Systems LLC.
 Use of this source code is governed by an ISC
 license that can be found in the LICENSE file.
 
