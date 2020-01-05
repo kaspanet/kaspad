b6cc6b80326d689eb12acc5b95faf81afe1ac398
Ori Newman <orinewman1@gmail.com>
2020-01-05 17:59:42 +0200

    [NOD-617] Log coverage

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Test each package (excluding vendor packages) separately
+# Test each package separately
 # Function inlining messes with monkey patching so we disable it by passing -gcflags='-l'
+# Running tests with -covermode=atomic saves us from race conditions unique to the testing environment
 go list ./... | \
-    xargs -n1 -I{} sh -c "go test -gcflags='-l' -timeout 60s {}"
+    xargs -n1 -I{} sh -c "go test -gcflags='-l' -timeout 60s -covermode=atomic -cover {}"
 
 retVal=$?
 if [ $retVal -ne 0 ]
