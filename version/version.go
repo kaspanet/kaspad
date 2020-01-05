2e16a831599cb19ed94755968dad48183707b88f
Mike Zak <feanorr@gmail.com>
2020-01-05 19:01:45 +0200

    [NOD-549] Remove any mention of semVer.

--- version/version.go
+++ version/version.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 )
 
-// semanticAlphabet
-const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+// validAlphabet is a list of charecters valid in the appBuild string
+const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
 
 const (
 	appMajor uint = 0
@@ -17,10 +17,10 @@ const (
 
 // appBuild is defined as a variable so it can be overridden during the build
 // process with '-ldflags "-X github.com/kaspanet/kaspad/version.appBuild=foo"' if needed.
-// It MUST only contain characters from semanticAlphabet.
+// It MUST only contain characters from validCharacters.
 var appBuild string
 
-var version = ""
+var version = "" // string used for memoization of version
 
 // Version returns the application version as a properly formed string
 func Version() string {
@@ -40,13 +40,11 @@ func Version() string {
 }
 
 // normalizeVerString returns the passed string stripped of all characters which
-// are not valid according to the semantic versioning guidelines for pre-release
-// version and build metadata strings. In particular they MUST only contain
-// characters in semanticAlphabet.
+// are not in validCharacters.
 func normalizeVerString(str string) string {
 	var result bytes.Buffer
 	for _, r := range str {
-		if strings.ContainsRune(semanticAlphabet, r) {
+		if strings.ContainsRune(validCharacters, r) {
 			result.WriteRune(r)
 		}
 	}
