c37b4548944274820b1fdd18d4bfb0f6ec210dec
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2020-01-08 17:30:44 +0200

    TEST COMMIT. DO NOT MERGE.

--- blockdag/dag.go
+++ blockdag/dag.go
@@ -577,7 +577,7 @@ func (dag *BlockDAG) connectBlock(node *blockNode,
 			return nil, ruleError(err.ErrorCode, newErrString)
 		}
 		return nil, errors.New(newErrString)
-	}fdgfsgdfdsgsd
+	}fdgfsgdfdfdsfdssgsd
 
 	err = node.validateCoinbaseTransaction(dag, block, txsAcceptanceData)
 	if err != nil {
