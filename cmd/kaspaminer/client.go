e0096d84cec6189f1903f07e99210e6dbb616777
Ori Newman <orinewman1@gmail.com>
2020-01-14 17:44:33 +0200

    [NOD-650] Add kaspaminer

---
+++ cmd/kaspaminer/client.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"github.com/kaspanet/kaspad/rpcclient"
+	"github.com/kaspanet/kaspad/util"
+	"github.com/kaspanet/kaspad/wire"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net"
+	"time"
+)
+
+type minerClient struct {
+	*rpcclient.Client
+	onBlockAdded chan struct{}
+}
+
+func newMinerClient(connCfg *rpcclient.ConnConfig) (*minerClient, error) {
+	client := &minerClient{
+		onBlockAdded: make(chan struct{}, 1),
+	}
+	notificationHandlers := &rpcclient.NotificationHandlers{
+		OnFilteredBlockAdded: func(height uint64, header *wire.BlockHeader,
+			txs []*util.Tx) {
+			client.onBlockAdded <- struct{}{}
+		},
+	}
+	var err error
+	client.Client, err = rpcclient.New(connCfg, notificationHandlers)
+	if err != nil {
+		return nil, errors.Errorf("Error connecting to address %s: %s", connCfg.Host, err)
+	}
+
+	if err = client.NotifyBlocks(); err != nil {
+		return nil, errors.Errorf("Error while registering client %s for block notifications: %s", client.Host(), err)
+	}
+	return client, nil
+}
+
+func connectToServer(cfg *configFlags) (*minerClient, error) {
+	cert, err := readCert(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	connCfg := &rpcclient.ConnConfig{
+		Host:           normalizeRPCServerAddress(cfg.RPCServer, cfg),
+		Endpoint:       "ws",
+		User:           cfg.RPCUser,
+		Pass:           cfg.RPCPassword,
+		DisableTLS:     cfg.DisableTLS,
+		RequestTimeout: time.Second * 10,
+		Certificates:   cert,
+	}
+
+	client, err := newMinerClient(connCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infof("Connected to server %s", client.Host())
+
+	return client, nil
+}
+
+// normalizeRPCServerAddress returns addr with the current network default
+// port appended if there is not already a port specified.
+func normalizeRPCServerAddress(addr string, cfg *configFlags) string {
+	_, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return net.JoinHostPort(addr, cfg.NetParams().RPCPort)
+	}
+	return addr
+}
+
+func readCert(cfg *configFlags) ([]byte, error) {
+	if cfg.DisableTLS {
+		return nil, nil
+	}
+
+	cert, err := ioutil.ReadFile(cfg.RPCCert)
+	if err != nil {
+		return nil, errors.Errorf("Error reading certificates file: %s", err)
+	}
+
+	return cert, nil
+}
---
+++ cmd/kaspaminer/config.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kaspanet/kaspad/config"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/kaspanet/kaspad/util"
+	"github.com/pkg/errors"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/kaspanet/kaspad/cmd/kaspaminer/version"
+)
+
+const (
+	defaultLogFilename    = "kaspaminer.log"
+	defaultErrLogFilename = "kaspaminer_err.log"
+)
+
+var (
+	// Default configuration options
+	defaultHomeDir    = util.AppDataDir("kaspaminer", false)
+	defaultLogFile    = filepath.Join(defaultHomeDir, defaultLogFilename)
+	defaultErrLogFile = filepath.Join(defaultHomeDir, defaultErrLogFilename)
+	defaultRPCServer  = "localhost"
+)
+
+type configFlags struct {
+	ShowVersion    bool   `short:"V" long:"version" description:"Display version information and exit"`
+	RPCUser        string `short:"u" long:"rpcuser" description:"RPC username"`
+	RPCPassword    string `short:"P" long:"rpcpass" default-mask:"-" description:"RPC password"`
+	RPCServer      string `short:"s" long:"rpcserver" description:"RPC server to connect to"`
+	RPCCert        string `short:"c" long:"rpccert" description:"RPC server certificate chain for validation"`
+	DisableTLS     bool   `long:"notls" description:"Disable TLS"`
+	Verbose        bool   `long:"verbose" short:"v" description:"Enable logging of RPC requests"`
+	NumberOfBlocks uint64 `short:"n" long:"numblocks" description:"Number of blocks to mine. If omitted, will mine until the process is interrupted."`
+	config.NetworkFlags
+}
+
+func parseConfig() (*configFlags, error) {
+	cfg := &configFlags{
+		RPCServer: defaultRPCServer,
+	}
+	parser := flags.NewParser(cfg, flags.PrintErrors|flags.HelpFlag)
+	_, err := parser.Parse()
+
+	// Show the version and exit if the version flag was specified.
+	if cfg.ShowVersion {
+		appName := filepath.Base(os.Args[0])
+		appName = strings.TrimSuffix(appName, filepath.Ext(appName))
+		fmt.Println(appName, "version", version.Version())
+		os.Exit(0)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = cfg.ResolveNetwork(parser)
+	if err != nil {
+		return nil, err
+	}
+
+	if cfg.RPCUser == "" {
+		return nil, errors.New("--rpcuser is required")
+	}
+	if cfg.RPCPassword == "" {
+		return nil, errors.New("--rpcpass is required")
+	}
+
+	if cfg.RPCCert == "" && !cfg.DisableTLS {
+		return nil, errors.New("--notls has to be disabled if --cert is used")
+	}
+	if cfg.RPCCert != "" && cfg.DisableTLS {
+		return nil, errors.New("--rpccert should be omitted if --notls is used")
+	}
+
+	initLog(defaultLogFile, defaultErrLogFile)
+
+	return cfg, nil
+}
