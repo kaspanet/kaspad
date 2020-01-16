29c3f743e530e7845fdcb4bc95c4cb0fbfbc94af
stasatdaglabs <Ai!8ij90jDti&tnm1KXU>
2020-01-16 10:39:35 +0200

    [NOD-616] Add ban score to peers misusing GetBlockLocator.

--- server/p2p/on_get_block_locator.go
+++ server/p2p/on_get_block_locator.go
@@ -1,21 +1,25 @@
 package p2p
 
 import (
+	"fmt"
 	"github.com/kaspanet/kaspad/peer"
 	"github.com/kaspanet/kaspad/wire"
 )
+
+const badGetBlockLocatorBanScore = 50
 
 // OnGetBlockLocator is invoked when a peer receives a getlocator kaspa
 // message.
 func (sp *Peer) OnGetBlockLocator(_ *peer.Peer, msg *wire.MsgGetBlockLocator) {
 	locator, err := sp.server.DAG.BlockLocatorFromHashes(msg.StartHash, msg.StopHash)
-	if err != nil {
-		peerLog.Infof("Couldn't build a block locator between blocks %s and %s "+
-			"that was requested from peer %s: %s", sp, err)
-	}
-	if len(locator) == 0 {
-		peerLog.Infof("Couldn't build a block locator between blocks %s and %s "+
-			"that was requested from peer %s", sp)
+	if err != nil || len(locator) == 0 {
+		warning := fmt.Sprintf("Couldn't build a block locator between blocks "+
+			"%s and %s that was requested from peer %s", msg.StartHash, msg.StopHash, sp)
+		if err != nil {
+			warning = fmt.Sprintf("%s: %s", warning, err)
+		}
+		peerLog.Warnf(warning)
+		sp.addBanScore(0, badGetBlockLocatorBanScore, "getBlockLocator")
 		return
 	}
 
