8b881ef1b0e6a9553a8cac31a696d2519c84cd33
Dan Aharoni <dan@daglabs.com>
2020-01-22 18:08:33 +0200

    [NOD-675] Fix test name.

--- rpcmodel/rpc_commands_test.go
+++ rpcmodel/rpc_commands_test.go
@@ -987,7 +987,7 @@ func TestRPCServerCommands(t *testing.T) {
 			unmarshalled: &rpcmodel.GetTopHeadersCmd{},
 		},
 		{
-			name: "getTopHeaders - with low hash",
+			name: "getTopHeaders - with high hash",
 			newCmd: func() (interface{}, error) {
 				return rpcmodel.NewCommand("getTopHeaders", "000000000000000000ba33b33e1fad70b69e234fc24414dd47113bff38f523f7")
 			},
