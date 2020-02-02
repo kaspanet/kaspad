cf76aa413da0b873dd034992641011ed45e8b093
Ori Newman <orinewman1@gmail.com>
2020-02-02 12:26:36 +0200

    [NOD-641] Rename cerr->bdRuleErr

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -811,9 +811,9 @@ func (mp *TxPool) maybeAcceptTransaction(tx *util.Tx, rejectDupOrphans bool) ([]
 	// transactions are allowed into blocks.
 	err := blockdag.CheckTransactionSanity(tx, subnetworkID)
 	if err != nil {
-		var cerr blockdag.RuleError
-		if ok := errors.As(err, &cerr); ok {
-			return nil, nil, dagRuleError(cerr)
+		var ruleErr blockdag.RuleError
+		if ok := errors.As(err, &ruleErr); ok {
+			return nil, nil, dagRuleError(ruleErr)
 		}
 		return nil, nil, err
 	}
@@ -919,9 +919,9 @@ func (mp *TxPool) maybeAcceptTransaction(tx *util.Tx, rejectDupOrphans bool) ([]
 	// with respect to its defined relative lock times.
 	sequenceLock, err := mp.cfg.CalcSequenceLockNoLock(tx, mp.mpUTXOSet)
 	if err != nil {
-		var cerr blockdag.RuleError
-		if ok := errors.As(err, &cerr); ok {
-			return nil, nil, dagRuleError(cerr)
+		var bdRuleErr blockdag.RuleError
+		if ok := errors.As(err, &bdRuleErr); ok {
+			return nil, nil, dagRuleError(bdRuleErr)
 		}
 		return nil, nil, err
 	}
@@ -949,9 +949,9 @@ func (mp *TxPool) maybeAcceptTransaction(tx *util.Tx, rejectDupOrphans bool) ([]
 	txFee, err := blockdag.CheckTransactionInputsAndCalulateFee(tx, nextBlockBlueScore,
 		mp.mpUTXOSet, mp.cfg.DAGParams, false)
 	if err != nil {
-		var cerr blockdag.RuleError
-		if ok := errors.As(err, &cerr); ok {
-			return nil, nil, dagRuleError(cerr)
+		var bdRuleErr blockdag.RuleError
+		if ok := errors.As(err, &bdRuleErr); ok {
+			return nil, nil, dagRuleError(bdRuleErr)
 		}
 		return nil, nil, err
 	}
@@ -1010,9 +1010,9 @@ func (mp *TxPool) maybeAcceptTransaction(tx *util.Tx, rejectDupOrphans bool) ([]
 	err = blockdag.ValidateTransactionScripts(tx, mp.mpUTXOSet,
 		txscript.StandardVerifyFlags, mp.cfg.SigCache)
 	if err != nil {
-		var cerr blockdag.RuleError
-		if ok := errors.As(err, &cerr); ok {
-			return nil, nil, dagRuleError(cerr)
+		var bdRuleErr blockdag.RuleError
+		if ok := errors.As(err, &bdRuleErr); ok {
+			return nil, nil, dagRuleError(bdRuleErr)
 		}
 		return nil, nil, err
 	}
