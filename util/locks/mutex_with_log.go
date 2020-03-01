70e4647059afa756123c85ab224a5b31b6389508
Ori Newman <orinewman1@gmail.com>
2020-03-01 14:07:12 +0200

    [NOD-799] Add comments to MutexWithLog and RWMutexWithLog methods

--- util/locks/mutex_with_log.go
+++ util/locks/mutex_with_log.go
@@ -10,11 +10,13 @@ type MutexWithLog struct {
 	sync.Mutex
 }
 
+// Lock locks MutexWithLog underlying sync.Mutex
 func (rwm *MutexWithLog) Lock() {
 	log.Debugf("MutexWithLog.Lock():%s", goroutineIDAndCallerToMutex())
 	rwm.Mutex.Lock()
 }
 
+// Unlock unlocks MutexWithLog underlying sync.Mutex
 func (rwm *MutexWithLog) Unlock() {
 	log.Debugf("MutexWithLog.Unlock():%s", goroutineIDAndCallerToMutex())
 	rwm.Mutex.Unlock()
--- util/locks/rwmutex_with_log.go
+++ util/locks/rwmutex_with_log.go
@@ -10,21 +10,25 @@ type RWMutexWithLog struct {
 	sync.RWMutex
 }
 
+// Lock locks RWMutexWithLog underlying sync.RWMutex
 func (rwm *RWMutexWithLog) Lock() {
 	log.Debugf("RWMutexWithLog.Lock():%s", goroutineIDAndCallerToMutex())
 	rwm.RWMutex.Lock()
 }
 
+// Unlock unlocks RWMutexWithLog underlying sync.RWMutex
 func (rwm *RWMutexWithLog) Unlock() {
 	log.Debugf("RWMutexWithLog.Unlock():%s", goroutineIDAndCallerToMutex())
 	rwm.RWMutex.Unlock()
 }
 
+// RLock read-locks RWMutexWithLog underlying sync.RWMutex
 func (rwm *RWMutexWithLog) RLock() {
 	log.Debugf("RWMutexWithLog.RLock():%s", goroutineIDAndCallerToMutex())
 	rwm.RWMutex.RLock()
 }
 
+// RUnlock read-unlocks RWMutexWithLog underlying sync.RWMutex
 func (rwm *RWMutexWithLog) RUnlock() {
 	log.Debugf("RWMutexWithLog.RUnlock():%s", goroutineIDAndCallerToMutex())
 	rwm.RWMutex.RUnlock()
