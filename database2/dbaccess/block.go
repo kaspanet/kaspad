3404bd18bd2e8db81b5c1f804cbe8bbb138b0250
stasatdaglabs <stas@daglabs.com>
2020-03-18 10:24:01 +0200

    [NOD-828] Remove anything from dbaccess that I'm not planning to implement as part of this ticket.

--- database2/dbaccess/block.go
+++ database2/dbaccess/block.go
@@ -5,18 +5,16 @@ import (
 	"github.com/kaspanet/kaspad/util/daghash"
 )
 
+const blockStoreName = "blocks"
+
 func StoreBlock(context Context, block *util.Block) error {
 	return nil
-}
-
-func HasBlock(context Context, hash *daghash.Hash) (bool, error) {
-	return false, nil
 }
 
 func FetchBlock(context Context, hash *daghash.Hash) ([]byte, error) {
 	return nil, nil
 }
 
-func FetchBlocks(context Context, hashes []*daghash.Hash) ([][]byte, error) {
-	return nil, nil
+func HasBlock(context Context, hash *daghash.Hash) (bool, error) {
+	return false, nil
 }
--- database2/dbaccess/dbmodel/indexblock.go
+++
@@ -1,4 +0,0 @@
-package dbmodel
-
-type DBBlockIndexBlock struct {
-}
