0cbf32dbcbc97e763c8102f4f1391e8cbab43dee
Ori Newman <orinewman1@gmail.com>
2020-03-31 15:29:05 +0300

    [NOD-865] Don't iterate cursors while changing db data

--- dbaccess/reachability.go
+++ dbaccess/reachability.go
@@ -43,18 +43,23 @@ func ClearReachabilityData() error {
 		return err
 	}
 
+	// Collect all of the keys
+	keys := make([][]byte, 0)
 	cursor, err := accessor.Cursor(reachabilityDataBucket.Path())
 	if err != nil {
 		return err
 	}
-
-	for ok := cursor.First(); ok; ok = cursor.Next() {
+	for cursor.Next() {
 		key, err := cursor.Key()
 		if err != nil {
 			return err
 		}
+		keys = append(keys, key)
+	}
 
-		err = accessor.Delete(key)
+	// Delete all of the keys
+	for _, key := range keys {
+		err := accessor.Delete(key)
 		if err != nil {
 			return err
 		}
