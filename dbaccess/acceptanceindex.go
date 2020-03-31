355a8b3414e7cace39e127b541cbb47f62cf037d
stasatdaglabs <stas@daglabs.com>
2020-03-31 16:26:41 +0300

    [NOD-866] Explain collecting keys before deleting them.

--- dbaccess/acceptanceindex.go
+++ dbaccess/acceptanceindex.go
@@ -70,7 +70,9 @@ func DropAcceptanceIndex() error {
 		return err
 	}
 
-	// Collect all of the keys
+	// Collect all of the keys before deleting them. We do this
+	// as to not modify the cursor while we're still iterating
+	// over it.
 	keys := make([][]byte, 0)
 	cursor, err := accessor.Cursor(acceptanceIndexBucket.Path())
 	if err != nil {
