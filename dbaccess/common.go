e511ec996d76ae178e52b91d486178935a579a83
Ori Newman <orinewman1@gmail.com>
2020-04-01 09:47:44 +0300

    [NOD-865] Make clearBucket function

---
+++ dbaccess/common.go
@@ -0,0 +1,42 @@
+package dbaccess
+
+import "github.com/kaspanet/kaspad/database2"
+
+func clearBucket(bucket *database2.Bucket) error {
+	dbTx, err := NewTx()
+	if err != nil {
+		return err
+	}
+	defer dbTx.RollbackUnlessClosed()
+
+	accessor, err := dbTx.accessor()
+	if err != nil {
+		return err
+	}
+
+	// Collect all of the keys before deleting them. We do this
+	// as to not modify the cursor while we're still iterating
+	// over it.
+	keys := make([][]byte, 0)
+	cursor, err := accessor.Cursor(bucket.Path())
+	if err != nil {
+		return err
+	}
+	for cursor.Next() {
+		key, err := cursor.Key()
+		if err != nil {
+			return err
+		}
+		keys = append(keys, key)
+	}
+
+	// Delete all of the keys
+	for _, key := range keys {
+		err := accessor.Delete(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return dbTx.Commit()
+}
--- dbaccess/reachability.go
+++ dbaccess/reachability.go
@@ -32,42 +32,7 @@ func StoreReachabilityData(context Context, blockHash *daghash.Hash, reachabilit
 // ClearReachabilityData clears the reachability data
 // from database.
 func ClearReachabilityData() error {
-	dbTx, err := NewTx()
-	if err != nil {
-		return err
-	}
-	defer dbTx.RollbackUnlessClosed()
-
-	accessor, err := dbTx.accessor()
-	if err != nil {
-		return err
-	}
-
-	// Collect all of the keys before deleting them. We do this
-	// as to not modify the cursor while we're still iterating
-	// over it.
-	keys := make([][]byte, 0)
-	cursor, err := accessor.Cursor(reachabilityDataBucket.Path())
-	if err != nil {
-		return err
-	}
-	for cursor.Next() {
-		key, err := cursor.Key()
-		if err != nil {
-			return err
-		}
-		keys = append(keys, key)
-	}
-
-	// Delete all of the keys
-	for _, key := range keys {
-		err := accessor.Delete(key)
-		if err != nil {
-			return err
-		}
-	}
-
-	return dbTx.Commit()
+	return clearBucket(reachabilityDataBucket)
 }
 
 func reachabilityKey(hash *daghash.Hash) []byte {
