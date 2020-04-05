badd3ea6ba2d7f8281e21ddf4a68541af4f5afb8
stasatdaglabs <stas@daglabs.com>
2020-04-05 12:37:20 +0300

    [NOD-897] Add a test for rolling back flat files within a transaction.

--- database/ffldb/ffldb_test.go
+++ database/ffldb/ffldb_test.go
@@ -107,3 +107,86 @@ func TestRepairFlatFiles(t *testing.T) {
 			"returned wrong error: %s", err)
 	}
 }
+
+func TestFlatFileRollbackInTransactions(t *testing.T) {
+	// Create a temp db to run tests against
+	path, err := ioutil.TempDir("", "TestFlatFileRollbackInTransactions")
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: TempDir unexpectedly "+
+			"failed: %s", err)
+	}
+	db, err := Open(path)
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: Open unexpectedly "+
+			"failed: %s", err)
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			t.Fatalf("TestFlatFileRollbackInTransactions: Close unexpectedly "+
+				"failed: %s", err)
+		}
+	}()
+
+	// Cast to ffldb since we're going to be messing with its internals
+	ffldbInstance, ok := db.(*ffldb)
+	if !ok {
+		t.Fatalf("TestRepairFlatFiles: unexpectedly can't cast " +
+			"db to ffldb")
+	}
+
+	storeName := "test"
+
+	// Append data1 to the store
+	_, err = ffldbInstance.AppendToStore(storeName, []byte("data1"))
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: AppendToStore unexpectedly "+
+			"failed: %s", err)
+	}
+
+	// Grab the current location to test against later
+	oldCurrentLocation, err := ffldbInstance.flatFileDB.CurrentLocation(storeName)
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: CurrentLocation "+
+			"unexpectedly failed: %s", err)
+	}
+
+	// Open a transaction
+	dbTx, err := db.Begin()
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: Begin "+
+			"unexpectedly failed: %s", err)
+	}
+	defer func() {
+		err := dbTx.RollbackUnlessClosed()
+		if err != nil {
+			t.Fatalf("TestFlatFileRollbackInTransactions: RollbackUnlessClosed "+
+				"unexpectedly failed: %s", err)
+		}
+	}()
+
+	// Append data2 to the store within the transaction
+	_, err = dbTx.AppendToStore(storeName, []byte("data2"))
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: AppendToStore unexpectedly "+
+			"failed: %s", err)
+	}
+
+	// Rollback the transaction
+	err = dbTx.Rollback()
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: Rollback "+
+			"unexpectedly failed: %s", err)
+	}
+
+	// Make sure that the current location rolled back as expected
+	currentLocation, err := ffldbInstance.flatFileDB.CurrentLocation(storeName)
+	if err != nil {
+		t.Fatalf("TestFlatFileRollbackInTransactions: CurrentLocation "+
+			"unexpectedly failed: %s", err)
+	}
+	if !reflect.DeepEqual(oldCurrentLocation, currentLocation) {
+		t.Fatalf("TestFlatFileRollbackInTransactions: currentLocation did " +
+			"not roll back")
+	}
+}
