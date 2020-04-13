c7071bf949d44eb6d672efff721c30bac75bb3ea
stasatdaglabs <stas@daglabs.com>
2020-04-13 11:30:53 +0300

    [NOD-863] Add tests that make sure that database changes don't affect previously opened transactions.

--- database/transaction_test.go
+++ database/transaction_test.go
@@ -72,9 +72,9 @@ func TestTransactionGet(t *testing.T) {
 
 func testTransactionGet(t *testing.T, db database.Database, testName string) {
 	// Put a value into the database
-	key := database.MakeBucket().Key([]byte("key"))
-	value := []byte("value")
-	err := db.Put(key, value)
+	key1 := database.MakeBucket().Key([]byte("key1"))
+	value1 := []byte("value1")
+	err := db.Put(key1, value1)
 	if err != nil {
 		t.Fatalf("%s: Put "+
 			"unexpectedly failed: %s", testName, err)
@@ -95,20 +95,40 @@ func testTransactionGet(t *testing.T, db database.Database, testName string) {
 	}()
 
 	// Get the value back and make sure it's the same one
-	returnedValue, err := dbTx.Get(key)
+	returnedValue, err := dbTx.Get(key1)
 	if err != nil {
 		t.Fatalf("%s: Get "+
 			"unexpectedly failed: %s", testName, err)
 	}
-	if !bytes.Equal(returnedValue, value) {
+	if !bytes.Equal(returnedValue, value1) {
 		t.Fatalf("%s: Get "+
 			"returned wrong value. Want: %s, got: %s",
-			testName, string(value), string(returnedValue))
+			testName, string(value1), string(returnedValue))
 	}
 
 	// Try getting a non-existent value and make sure
 	// the returned error is ErrNotFound
 	_, err = dbTx.Get(database.MakeBucket().Key([]byte("doesn't exist")))
+	if err == nil {
+		t.Fatalf("%s: Get "+
+			"unexpectedly succeeded", testName)
+	}
+	if !database.IsNotFoundError(err) {
+		t.Fatalf("%s: Get "+
+			"returned wrong error: %s", testName, err)
+	}
+
+	// Put a new value into the database outside of the transaction
+	key2 := database.MakeBucket().Key([]byte("key2"))
+	value2 := []byte("value2")
+	err = db.Put(key2, value2)
+	if err != nil {
+		t.Fatalf("%s: Put "+
+			"unexpectedly failed: %s", testName, err)
+	}
+
+	// Make sure that the new value doesn't exist inside the transaction
+	_, err = dbTx.Get(key2)
 	if err == nil {
 		t.Fatalf("%s: Get "+
 			"unexpectedly succeeded", testName)
@@ -125,9 +145,9 @@ func TestTransactionHas(t *testing.T) {
 
 func testTransactionHas(t *testing.T, db database.Database, testName string) {
 	// Put a value into the database
-	key := database.MakeBucket().Key([]byte("key"))
-	value := []byte("value")
-	err := db.Put(key, value)
+	key1 := database.MakeBucket().Key([]byte("key1"))
+	value1 := []byte("value1")
+	err := db.Put(key1, value1)
 	if err != nil {
 		t.Fatalf("%s: Put "+
 			"unexpectedly failed: %s", testName, err)
@@ -148,7 +168,7 @@ func testTransactionHas(t *testing.T, db database.Database, testName string) {
 	}()
 
 	// Make sure that Has returns true for the value we just put
-	exists, err := dbTx.Has(key)
+	exists, err := dbTx.Has(key1)
 	if err != nil {
 		t.Fatalf("%s: Has "+
 			"unexpectedly failed: %s", testName, err)
@@ -160,6 +180,26 @@ func testTransactionHas(t *testing.T, db database.Database, testName string) {
 
 	// Make sure that Has returns false for a non-existent value
 	exists, err = dbTx.Has(database.MakeBucket().Key([]byte("doesn't exist")))
+	if err != nil {
+		t.Fatalf("%s: Has "+
+			"unexpectedly failed: %s", testName, err)
+	}
+	if exists {
+		t.Fatalf("%s: Has "+
+			"unexpectedly returned that the value exists", testName)
+	}
+
+	// Put a new value into the database outside of the transaction
+	key2 := database.MakeBucket().Key([]byte("key2"))
+	value2 := []byte("value2")
+	err = db.Put(key2, value2)
+	if err != nil {
+		t.Fatalf("%s: Put "+
+			"unexpectedly failed: %s", testName, err)
+	}
+
+	// Make sure that the new value doesn't exist inside the transaction
+	exists, err = dbTx.Has(key2)
 	if err != nil {
 		t.Fatalf("%s: Has "+
 			"unexpectedly failed: %s", testName, err)
@@ -184,29 +224,39 @@ func testTransactionDelete(t *testing.T, db database.Database, testName string) 
 			"unexpectedly failed: %s", testName, err)
 	}
 
-	// Begin a new transaction
-	dbTx, err := db.Begin()
+	// Begin two new transactions
+	dbTx1, err := db.Begin()
+	if err != nil {
+		t.Fatalf("%s: Begin "+
+			"unexpectedly failed: %s", testName, err)
+	}
+	dbTx2, err := db.Begin()
 	if err != nil {
 		t.Fatalf("%s: Begin "+
 			"unexpectedly failed: %s", testName, err)
 	}
 	defer func() {
-		err := dbTx.RollbackUnlessClosed()
+		err := dbTx1.RollbackUnlessClosed()
+		if err != nil {
+			t.Fatalf("%s: RollbackUnlessClosed "+
+				"unexpectedly failed: %s", testName, err)
+		}
+		err = dbTx2.RollbackUnlessClosed()
 		if err != nil {
 			t.Fatalf("%s: RollbackUnlessClosed "+
 				"unexpectedly failed: %s", testName, err)
 		}
 	}()
 
-	// Delete the value in the transaction
-	err = dbTx.Delete(key)
+	// Delete the value in the first transaction
+	err = dbTx1.Delete(key)
 	if err != nil {
 		t.Fatalf("%s: Delete "+
 			"unexpectedly failed: %s", testName, err)
 	}
 
-	// Commit the transaction
-	err = dbTx.Commit()
+	// Commit the first transaction
+	err = dbTx1.Commit()
 	if err != nil {
 		t.Fatalf("%s: Commit "+
 			"unexpectedly failed: %s", testName, err)
@@ -221,6 +271,17 @@ func testTransactionDelete(t *testing.T, db database.Database, testName string) 
 	if exists {
 		t.Fatalf("%s: Has "+
 			"unexpectedly returned that the value exists", testName)
+	}
+
+	// Make sure that the second transaction was no affected
+	exists, err = dbTx2.Has(key)
+	if err != nil {
+		t.Fatalf("%s: Has "+
+			"unexpectedly failed: %s", testName, err)
+	}
+	if !exists {
+		t.Fatalf("%s: Has "+
+			"unexpectedly returned that the value does not exist", testName)
 	}
 }
 
