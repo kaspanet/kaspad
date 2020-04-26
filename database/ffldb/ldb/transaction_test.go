77ecd780544e46aa838400e687771bad656bc6b9
stasatdaglabs <stas@daglabs.com>
2020-04-26 10:18:36 +0300

    [NOD-849] Merge TestTransactionCommitErrors and TestTransactionRollbackErrors into TestTransactionCloseErrors.

--- database/ffldb/ldb/transaction_test.go
+++ database/ffldb/ldb/transaction_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestTransactionCommitErrors(t *testing.T) {
+func TestTransactionCloseErrors(t *testing.T) {
 	tests := []struct {
 		name string
 
@@ -73,163 +73,72 @@ func TestTransactionCommitErrors(t *testing.T) {
 
 	for _, test := range tests {
 		func() {
-			ldb, teardownFunc := prepareDatabaseForTest(t, "TestTransactionCommitErrors")
+			ldb, teardownFunc := prepareDatabaseForTest(t, "TestTransactionCloseErrors")
 			defer teardownFunc()
 
-			// Begin a new transaction
-			dbTx, err := ldb.Begin()
+			// Begin a new transaction to test Commit
+			commitTx, err := ldb.Begin()
 			if err != nil {
-				t.Fatalf("TestTransactionCommitErrors: Begin "+
+				t.Fatalf("TestTransactionCloseErrors: Begin "+
 					"unexpectedly failed: %s", err)
 			}
 			defer func() {
-				err := dbTx.RollbackUnlessClosed()
+				err := commitTx.RollbackUnlessClosed()
 				if err != nil {
-					t.Fatalf("TestTransactionCommitErrors: RollbackUnlessClosed "+
+					t.Fatalf("TestTransactionCloseErrors: RollbackUnlessClosed "+
 						"unexpectedly failed: %s", err)
 				}
 			}()
 
-			// Commit the transaction
-			err = dbTx.Commit()
+			// Commit the Commit test transaction
+			err = commitTx.Commit()
 			if err != nil {
-				t.Fatalf("TestTransactionCommitErrors: Commit "+
+				t.Fatalf("TestTransactionCloseErrors: Commit "+
+					"unexpectedly failed: %s", err)
+			}
+
+			// Begin a new transaction to test Rollback
+			rollbackTx, err := ldb.Begin()
+			if err != nil {
+				t.Fatalf("TestTransactionCloseErrors: Begin "+
+					"unexpectedly failed: %s", err)
+			}
+			defer func() {
+				err := rollbackTx.RollbackUnlessClosed()
+				if err != nil {
+					t.Fatalf("TestTransactionCloseErrors: RollbackUnlessClosed "+
+						"unexpectedly failed: %s", err)
+				}
+			}()
+
+			// Rollback the Rollback test transaction
+			err = rollbackTx.Rollback()
+			if err != nil {
+				t.Fatalf("TestTransactionCloseErrors: Rollback "+
 					"unexpectedly failed: %s", err)
 			}
 
 			expectedErrContainsString := "closed transaction"
 
 			// Make sure that the test function returns a "closed transaction" error
-			err = test.function(dbTx)
-			if test.shouldReturnError {
-				if err == nil {
-					t.Fatalf("TestTransactionCommitErrors: %s "+
-						"unexpectedly succeeded", test.name)
-				}
-				if !strings.Contains(err.Error(), expectedErrContainsString) {
-					t.Fatalf("TestTransactionCommitErrors: %s "+
-						"returned wrong error. Want: %s, got: %s",
-						test.name, expectedErrContainsString, err)
-				}
-			} else {
-				if err != nil {
-					t.Fatalf("TestTransactionCommitErrors: %s "+
-						"unexpectedly failed: %s", test.name, err)
-				}
-			}
-		}()
-	}
-}
-
-func TestTransactionRollbackErrors(t *testing.T) {
-	tests := []struct {
-		name string
-
-		// function is the LevelDBTransaction function that
-		// we're verifying whether it returns an error after
-		// the transaction had been closed.
-		function          func(dbTx *LevelDBTransaction) error
-		shouldReturnError bool
-	}{
-		{
-			name: "Put",
-			function: func(dbTx *LevelDBTransaction) error {
-				return dbTx.Put(database.MakeBucket().Key([]byte("key")), []byte("value"))
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Get",
-			function: func(dbTx *LevelDBTransaction) error {
-				_, err := dbTx.Get(database.MakeBucket().Key([]byte("key")))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Has",
-			function: func(dbTx *LevelDBTransaction) error {
-				_, err := dbTx.Has(database.MakeBucket().Key([]byte("key")))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Delete",
-			function: func(dbTx *LevelDBTransaction) error {
-				return dbTx.Delete(database.MakeBucket().Key([]byte("key")))
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Cursor",
-			function: func(dbTx *LevelDBTransaction) error {
-				_, err := dbTx.Cursor(database.MakeBucket([]byte("bucket")))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name:              "Rollback",
-			function:          (*LevelDBTransaction).Rollback,
-			shouldReturnError: true,
-		},
-		{
-			name:              "Commit",
-			function:          (*LevelDBTransaction).Commit,
-			shouldReturnError: true,
-		},
-		{
-			name:              "RollbackUnlessClosed",
-			function:          (*LevelDBTransaction).RollbackUnlessClosed,
-			shouldReturnError: false,
-		},
-	}
-
-	for _, test := range tests {
-		func() {
-			ldb, teardownFunc := prepareDatabaseForTest(t, "TestTransactionRollbackErrors")
-			defer teardownFunc()
-
-			// Begin a new transaction
-			dbTx, err := ldb.Begin()
-			if err != nil {
-				t.Fatalf("TestTransactionRollbackErrors: Begin "+
-					"unexpectedly failed: %s", err)
-			}
-			defer func() {
-				err := dbTx.RollbackUnlessClosed()
-				if err != nil {
-					t.Fatalf("TestTransactionRollbackErrors: RollbackUnlessClosed "+
-						"unexpectedly failed: %s", err)
-				}
-			}()
-
-			// Rollback the transaction
-			err = dbTx.Rollback()
-			if err != nil {
-				t.Fatalf("TestTransactionRollbackErrors: Rollback "+
-					"unexpectedly failed: %s", err)
-			}
-
-			expectedErrContainsString := "closed transaction"
-
-			// Make sure that the test function returns a "closed transaction" error
-			err = test.function(dbTx)
-			if test.shouldReturnError {
-				if err == nil {
-					t.Fatalf("TestTransactionRollbackErrors: %s "+
-						"unexpectedly succeeded", test.name)
-				}
-				if !strings.Contains(err.Error(), expectedErrContainsString) {
-					t.Fatalf("TestTransactionRollbackErrors: %s "+
-						"returned wrong error. Want: %s, got: %s",
-						test.name, expectedErrContainsString, err)
-				}
-			} else {
-				if err != nil {
-					t.Fatalf("TestTransactionRollbackErrors: %s "+
-						"unexpectedly failed: %s", test.name, err)
+			// for both the commitTx and the rollbackTx
+			for _, closedTx := range []*LevelDBTransaction{commitTx, rollbackTx} {
+				err = test.function(closedTx)
+				if test.shouldReturnError {
+					if err == nil {
+						t.Fatalf("TestTransactionCloseErrors: %s "+
+							"unexpectedly succeeded", test.name)
+					}
+					if !strings.Contains(err.Error(), expectedErrContainsString) {
+						t.Fatalf("TestTransactionCloseErrors: %s "+
+							"returned wrong error. Want: %s, got: %s",
+							test.name, expectedErrContainsString, err)
+					}
+				} else {
+					if err != nil {
+						t.Fatalf("TestTransactionCloseErrors: %s "+
+							"unexpectedly failed: %s", test.name, err)
+					}
 				}
 			}
 		}()
--- database/ffldb/transaction_test.go
+++ database/ffldb/transaction_test.go
@@ -279,7 +279,7 @@ func TestTransactionRollbackForLevelDBMethods(t *testing.T) {
 	}
 }
 
-func TestTransactionCommitErrors(t *testing.T) {
+func TestTransactionCloseErrors(t *testing.T) {
 	tests := []struct {
 		name              string
 		function          func(dbTx database.Transaction) error
@@ -364,181 +364,72 @@ func TestTransactionCommitErrors(t *testing.T) {
 
 	for _, test := range tests {
 		func() {
-			db, teardownFunc := prepareDatabaseForTest(t, "TestTransactionCommitErrors")
+			db, teardownFunc := prepareDatabaseForTest(t, "TestTransactionCloseErrors")
 			defer teardownFunc()
 
-			// Begin a new transaction
-			dbTx, err := db.Begin()
+			// Begin a new transaction to test Commit
+			commitTx, err := db.Begin()
 			if err != nil {
-				t.Fatalf("TestTransactionCommitErrors: Begin "+
+				t.Fatalf("TestTransactionCloseErrors: Begin "+
 					"unexpectedly failed: %s", err)
 			}
 			defer func() {
-				err := dbTx.RollbackUnlessClosed()
+				err := commitTx.RollbackUnlessClosed()
 				if err != nil {
-					t.Fatalf("TestTransactionCommitErrors: RollbackUnlessClosed "+
+					t.Fatalf("TestTransactionCloseErrors: RollbackUnlessClosed "+
 						"unexpectedly failed: %s", err)
 				}
 			}()
 
-			// Commit the transaction
-			err = dbTx.Commit()
+			// Commit the Commit test transaction
+			err = commitTx.Commit()
 			if err != nil {
-				t.Fatalf("TestTransactionCommitErrors: Commit "+
+				t.Fatalf("TestTransactionCloseErrors: Commit "+
+					"unexpectedly failed: %s", err)
+			}
+
+			// Begin a new transaction to test Rollback
+			rollbackTx, err := db.Begin()
+			if err != nil {
+				t.Fatalf("TestTransactionCloseErrors: Begin "+
+					"unexpectedly failed: %s", err)
+			}
+			defer func() {
+				err := rollbackTx.RollbackUnlessClosed()
+				if err != nil {
+					t.Fatalf("TestTransactionCloseErrors: RollbackUnlessClosed "+
+						"unexpectedly failed: %s", err)
+				}
+			}()
+
+			// Rollback the Rollback test transaction
+			err = rollbackTx.Rollback()
+			if err != nil {
+				t.Fatalf("TestTransactionCloseErrors: Rollback "+
 					"unexpectedly failed: %s", err)
 			}
 
 			expectedErrContainsString := "closed transaction"
 
 			// Make sure that the test function returns a "closed transaction" error
-			err = test.function(dbTx)
-			if test.shouldReturnError {
-				if err == nil {
-					t.Fatalf("TestTransactionCommitErrors: %s "+
-						"unexpectedly succeeded", test.name)
-				}
-				if !strings.Contains(err.Error(), expectedErrContainsString) {
-					t.Fatalf("TestTransactionCommitErrors: %s "+
-						"returned wrong error. Want: %s, got: %s",
-						test.name, expectedErrContainsString, err)
-				}
-			} else {
-				if err != nil {
-					t.Fatalf("TestTransactionCommitErrors: %s "+
-						"unexpectedly failed: %s", test.name, err)
-				}
-			}
-		}()
-	}
-}
-
-func TestTransactionRollbackErrors(t *testing.T) {
-	tests := []struct {
-		name              string
-		function          func(dbTx database.Transaction) error
-		shouldReturnError bool
-	}{
-		{
-			name: "Put",
-			function: func(dbTx database.Transaction) error {
-				return dbTx.Put(database.MakeBucket().Key([]byte("key")), []byte("value"))
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Get",
-			function: func(dbTx database.Transaction) error {
-				_, err := dbTx.Get(database.MakeBucket().Key([]byte("key")))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Has",
-			function: func(dbTx database.Transaction) error {
-				_, err := dbTx.Has(database.MakeBucket().Key([]byte("key")))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Delete",
-			function: func(dbTx database.Transaction) error {
-				return dbTx.Delete(database.MakeBucket().Key([]byte("key")))
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Cursor",
-			function: func(dbTx database.Transaction) error {
-				_, err := dbTx.Cursor(database.MakeBucket([]byte("bucket")))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "AppendToStore",
-			function: func(dbTx database.Transaction) error {
-				_, err := dbTx.AppendToStore("store", []byte("data"))
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "RetrieveFromStore",
-			function: func(dbTx database.Transaction) error {
-				_, err := dbTx.RetrieveFromStore("store", []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
-				return err
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Rollback",
-			function: func(dbTx database.Transaction) error {
-				return dbTx.Rollback()
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "Commit",
-			function: func(dbTx database.Transaction) error {
-				return dbTx.Commit()
-			},
-			shouldReturnError: true,
-		},
-		{
-			name: "RollbackUnlessClosed",
-			function: func(dbTx database.Transaction) error {
-				return dbTx.RollbackUnlessClosed()
-			},
-			shouldReturnError: false,
-		},
-	}
-
-	for _, test := range tests {
-		func() {
-			db, teardownFunc := prepareDatabaseForTest(t, "TestTransactionRollbackErrors")
-			defer teardownFunc()
-
-			// Begin a new transaction
-			dbTx, err := db.Begin()
-			if err != nil {
-				t.Fatalf("TestTransactionRollbackErrors: Begin "+
-					"unexpectedly failed: %s", err)
-			}
-			defer func() {
-				err := dbTx.RollbackUnlessClosed()
-				if err != nil {
-					t.Fatalf("TestTransactionRollbackErrors: RollbackUnlessClosed "+
-						"unexpectedly failed: %s", err)
-				}
-			}()
-
-			// Rollback the transaction
-			err = dbTx.Rollback()
-			if err != nil {
-				t.Fatalf("TestTransactionRollbackErrors: Rollback "+
-					"unexpectedly failed: %s", err)
-			}
-
-			expectedErrContainsString := "closed transaction"
-
-			// Make sure that the test function returns a "closed transaction" error
-			err = test.function(dbTx)
-			if test.shouldReturnError {
-				if err == nil {
-					t.Fatalf("TestTransactionRollbackErrors: %s "+
-						"unexpectedly succeeded", test.name)
-				}
-				if !strings.Contains(err.Error(), expectedErrContainsString) {
-					t.Fatalf("TestTransactionRollbackErrors: %s "+
-						"returned wrong error. Want: %s, got: %s",
-						test.name, expectedErrContainsString, err)
-				}
-			} else {
-				if err != nil {
-					t.Fatalf("TestTransactionRollbackErrors: %s "+
-						"unexpectedly failed: %s", test.name, err)
+			// for both the commitTx and the rollbackTx
+			for _, closedTx := range []database.Transaction{commitTx, rollbackTx} {
+				err = test.function(closedTx)
+				if test.shouldReturnError {
+					if err == nil {
+						t.Fatalf("TestTransactionCloseErrors: %s "+
+							"unexpectedly succeeded", test.name)
+					}
+					if !strings.Contains(err.Error(), expectedErrContainsString) {
+						t.Fatalf("TestTransactionCloseErrors: %s "+
+							"returned wrong error. Want: %s, got: %s",
+							test.name, expectedErrContainsString, err)
+					}
+				} else {
+					if err != nil {
+						t.Fatalf("TestTransactionCloseErrors: %s "+
+							"unexpectedly failed: %s", test.name, err)
+					}
 				}
 			}
 		}()
