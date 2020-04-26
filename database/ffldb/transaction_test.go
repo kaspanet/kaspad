d8b247626af5dcd61af312e9445f7ed581251da7
stasatdaglabs <stas@daglabs.com>
2020-04-26 10:23:14 +0300

    [NOD-849] Move prepareDatabaseForTest into ffldb_test.go.

--- database/ffldb/transaction_test.go
+++ database/ffldb/transaction_test.go
@@ -3,32 +3,9 @@ package ffldb
 import (
 	"bytes"
 	"github.com/kaspanet/kaspad/database"
-	"io/ioutil"
 	"strings"
 	"testing"
 )
-
-func prepareDatabaseForTest(t *testing.T, testName string) (db database.Database, teardownFunc func()) {
-	// Create a temp db to run tests against
-	path, err := ioutil.TempDir("", testName)
-	if err != nil {
-		t.Fatalf("%s: TempDir unexpectedly "+
-			"failed: %s", testName, err)
-	}
-	db, err = Open(path)
-	if err != nil {
-		t.Fatalf("%s: Open unexpectedly "+
-			"failed: %s", testName, err)
-	}
-	teardownFunc = func() {
-		err = db.Close()
-		if err != nil {
-			t.Fatalf("%s: Close unexpectedly "+
-				"failed: %s", testName, err)
-		}
-	}
-	return db, teardownFunc
-}
 
 func TestTransactionCommitForLevelDBMethods(t *testing.T) {
 	db, teardownFunc := prepareDatabaseForTest(t, "TestTransactionCommitForLevelDBMethods")
