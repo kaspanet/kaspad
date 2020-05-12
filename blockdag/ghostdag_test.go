c4ccf8c3ec4d3e1877044e1ea4909391a505d925
stasatdaglabs <39559713+stasatdaglabs@users.noreply.github.com>
2020-05-12 15:07:25 +0300

    [NOD-976] Implement tests for UTXO commitments (#715)
    * [NOD-975] Don't include block transactions inside its UTXO commitment.
    
    * Revert "[NOD-975] Don't include block transactions inside its UTXO commitment."
    
    This reverts commit b1a2ae66
    
    * [NOD-975] Implement a (currently failing) TestUTXOCommitment.
    
    * [NOD-975] Remove the block's own transactions from calcMultiset.
    
    * [NOD-975] Simplify calcMultiset.
    
    * [NOD-975] Add a comment on top of selectedParentMultiset.
    
    * [NOD-975] Use pastUTXO instead of selectedParentUTXO in calcMultiset.
    
    * [NOD-975] Use selected parent's pastUTXO instead of this block's pastUTXO in calcMultiset.
    
    * [NOD-975] Extract selectedParentPastUTXO to a separate function.
    
    * [NOD-975] Remove selectedParentUTXO from pastUTXO's return values.
    
    * [NOD-975] Add txs to TestUTXOCommitment.
    
    * [NOD-976] Generate new blockDB blocks for tests.
    
    * [NOD-976] Fix TestBlueBlockWindow.
    
    * [NOD-976] Fix TestIsKnownBlock.
    
    * [NOD-976] Fix TestGHOSTDAG.
    
    * [NOD-976] Fix TestUTXOCommitment.
    
    * [NOD-976] Remove kaka.

--- blockdag/ghostdag_test.go
+++ blockdag/ghostdag_test.go
@@ -33,7 +33,7 @@ func TestGHOSTDAG(t *testing.T) {
 	}{
 		{
 			k:            3,
-			expectedReds: []string{"F", "G", "H", "I", "N", "Q"},
+			expectedReds: []string{"F", "G", "H", "I", "O", "Q"},
 			dagData: []*testBlockData{
 				{
 					parents:                []string{"A"},
@@ -166,7 +166,7 @@ func TestGHOSTDAG(t *testing.T) {
 					id:                     "T",
 					expectedScore:          13,
 					expectedSelectedParent: "S",
-					expectedBlues:          []string{"S", "O", "P"},
+					expectedBlues:          []string{"S", "N", "P"},
 				},
 			},
 		},
