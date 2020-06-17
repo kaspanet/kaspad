7d44c6f886938d7b8eaf780c13b57459c2867195
Ori Newman <orinewman1@gmail.com>
2020-06-17 11:29:39 +0300

    [NOD-1055] Give higher priority for requesting missing ancestors when sending a getdata message (#767)

--- peer/message_logging.go
+++ peer/message_logging.go
@@ -46,6 +46,8 @@ func invSummary(invList []*wire.InvVect) string {
 			return fmt.Sprintf("block %s", iv.Hash)
 		case wire.InvTypeSyncBlock:
 			return fmt.Sprintf("sync block %s", iv.Hash)
+		case wire.InvTypeMissingAncestor:
+			return fmt.Sprintf("missing ancestor %s", iv.Hash)
 		case wire.InvTypeTx:
 			return fmt.Sprintf("tx %s", iv.Hash)
 		}
