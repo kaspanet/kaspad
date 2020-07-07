cd7d50b66e6a18b5304551abea9ebca11fd4a023
stasatdaglabs <stas@daglabs.com>
2020-07-07 11:57:36 +0300

    [NOD-1124] Implementing a stub gRPC server..

--- netadapter/server/grpc/grpcserver.go
+++ netadapter/server/grpc/grpcserver.go
@@ -1,1 +1,52 @@
 package grpc
+
+import (
+	"fmt"
+	"github.com/kaspanet/kaspad/netadapter/server"
+	"github.com/kaspanet/kaspad/wire"
+)
+
+type gRPCServer struct {
+	connections []server.Connection
+}
+
+// newGRPCServer creates and starts a gRPC server with the given
+// listening port
+func newGRPCServer(listeningPort string) (server.Server, error) {
+	fmt.Printf("Listening on 127.0.0.1:%s\n", listeningPort)
+
+	return &gRPCServer{}, nil
+}
+
+// Connect connects to the given address
+// This is part of the Server interface
+func (s *gRPCServer) Connect(address string) (server.Connection, error) {
+	return &gRPCConnection{}, nil
+}
+
+// Connections returns a slice of connections the server
+// is currently connected to.
+// This is part of the Server interface
+func (s *gRPCServer) Connections() []server.Connection {
+	return s.connections
+}
+
+type gRPCConnection struct{}
+
+// Send sends the given message through the connection
+// This is part of the Connection interface
+func (c *gRPCConnection) Send(message wire.Message) error {
+	return nil
+}
+
+// Receive receives the next message from the connection
+// This is part of the Connection interface
+func (c *gRPCConnection) Receive() (wire.Message, error) {
+	return nil, nil
+}
+
+// Disconnect disconnects the connection
+// This is part of the Connection interface
+func (c *gRPCConnection) Disconnect() error {
+	return nil
+}
