5ab8c65162c8a0dce7e484d83e464d3d942b8221
stasatdaglabs <stas@daglabs.com>
2020-07-07 17:17:29 +0300

    [NOD-1124] Panic/Add TODOs everywhere that isn't implemented.

--- netadapter/server/grpcserver/grpcserver.go
+++ netadapter/server/grpcserver/grpcserver.go
@@ -7,13 +7,16 @@ import (
 )
 
 type gRPCServer struct {
-	newConnectionHandler func(connection server.Connection)
+	newConnectionHandler server.NewConnectionHandler
 	connections          []server.Connection
 }
 
 // NewGRPCServer creates and starts a gRPC server with the given
 // listening port
 func NewGRPCServer(listeningPort string) (server.Server, error) {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	fmt.Printf("Listening on 127.0.0.1:%s\n", listeningPort)
 
 	return &gRPCServer{}, nil
@@ -21,13 +24,16 @@ func NewGRPCServer(listeningPort string) (server.Server, error) {
 
 // SetNewConnectionHandler sets the new connection handler
 // function for the server
-func (s *gRPCServer) SetNewConnectionHandler(handleNewConnection func(connection server.Connection)) {
-	s.newConnectionHandler = handleNewConnection
+func (s *gRPCServer) SetNewConnectionHandler(newConnectionHandler server.NewConnectionHandler) {
+	s.newConnectionHandler = newConnectionHandler
 }
 
 // Connect connects to the given address
 // This is part of the Server interface
 func (s *gRPCServer) Connect(address string) (server.Connection, error) {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	connection := gRPCConnection{}
 	s.newConnectionHandler(&connection)
 	return &connection, nil
@@ -37,10 +43,16 @@ func (s *gRPCServer) Connect(address string) (server.Connection, error) {
 // is currently connected to.
 // This is part of the Server interface
 func (s *gRPCServer) Connections() []server.Connection {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	return s.connections
 }
 
 func (s *gRPCServer) Close() error {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	for _, connection := range s.connections {
 		err := connection.Disconnect()
 		if err != nil {
@@ -55,17 +67,26 @@ type gRPCConnection struct{}
 // Send sends the given message through the connection
 // This is part of the Connection interface
 func (c *gRPCConnection) Send(message wire.Message) error {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	return nil
 }
 
 // Receive receives the next message from the connection
 // This is part of the Connection interface
 func (c *gRPCConnection) Receive() (wire.Message, error) {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	return nil, nil
 }
 
 // Disconnect disconnects the connection
 // This is part of the Connection interface
 func (c *gRPCConnection) Disconnect() error {
+	// TODO(libp2p): unimplemented
+	panic("unimplemented")
+
 	return nil
 }
