860618d4fc272e9d5c0472c0492febdc3452264d
Ori Newman <orinewman1@gmail.com>
2020-07-12 11:05:29 +0300

    [NOD-1126] Refactor protocol.go

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -45,7 +45,7 @@ func newRouterInitializer(netAdapter *netadapter.NetAdapter, dag *blockdag.Block
 	return func() (*netadapter.Router, error) {
 		router := netadapter.NewRouter()
 		spawn(func() {
-			err := startFlow(netAdapter, router, dag)
+			err := startFlows(netAdapter, router, dag)
 			if err != nil {
 				// TODO(libp2p) Ban peer
 			}
@@ -54,46 +54,40 @@ func newRouterInitializer(netAdapter *netadapter.NetAdapter, dag *blockdag.Block
 	}
 }
 
-func startFlow(netAdapter *netadapter.NetAdapter, router *netadapter.Router, dag *blockdag.BlockDAG) error {
+func startFlows(netAdapter *netadapter.NetAdapter, router *netadapter.Router, dag *blockdag.BlockDAG) error {
 	stop := make(chan error)
-	shutdown := uint32(0)
+	stopped := uint32(0)
 
 	peer := new(peerpkg.Peer)
 
-	blockRelayCh := make(chan wire.Message)
-	err := router.AddRoute([]string{wire.CmdInvRelayBlock, wire.CmdBlock}, blockRelayCh)
-	if err != nil {
-		panic(err)
-	}
-
-	spawn(func() {
-		err := blockrelay.StartBlockRelay(blockRelayCh, peer, netAdapter, router, dag)
-		if err != nil {
-			log.Errorf("error from StartBlockRelay: %s", err)
-		}
-
-		if atomic.AddUint32(&shutdown, 1) == 1 {
-			stop <- err
-		}
+	addFlow("blockrelay", router, []string{wire.CmdInvRelayBlock, wire.CmdBlock}, &stopped, stop, func(ch chan wire.Message) error {
+		return blockrelay.StartBlockRelay(ch, peer, netAdapter, router, dag)
 	})
 
-	getRelayBlocksListenerCh := make(chan wire.Message)
-	err = router.AddRoute([]string{wire.CmdGetRelayBlocks}, getRelayBlocksListenerCh)
-	if err != nil {
-		panic(err)
-	}
-
-	spawn(func() {
-		err := getrelayblockslistener.StartGetRelayBlocksListener(getRelayBlocksListenerCh, router, dag)
-		if err != nil {
-			log.Errorf("error from StartGetRelayBlocksListener: %s", err)
-		}
-
-		if atomic.AddUint32(&shutdown, 1) == 1 {
-			stop <- err
-		}
+	addFlow("getrelayblocks", router, []string{wire.CmdGetRelayBlocks}, &stopped, stop, func(ch chan wire.Message) error {
+		return getrelayblockslistener.StartGetRelayBlocksListener(ch, router, dag)
 	})
 
-	err = <-stop
+	err := <-stop
 	return err
+}
+
+func addFlow(name string, router *netadapter.Router, messageTypes []string, stopped *uint32,
+	stopChan chan error, flow func(ch chan wire.Message) error) {
+
+	ch := make(chan wire.Message)
+	err := router.AddRoute(messageTypes, ch)
+	if err != nil {
+		panic(err)
+	}
+
+	spawn(func() {
+		err := flow(ch)
+		if err != nil {
+			log.Errorf("error from %s flow: %s", name, err)
+		}
+		if atomic.AddUint32(stopped, 1) == 1 {
+			stopChan <- err
+		}
+	})
 }
