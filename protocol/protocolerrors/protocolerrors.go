adf5e709ed2474c860f94acbb3d74504b5b76e42
Ori Newman <orinewman1@gmail.com>
2020-07-14 16:32:47 +0300

    [NOD-1137] Add comments

--- protocol/protocolerrors/protocolerrors.go
+++ protocol/protocolerrors/protocolerrors.go
@@ -2,6 +2,8 @@ package protocolerrors
 
 import "github.com/pkg/errors"
 
+// ProtocolError is an error that signifies a violation
+// of the peer-to-peer protocol
 type ProtocolError struct {
 	ShouldBan bool
 	Cause     error
@@ -15,6 +17,9 @@ func (e *ProtocolError) Unwrap() error {
 	return e.Cause
 }
 
+// Errorf formats according to a format specifier and returns the string
+// as a value that satisfies error.
+// Errorf also records the stack trace at the point it was called.
 func Errorf(shouldBan bool, format string, args ...interface{}) error {
 	return &ProtocolError{
 		ShouldBan: shouldBan,
@@ -22,6 +27,8 @@ func Errorf(shouldBan bool, format string, args ...interface{}) error {
 	}
 }
 
+// New returns an error with the supplied message.
+// New also records the stack trace at the point it was called.
 func New(shouldBan bool, message string) error {
 	return &ProtocolError{
 		ShouldBan: shouldBan,
@@ -29,6 +36,8 @@ func New(shouldBan bool, message string) error {
 	}
 }
 
+// Wrap returns an error annotating err with a stack trace
+// at the point Wrap is called, and the supplied message.
 func Wrap(shouldBan bool, err error, message string) error {
 	return &ProtocolError{
 		ShouldBan: shouldBan,
@@ -36,6 +45,8 @@ func Wrap(shouldBan bool, err error, message string) error {
 	}
 }
 
+// Wrapf returns an error annotating err with a stack trace
+// at the point Wrapf is called, and the format specifier.
 func Wrapf(shouldBan bool, err error, format string, args ...interface{}) error {
 	return &ProtocolError{
 		ShouldBan: shouldBan,
