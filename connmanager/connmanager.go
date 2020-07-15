315b383910f82576cfbd3439acd876f7bd812709
Mike Zak <feanorr@gmail.com>
2020-07-15 14:32:43 +0300

    [NOD-1120] Moved checkIncomingConnections to it's own file

--- connmanager/connmanager.go
+++ connmanager/connmanager.go
@@ -124,21 +124,3 @@ func (c *ConnectionManager) connectionsLoop() {
 		<-time.Tick(connectionsLoopInterval)
 	}
 }
-
-// checkIncomingConnections makes sure there's no more then maxIncoming incoming connections
-// if there are - it randomly disconnects enough to go below that number
-func (c *ConnectionManager) checkIncomingConnections(connSet connectionSet) {
-	if len(connSet) <= c.maxIncoming {
-		return
-	}
-
-	// randomly disconnect nodes until the number of incoming connections is smaller the maxIncoming
-	for address, connection := range connSet {
-		err := connection.Disconnect()
-		if err != nil {
-			log.Errorf("Error disconnecting from %s: %+v", address, err)
-		}
-
-		connSet.remove(connection)
-	}
-}
