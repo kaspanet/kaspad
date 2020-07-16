232f859ea530b1c3436a686b486e7f54b88a01e7
Mike Zak <feanorr@gmail.com>
2020-07-16 16:43:09 +0300

    [NOD-1120] Rename connSet -> incomingConnectionSet

--- connmanager/incoming_connections.go
+++ connmanager/incoming_connections.go
@@ -2,14 +2,14 @@ package connmanager
 
 // checkIncomingConnections makes sure there's no more then maxIncoming incoming connections
 // if there are - it randomly disconnects enough to go below that number
-func (c *ConnectionManager) checkIncomingConnections(connSet connectionSet) {
-	if len(connSet) <= c.maxIncoming {
+func (c *ConnectionManager) checkIncomingConnections(incomingConnectionSet connectionSet) {
+	if len(incomingConnectionSet) <= c.maxIncoming {
 		return
 	}
 
-	numConnectionsOverMax := len(connSet) - c.maxIncoming
-	// randomly disconnect nodes until the number of incoming connections is smaller the maxIncoming
-	for address, connection := range connSet {
+	numConnectionsOverMax := len(incomingConnectionSet) - c.maxIncoming
+	// randomly disconnect nodes until the number of incoming connections is smaller than maxIncoming
+	for address, connection := range incomingConnectionSet {
 		err := c.netAdapter.Disconnect(connection)
 		if err != nil {
 			log.Errorf("Error disconnecting from %s: %+v", address, err)
