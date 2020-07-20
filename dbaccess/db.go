5cfaba10301f7b66d943b3e26b4c1a8fe93e20e7
Mike Zak <feanorr@gmail.com>
2020-07-20 15:03:14 +0300

    [NOD-1164] Defined DatabaseContext as the basic object of dbaccess

--- dbaccess/db.go
+++ dbaccess/db.go
@@ -3,41 +3,26 @@ package dbaccess
 import (
 	"github.com/kaspanet/kaspad/database"
 	"github.com/kaspanet/kaspad/database/ffldb"
-	"github.com/pkg/errors"
 )
 
-// dbSingleton is a handle to an instance of the kaspad database
-var dbSingleton database.Database
-
-// db returns a handle to the database
-func db() (database.Database, error) {
-	if dbSingleton == nil {
-		return nil, errors.New("database is not open")
-	}
-	return dbSingleton, nil
+type DatabaseContext struct {
+	db database.Database
+	*noTxContext
 }
 
-// Open opens the database for given path
-func Open(path string) error {
-	if dbSingleton != nil {
-		return errors.New("database is already open")
-	}
-
+func New(path string) (*DatabaseContext, error) {
 	db, err := ffldb.Open(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	dbSingleton = db
-	return nil
+	databaseContext := &DatabaseContext{db: db}
+	databaseContext.noTxContext = &noTxContext{backend: databaseContext}
+
+	return databaseContext, nil
 }
 
 // Close closes the database, if it's open
-func Close() error {
-	if dbSingleton == nil {
-		return nil
-	}
-	err := dbSingleton.Close()
-	dbSingleton = nil
-	return err
+func (ctx *DatabaseContext) Close() error {
+	return ctx.db.Close()
 }
