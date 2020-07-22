ff0204aeb227f79c78051a96be58838395f9d08e
stasatdaglabs <stas@daglabs.com>
2020-07-22 11:45:26 +0300

    [NOD-1175] Get rid of something weird.

--- rpc/handle_submit_block.go
+++ rpc/handle_submit_block.go
@@ -13,9 +13,6 @@ func handleSubmitBlock(s *Server, cmd interface{}, closeChan <-chan struct{}) (i
 
 	// Deserialize the submitted block.
 	hexStr := c.HexBlock
-	if len(hexStr)%2 != 0 {
-		hexStr = "0" + c.HexBlock
-	}
 	serializedBlock, err := hex.DecodeString(hexStr)
 	if err != nil {
 		return nil, rpcDecodeHexError(hexStr)
