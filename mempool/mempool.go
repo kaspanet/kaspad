e7bd1109387b1b64363144898241b56d7b30b700
Mike Zak <feanorr@gmail.com>
2020-08-12 11:17:29 +0300

    [NOD-1259] Handle submitting of coinbase transactions properly

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -851,7 +851,7 @@ func (mp *TxPool) maybeAcceptTransaction(tx *util.Tx, rejectDupOrphans bool) ([]
 
 	// Check that transaction does not overuse GAS
 	msgTx := tx.MsgTx()
-	if !msgTx.SubnetworkID.IsEqual(subnetworkid.SubnetworkIDNative) && !msgTx.SubnetworkID.IsEqual(subnetworkid.SubnetworkIDRegistry) {
+	if !msgTx.SubnetworkID.IsBuiltIn() {
 		gasLimit, err := mp.cfg.DAG.GasLimit(&msgTx.SubnetworkID)
 		if err != nil {
 			return nil, nil, err
