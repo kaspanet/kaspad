2cbab65931afe23f36eae12d92d3748e8c82682b
Ori Newman <orinewman1@gmail.com>
2020-08-23 18:15:09 +0300

    [NOD-1301] Add MsgReject to protowire mapping

--- infrastructure/network/netadapter/server/grpcserver/protowire/message_reject.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/message_reject.go
@@ -4,13 +4,13 @@ import (
 	"github.com/kaspanet/kaspad/app/appmessage"
 )
 
-func (x *KaspadMessage_Reject) toWireMessage() (appmessage.Message, error) {
+func (x *KaspadMessage_Reject) toAppMessage() (appmessage.Message, error) {
 	return &appmessage.MsgReject{
 		Reason: x.Reject.Reason,
 	}, nil
 }
 
-func (x *KaspadMessage_Reject) fromWireMessage(msgReject *appmessage.MsgReject) error {
+func (x *KaspadMessage_Reject) fromAppMessage(msgReject *appmessage.MsgReject) error {
 	x.Reject = &RejectMessage{
 		Reason: msgReject.Reason,
 	}
