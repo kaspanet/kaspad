a991c32b71bc19369e981dfe2d0567c45a27f0a2
stasatdaglabs <stas@daglabs.com>
2020-08-30 16:48:30 +0300

    [NOD-1319] Add a block added listener stub.

--- app/rpc/rpchandlers/notify_block_added.go
+++ app/rpc/rpchandlers/notify_block_added.go
@@ -4,10 +4,21 @@ import (
 	"github.com/kaspanet/kaspad/app/appmessage"
 	"github.com/kaspanet/kaspad/app/rpc/rpccontext"
 	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
+	"github.com/kaspanet/kaspad/util"
 )
 
 // HandleNotifyBlockAdded handles the respectively named RPC command
 func HandleNotifyBlockAdded(context *rpccontext.Context, router *router.Router, _ appmessage.Message) (appmessage.Message, error) {
+	listener, err := context.NotificationManager.Listener(router)
+	if err != nil {
+		return nil, err
+	}
+	listener.SetOnBlockAddedListener(func(block *util.Block) error {
+		//notification := NewBlockAddedNotification()
+		//router.OutgoingRoute().Enqueue()
+		return nil
+	})
+
 	response := appmessage.NewNotifyBlockAddedResponseMessage()
 	return response, nil
 }
