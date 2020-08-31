eab10b2ab4c773cd05b985ddbd82b57c8c04dbff
stasatdaglabs <stas@daglabs.com>
2020-08-31 10:39:23 +0300

    [NOD-1319] Remove unused flags in kaspactl2.

--- cmd/kaspactl2/config.go
+++ cmd/kaspactl2/config.go
@@ -12,11 +12,7 @@ var (
 )
 
 type configFlags struct {
-	RPCUser     string `short:"u" long:"rpcuser" description:"RPC username"`
-	RPCPassword string `short:"P" long:"rpcpass" default-mask:"-" description:"RPC password"`
 	RPCServer   string `short:"s" long:"rpcserver" description:"RPC server to connect to"`
-	RPCCert     string `short:"c" long:"rpccert" description:"RPC server certificate chain for validation"`
-	DisableTLS  bool   `long:"notls" description:"Disable TLS"`
 	RequestJSON string `description:"The request in JSON format"`
 	config.NetworkFlags
 }
@@ -34,20 +30,6 @@ func parseConfig() (*configFlags, error) {
 	err = cfg.ResolveNetwork(parser)
 	if err != nil {
 		return nil, err
-	}
-
-	if cfg.RPCUser == "" {
-		return nil, errors.New("--rpcuser is required")
-	}
-	if cfg.RPCPassword == "" {
-		return nil, errors.New("--rpcpass is required")
-	}
-
-	if cfg.RPCCert == "" && !cfg.DisableTLS {
-		return nil, errors.New("either --notls or --rpccert must be specified")
-	}
-	if cfg.RPCCert != "" && cfg.DisableTLS {
-		return nil, errors.New("--rpccert should be omitted if --notls is used")
 	}
 
 	if len(args) != 1 {
