0bcfa503c1b08d8a8c0ff301771601fa3160592a
Karim <karimkaspersky@yahoo.com>
2020-09-04 02:49:43 +0300

    [NOD-1344]  Add mid-complexity unit tests for relaytransactions * Improve TestHandleRelayedTransactionssub tests * Improve TestHandleRequestedTransactions sub tests

--- app/protocol/flows/relaytransactions/relaytransactions_test.go
+++ app/protocol/flows/relaytransactions/relaytransactions_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/kaspanet/kaspad/infrastructure/db/dbaccess"
 	"github.com/kaspanet/kaspad/infrastructure/network/netadapter"
 	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
+	"github.com/kaspanet/kaspad/util/daghash"
 )
 
 type mocTransactionsRelayContext struct {
@@ -51,7 +52,7 @@ func newMocTransactionsRelayContext(dag *blockdag.BlockDAG) *mocTransactionsRela
 
 	return &mocTransactionsRelayContext{
 		netAdapter:                  adapter,
-		dag:                         nil,
+		dag:                         dag,
 		txPool:                      txPool,
 		sharedRequestedTransactions: sharedRequestedTransactions,
 	}
@@ -99,54 +100,148 @@ func createMempoolConfig(dag *blockdag.BlockDAG) *mempool.Config {
 }
 
 func TestHandleRelayedTransactions(t *testing.T) {
-	incomingRoute := router.NewRoute()
-	outgoingRoute := router.NewRoute()
-	dag, cleanDag := createDag()
-	defer cleanDag()
+	params := dagconfig.SimnetParams
+	dagConfig := blockdag.Config{DAGParams: &params}
+	dag, teardownFunc, err := blockdag.DAGSetup("TestHandleRelayedTransactions", true, dagConfig)
 
-	t.Run("Test for invalid arguments 1", func(t *testing.T) {
-		context := newMocTransactionsRelayContext(dag)
-		incomingRoute.Enqueue(&appmessage.MsgAddresses{})
-		HandleRelayedTransactions(context, incomingRoute, outgoingRoute)
-	})
+	if err != nil {
+		t.Fatalf("Failed to setup DAG instance: %v", err)
+	}
+	defer teardownFunc()
 
-	t.Run("Test for invalid arguments 2", func(t *testing.T) {
+	t.Run("Simple call test", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
 		context := newMocTransactionsRelayContext(dag)
 		incomingRoute.Enqueue(&appmessage.MsgInvTransaction{})
 		incomingRoute.Enqueue(&appmessage.MsgAddresses{})
 		HandleRelayedTransactions(context, incomingRoute, outgoingRoute)
 	})
 
-	t.Run("Test for invalid arguments 3", func(t *testing.T) {
+	t.Run("Test on wrong message type", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
+		context := newMocTransactionsRelayContext(dag)
+		incomingRoute.Enqueue(&appmessage.MsgAddresses{})
+		HandleRelayedTransactions(context, incomingRoute, outgoingRoute)
+	})
+
+	t.Run("Test on closed route", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
 		context := newMocTransactionsRelayContext(dag)
 		incomingRoute.Close()
+		HandleRelayedTransactions(context, incomingRoute, outgoingRoute)
+	})
+
+	t.Run("Test Outgoing Message", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
+		context := newMocTransactionsRelayContext(dag)
+
+		txID1 := &daghash.TxID{1, 2, 3}
+		txID2 := &daghash.TxID{3, 4, 5}
+		txIDs := []*daghash.TxID{txID1, txID2}
+		msg := appmessage.NewMsgInvTransaction(txIDs)
+
+		go func() {
+			defer func() {
+				incomingRoute.Close()
+				outgoingRoute.Close()
+			}()
+
+			msg, err := outgoingRoute.Dequeue()
+			if err != nil {
+				t.Fatalf("TestHandleRelayedTransactions: %s", err)
+			}
+
+			inv := msg.(*appmessage.MsgRequestTransactions)
+
+			if len(txIDs) != len(inv.IDs) {
+				t.Fatalf("TestHandleRelayedTransactions: expected %d got %d", len(txIDs), len(inv.IDs))
+			}
+
+			for i, id := range inv.IDs {
+				if txIDs[i].String() != id.String() {
+					t.Fatalf("TestHandleRelayedTransactions: expected equal txID  %s != %s", txIDs[i].String(), id.String())
+				}
+
+				incomingRoute.Enqueue(appmessage.NewMsgTransactionNotFound(id))
+			}
+		}()
+
+		incomingRoute.Enqueue(msg)
 		HandleRelayedTransactions(context, incomingRoute, outgoingRoute)
 	})
 }
 
 func TestHandleRequestedTransactions(t *testing.T) {
-	incomingRoute := router.NewRoute()
-	outgoingRoute := router.NewRoute()
-	dag, cleanDag := createDag()
-	defer cleanDag()
+	params := dagconfig.SimnetParams
+	dagConfig := blockdag.Config{DAGParams: &params}
+	dag, teardownFunc, err := blockdag.DAGSetup("TestHandleRequestedTransactions", true, dagConfig)
 
-	t.Run("Test for invalid arguments 1", func(t *testing.T) {
-		context := newMocTransactionsRelayContext(dag)
-		incomingRoute.Enqueue(&appmessage.MsgAddresses{})
-		HandleRequestedTransactions(context, incomingRoute, outgoingRoute)
-	})
+	if err != nil {
+		t.Fatalf("Failed to setup DAG instance: %v", err)
+	}
+	defer teardownFunc()
 
-	t.Run("Test for invalid arguments 2", func(t *testing.T) {
+	t.Run("Simple call test", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
 		context := newMocTransactionsRelayContext(dag)
 		incomingRoute.Enqueue(&appmessage.MsgRequestTransactions{})
 		incomingRoute.Enqueue(&appmessage.MsgAddresses{})
 		HandleRequestedTransactions(context, incomingRoute, outgoingRoute)
 	})
 
-	t.Run("Test for invalid arguments 3", func(t *testing.T) {
-		context := newMocTransactionsRelayContext(nil)
-		incomingRoute.Enqueue(&appmessage.MsgRequestTransactions{})
+	t.Run("Test on wrong message type", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
+		context := newMocTransactionsRelayContext(dag)
 		incomingRoute.Enqueue(&appmessage.MsgAddresses{})
+		HandleRequestedTransactions(context, incomingRoute, outgoingRoute)
+	})
+
+	t.Run("Test on closed route", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
+		context := newMocTransactionsRelayContext(dag)
+		incomingRoute.Close()
+		outgoingRoute.Close()
+		HandleRequestedTransactions(context, incomingRoute, outgoingRoute)
+	})
+
+	t.Run("Test Outgoing Message", func(t *testing.T) {
+		incomingRoute := router.NewRoute()
+		outgoingRoute := router.NewRoute()
+		context := newMocTransactionsRelayContext(dag)
+
+		txID1 := &daghash.TxID{1, 2, 3}
+		txID2 := &daghash.TxID{3, 4, 5}
+		txIDs := []*daghash.TxID{txID1, txID2}
+		msg := appmessage.NewMsgRequestTransactions(txIDs)
+
+		go func() {
+			defer func() {
+				incomingRoute.Close()
+				outgoingRoute.Close()
+			}()
+
+			for i, id := range txIDs {
+				msg, err := outgoingRoute.Dequeue()
+				if err != nil {
+					t.Fatalf("TestHandleRequestedTransactions: %s", err)
+				}
+
+				outMsg := msg.(*appmessage.MsgTransactionNotFound)
+
+				if txIDs[i].String() != outMsg.ID.String() {
+					t.Fatalf("TestHandleRequestedTransactions: expected equal txID  %s != %s", txIDs[i].String(), id.String())
+				}
+			}
+		}()
+
+		incomingRoute.Enqueue(msg)
 		HandleRequestedTransactions(context, incomingRoute, outgoingRoute)
 	})
 }
