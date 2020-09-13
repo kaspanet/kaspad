48b6773b3c6ccafafb06dd341cf78e84fa63bb2b
stasatdaglabs <stas@daglabs.com>
2020-09-13 16:06:41 +0300

    [NOD-1032] Remove irrelevant comments.

--- infrastructure/network/rpc/model/rpc_websocket_commands.go
+++ infrastructure/network/rpc/model/rpc_websocket_commands.go
@@ -9,11 +9,3 @@ package model
 //	return &NotifyFinalityConflictCmd{}
 //}
 //
-//// StopNotifyFinalityConflictCmd defines the stopNotifyFinalityConflicts JSON-RPC command.
-//type StopNotifyFinalityConflictCmd struct{}
-//
-//// NewStopNotifyFinalityConflictsCmd returns a new instance which can be used to issue
-//// a stopNotifyFinalityConflicts JSON-RPC command.
-//func NewStopNotifyFinalityConflictsCmd() *NotifyFinalityConflictCmd {
-//	return &NotifyFinalityConflictCmd{}
-//}
