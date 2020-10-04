7d355cf0d2b1efd6b4a799d3593adcc32df7a399
stasatdaglabs <stas@daglabs.com>
2020-10-04 13:05:39 +0300

    [NOD-1414] Add Entry() to ReadOnlyUTXOSet.

--- domain/kaspadstate/model/readonlyutxoset.go
+++ domain/kaspadstate/model/readonlyutxoset.go
@@ -1,11 +1,15 @@
 package model
 
+import "github.com/kaspanet/kaspad/app/appmessage"
+
 // ReadOnlyUTXOSet ...
 type ReadOnlyUTXOSet interface {
 	Iterator() ReadOnlyUTXOSetIterator
+	Entry(outpoint *appmessage.Outpoint) *UTXOEntry
 }
 
 // ReadOnlyUTXOSetIterator ...
 type ReadOnlyUTXOSetIterator interface {
 	Next() bool
+	Get() (outpoint *appmessage.Outpoint, utxoEntry *UTXOEntry)
 }
