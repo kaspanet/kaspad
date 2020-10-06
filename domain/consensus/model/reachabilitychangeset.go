b891de6797a8f40e50e4a4744fe1ef8f2ebfab8e
stasatdaglabs <stas@daglabs.com>
2020-10-06 10:25:41 +0300

    [NOD-1414] Make reachability structs public.

--- domain/consensus/model/reachabilitychangeset.go
+++ domain/consensus/model/reachabilitychangeset.go
@@ -5,6 +5,6 @@ import "github.com/kaspanet/kaspad/util/daghash"
 // ReachabilityChangeset holds the set of changes to make to a
 // reachability tree to insert a new reachability node
 type ReachabilityChangeset struct {
-	treeNodeChanges          map[*daghash.Hash]*reachabilityTreeNode
-	futureCoveringSetChanges map[*daghash.Hash]futureCoveringTreeNodeSet
+	TreeNodeChanges          map[*daghash.Hash]*ReachabilityTreeNode
+	FutureCoveringSetChanges map[*daghash.Hash]FutureCoveringTreeNodeSet
 }
--- domain/consensus/model/reachabilitydata.go
+++ domain/consensus/model/reachabilitydata.go
@@ -3,11 +3,11 @@ package model
 // ReachabilityData holds the set of data required to answer
 // reachability queries
 type ReachabilityData struct {
-	treeNode          *reachabilityTreeNode
-	futureCoveringSet futureCoveringTreeNodeSet
+	TreeNode          *ReachabilityTreeNode
+	FutureCoveringSet FutureCoveringTreeNodeSet
 }
 
-// reachabilityTreeNode represents a node in the reachability tree
+// ReachabilityTreeNode represents a node in the reachability tree
 // of some DAG block. It mainly provides the ability to query *tree*
 // reachability with O(1) query time. It does so by managing an
 // index interval for each node and making sure all nodes in its
@@ -22,40 +22,40 @@ type ReachabilityData struct {
 // (e.g., [0, 2^64-1]) should always suffice for any practical use-
 // case, and so reindexing should always succeed unless more than
 // 2^64 blocks are added to the DAG/tree.
-type reachabilityTreeNode struct {
-	children []*reachabilityTreeNode
-	parent   *reachabilityTreeNode
+type ReachabilityTreeNode struct {
+	Children []*ReachabilityTreeNode
+	Parent   *ReachabilityTreeNode
 
 	// interval is the index interval containing all intervals of
 	// blocks in this node's subtree
-	interval *reachabilityInterval
+	Interval *ReachabilityInterval
 }
 
-// reachabilityInterval represents an interval to be used within the
-// tree reachability algorithm. See reachabilityTreeNode for further
+// ReachabilityInterval represents an interval to be used within the
+// tree reachability algorithm. See ReachabilityTreeNode for further
 // details.
-type reachabilityInterval struct {
-	start uint64
-	end   uint64
+type ReachabilityInterval struct {
+	Start uint64
+	End   uint64
 }
 
-// orderedTreeNodeSet is an ordered set of reachabilityTreeNodes
+// OrderedTreeNodeSet is an ordered set of reachabilityTreeNodes
 // Note that this type does not validate order validity. It's the
 // responsibility of the caller to construct instances of this
 // type properly.
-type orderedTreeNodeSet []*reachabilityTreeNode
+type OrderedTreeNodeSet []*ReachabilityTreeNode
 
-// futureCoveringTreeNodeSet represents a collection of blocks in the future of
+// FutureCoveringTreeNodeSet represents a collection of blocks in the future of
 // a certain block. Once a block B is added to the DAG, every block A_i in
-// B's selected parent anticone must register B in its futureCoveringTreeNodeSet. This allows
-// to relatively quickly (O(log(|futureCoveringTreeNodeSet|))) query whether B
+// B's selected parent anticone must register B in its FutureCoveringTreeNodeSet. This allows
+// to relatively quickly (O(log(|FutureCoveringTreeNodeSet|))) query whether B
 // is a descendent (is in the "future") of any block that previously
 // registered it.
 //
-// Note that futureCoveringTreeNodeSet is meant to be queried only if B is not
+// Note that FutureCoveringTreeNodeSet is meant to be queried only if B is not
 // a reachability tree descendant of the block in question, as reachability
 // tree queries are always O(1).
 //
 // See insertNode, hasAncestorOf, and reachabilityTree.isInPast for further
 // details.
-type futureCoveringTreeNodeSet orderedTreeNodeSet
+type FutureCoveringTreeNodeSet OrderedTreeNodeSet
