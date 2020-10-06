619eecccd625fd767a971ef3ac815705ab5c4aa3
stasatdaglabs <stas@daglabs.com>
2020-10-06 10:03:35 +0300

    [NOD-1414] Reorg BlockValidator.

--- domain/consensus/processes/interfaces.go
+++ domain/consensus/processes/interfaces.go
@@ -22,8 +22,8 @@ type BlockProcessor interface {
 // it's possible to determine whether a block is valid
 type BlockValidator interface {
 	ValidateHeaderInIsolation(block *appmessage.MsgBlock) error
-	ValidateHeaderInContext(block *appmessage.MsgBlock) error
 	ValidateBodyInIsolation(block *appmessage.MsgBlock) error
+	ValidateHeaderInContext(block *appmessage.MsgBlock) error
 	ValidateBodyInContext(block *appmessage.MsgBlock) error
 	ValidateAgainstPastUTXO(block *appmessage.MsgBlock) error
 	ValidateFinality(block *appmessage.MsgBlock) error
