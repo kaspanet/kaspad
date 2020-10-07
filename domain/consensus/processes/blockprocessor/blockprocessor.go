62dd61eea1e8f4c5df31a232c1a1f018c287e765
stasatdaglabs <stas@daglabs.com>
2020-10-07 16:11:58 +0300

    [NOD-1416] Implement validateBlock.

--- domain/consensus/processes/blockprocessor/blockprocessor.go
+++ domain/consensus/processes/blockprocessor/blockprocessor.go
@@ -10,6 +10,7 @@ import (
 	"github.com/kaspanet/kaspad/util"
 	"github.com/kaspanet/kaspad/util/daghash"
 	"github.com/kaspanet/kaspad/util/mstime"
+	"github.com/pkg/errors"
 )
 
 // BlockProcessor is responsible for processing incoming blocks
@@ -98,12 +99,15 @@ func (bp *BlockProcessor) ValidateAndInsertBlock(block *appmessage.MsgBlock) err
 	start := mstime.Now()
 	log.Debugf("ValidateAndInsertBlock start")
 
-	blockStatus, err := bp.validateBlock(block)
+	shouldInsertBlock, blockStatus, err := bp.validateBlock(block)
 	if err != nil {
 		return err
 	}
+	if !shouldInsertBlock {
+		return nil
+	}
 	if blockStatus != model.StatusValid {
-		return bp.insertInvalidBlock(block, blockStatus)
+		return bp.processNonValidBlock(block, blockStatus)
 	}
 	err = bp.insertBlock(block)
 	if err != nil {
@@ -114,14 +118,74 @@ func (bp *BlockProcessor) ValidateAndInsertBlock(block *appmessage.MsgBlock) err
 	return nil
 }
 
-func (bp *BlockProcessor) validateBlock(block *appmessage.MsgBlock) (model.BlockStatus, error) {
-	return model.StatusValid, nil
+func (bp *BlockProcessor) validateBlock(block *appmessage.MsgBlock) (
+	shouldInsertBlock bool, blockStatus model.BlockStatus, err error) {
+
+	var validationErr *model.ValidationError
+
+	err = bp.blockValidator.ValidateProofOfWork(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return false, model.StatusValidateFailed, nil
+	}
+
+	err = bp.blockValidator.ValidateHeaderInIsolation(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return true, model.StatusValidateFailed, nil
+	}
+
+	err = bp.blockValidator.ValidateHeaderInContext(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return true, model.StatusValidateFailed, nil
+	}
+
+	err = bp.blockValidator.ValidateBodyInIsolation(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return true, model.StatusValidateFailed, nil
+	}
+
+	err = bp.blockValidator.ValidateBodyInContext(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return true, model.StatusValidateFailed, nil
+	}
+
+	err = bp.blockValidator.ValidateAgainstPastUTXO(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return true, model.StatusDisqualifiedFromChain, nil
+	}
+
+	err = bp.blockValidator.ValidateFinality(block)
+	if err != nil {
+		if !errors.As(err, validationErr) {
+			return false, model.StatusValidateFailed, err
+		}
+		return true, model.StatusUTXOPendingVerification, nil
+	}
+
+	return true, model.StatusValid, nil
 }
 
 func (bp *BlockProcessor) insertBlock(block *appmessage.MsgBlock) error {
 	return nil
 }
 
-func (bp *BlockProcessor) insertInvalidBlock(block *appmessage.MsgBlock, blockStatus model.BlockStatus) error {
+func (bp *BlockProcessor) processNonValidBlock(block *appmessage.MsgBlock, blockStatus model.BlockStatus) error {
 	return nil
 }
