1a39d97961993882960150b44616a7cb7ac8d0b8
stasatdaglabs <stas@daglabs.com>
2020-10-11 14:27:18 +0300

    [NOD-1413] Simplify BlockGHOSTDAGData.

--- domain/consensus/model/blockghostdagdata.go
+++ domain/consensus/model/blockghostdagdata.go
@@ -2,33 +2,8 @@ package model
 
 // BlockGHOSTDAGData represents GHOSTDAG data for some block
 type BlockGHOSTDAGData struct {
-	blueScore      uint64
-	selectedParent *DomainHash
-	mergeSetBlues  []*DomainHash
-	mergeSetReds   []*DomainHash
-}
-
-// MergeSetBlues returns the merge-set blues of this block
-func (bgd *BlockGHOSTDAGData) MergeSetBlues() []*DomainHash {
-	return nil
-}
-
-// BlueScore returns the blue score of this block
-func (bgd *BlockGHOSTDAGData) BlueScore() uint64 {
-	return 0
-}
-
-// MergeSetReds returns the merge-set reds of this block
-func (bgd *BlockGHOSTDAGData) MergeSetReds() []*DomainHash {
-	return nil
-}
-
-// MergeSet returns the entire merge-set of this block
-func (bgd *BlockGHOSTDAGData) MergeSet() []*DomainHash {
-	return nil
-}
-
-// SelectedParent returns this block's selected parent
-func (bgd *BlockGHOSTDAGData) SelectedParent() *DomainHash {
-	return nil
+	BlueScore      uint64
+	SelectedParent *DomainHash
+	MergeSetBlues  []*DomainHash
+	MergeSetReds   []*DomainHash
 }
