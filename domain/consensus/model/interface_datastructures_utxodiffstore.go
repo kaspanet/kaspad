5ff1436df4e048dde074558fd95652a6d379577b
stasatdaglabs <stas@daglabs.com>
2020-10-13 12:07:54 +0300

    [NOD-1413] Make changes to UTXODiffStore's interface harder.

--- domain/consensus/model/interface_datastructures_utxodiffstore.go
+++ domain/consensus/model/interface_datastructures_utxodiffstore.go
@@ -2,7 +2,7 @@ package model
 
 // UTXODiffStore represents a store of UTXODiffs
 type UTXODiffStore interface {
-	Insert(dbTx DBTxProxy, blockHash *DomainHash, utxoDiff *UTXODiff)
+	Insert(dbTx DBTxProxy, blockHash *DomainHash, utxoDiff *UTXODiff, utxoDiffChild *DomainHash)
 	Get(dbContext DBContextProxy, blockHash *DomainHash) *UTXODiff
 	Delete(dbTx DBTxProxy, blockHash *DomainHash)
 }
