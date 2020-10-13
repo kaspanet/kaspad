b0db843b8a969f18694d1b3dc427d6b4b732cf43
stasatdaglabs <stas@daglabs.com>
2020-10-13 12:42:10 +0300

    [NOD-1413] Add missing fields to ConsensusStateChanges.

--- domain/consensus/model/consensusstatechanges.go
+++ domain/consensus/model/consensusstatechanges.go
@@ -3,6 +3,10 @@ package model
 // ConsensusStateChanges represents a set of changes that need to be made
 // to transition the current consensus state to a new one
 type ConsensusStateChanges struct {
-	AcceptanceData *BlockAcceptanceData
-	UTXODiff       *UTXODiff
+	AcceptanceData  *BlockAcceptanceData
+	VirtualUTXODiff *UTXODiff
+
+	NewBlockUTXODiff  *UTXODiff
+	NewBlockMultiset  Multiset
+	ParentDiffChanges *map[*DomainHash]UTXODiff
 }
