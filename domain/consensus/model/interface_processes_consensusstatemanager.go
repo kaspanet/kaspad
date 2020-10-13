298ce8bc985f8aba616095eb22d4c70893aca465
stasatdaglabs <stas@daglabs.com>
2020-10-13 12:00:31 +0300

    [NOD-1413] Make changes to ConsensusStateManager's interface.

--- domain/consensus/model/interface_processes_consensusstatemanager.go
+++ domain/consensus/model/interface_processes_consensusstatemanager.go
@@ -3,5 +3,11 @@ package model
 // ConsensusStateManager manages the node's consensus state
 type ConsensusStateManager interface {
 	UTXOByOutpoint(outpoint *DomainOutpoint) *UTXOEntry
-	CalculateConsensusStateChanges(block *DomainBlock) *ConsensusStateChanges
+	CalculateConsensusStateChanges(block *DomainBlock, parents []*DomainHash,
+		transactions []*DomainTransaction, isDisqualified bool) (stateChanges *ConsensusStateChanges,
+		utxoDiffChanges *UTXODiffChanges, virtualGHOSTDAGData *BlockGHOSTDAGData)
+	CalculateAcceptanceDataAndMultiset(blockHash *DomainHash) (*BlockAcceptanceData, Multiset)
+	Tips() []*DomainHash
+	VirtualData() (medianTime int64, blueScore uint64)
+	RestoreUTXOSet(blockHash *DomainHash) ReadOnlyUTXOSet
 }
