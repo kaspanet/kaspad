327f34f2dcb5567ef983a7d63e857688e5b8a395
tal <tal@daglabs.com>
2020-10-19 15:20:27 +0300

    Add alternative implementation for ghostdag. change all function's signatures (add error type)

--- domain/consensus/processes/ghostdag2/ghostdagimpl.go
+++ domain/consensus/processes/ghostdag2/ghostdagimpl.go
@@ -1,20 +1,18 @@
 package ghostdag2
 
-
 import (
-	"sort"
 	"github.com/kaspanet/kaspad/domain/consensus/database"
 	"github.com/kaspanet/kaspad/domain/consensus/model"
 	"github.com/kaspanet/kaspad/infrastructure/db/dbaccess"
+	"sort"
 )
 
-type ghostdagHelper struct{
-	k 		      model.KType
-	dataStore   model.GHOSTDAGDataStore
-	dbAccess 	  *database.DomainDBContext
+type ghostdagHelper struct {
+	k                  model.KType
+	dataStore          model.GHOSTDAGDataStore
+	dbAccess           *database.DomainDBContext
 	dagTopologyManager model.DAGTopologyManager
 }
-
 
 // New instantiates a new GHOSTDAGHelper -like a factory
 func New(
@@ -24,27 +22,28 @@ func New(
 	k model.KType) model.GHOSTDAGManager {
 
 	return &ghostdagHelper{
-		dbAccess:    database.NewDomainDBContext(databaseContext),
+		dbAccess:           database.NewDomainDBContext(databaseContext),
 		dagTopologyManager: dagTopologyManager,
-		dataStore:  ghostdagDataStore,
+		dataStore:          ghostdagDataStore,
 		k:                  k,
-
 	}
 }
 
-
 /* --------------------------------------------- */
 //K, GHOSTDAGDataStore
-func(gh *ghostdagHelper) GHOSTDAG (blockParents []*model.DomainHash) (*model.BlockGHOSTDAGData, error){
+func (gh *ghostdagHelper) GHOSTDAG(blockParents []*model.DomainHash) (*model.BlockGHOSTDAGData, error) {
 	var maxNum uint64 = 0
-	var score uint64
 	var myScore uint64
 	var selectedParent *model.DomainHash
 	/* find the selectedParent */
-	for _,w := range blockParents{
-		score = gh.dataStore.Get(gh.dbAccess, w).BlueScore
+	for _, w := range blockParents {
+		blockData, err := gh.dataStore.Get(gh.dbAccess, w)
+		if err != nil {
+			return nil, err
+		}
+		score := blockData.BlueScore
 		//GHOSTDAGDataStore.get(w).blueScore()
-		if score > maxNum{
+		if score > maxNum {
 			selectedParent = w
 			maxNum = score
 		}
@@ -56,20 +55,31 @@ func(gh *ghostdagHelper) GHOSTDAG (blockParents []*model.DomainHash) (*model.Blo
 	   1. If block A is in B's reds group (for block B that belong to the blue group) it will never be in blue(or blues).
 	*/
 
-
-
 	var blues []*model.DomainHash = make([]*model.DomainHash, 0)
 	var reds []*model.DomainHash = make([]*model.DomainHash, 0)
-	var mergeSetArr []*model.DomainHash = make([]*model.DomainHash, 0)
 	var blueSet []*model.DomainHash = make([]*model.DomainHash, 0)
 
-	gh.findMergeSet(blockParents, &mergeSetArr, selectedParent)
-	gh.sortByBlueScore(&mergeSetArr)
-	gh.findBlueSet(&blueSet, selectedParent)
-	for _,d := range mergeSetArr{
-		gh.divideBlueRed(selectedParent, d, &blues, &reds, &blueSet)
+	mergeSetArr, err := gh.findMergeSet(blockParents, selectedParent)
+	if err != nil {
+		return nil, err
 	}
-	myScore+= uint64(len(blues))
+	//STOP HERE
+	err = gh.sortByBlueScore(mergeSetArr)
+	if err != nil {
+		return nil, err
+	}
+	err = gh.findBlueSet(&blueSet, selectedParent)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, d := range mergeSetArr {
+		err := gh.divideBlueRed(selectedParent, d, &blues, &reds, &blueSet)
+		if err != nil {
+			return nil, err
+		}
+	}
+	myScore += uint64(len(blues))
 	/* Finial Data:
 	   1. BlueScore => myScore
 	   2. blues
@@ -83,10 +93,8 @@ func(gh *ghostdagHelper) GHOSTDAG (blockParents []*model.DomainHash) (*model.Blo
 		MergeSetReds:   reds,
 	}
 
-	return &e,nil
+	return &e, nil
 }
-
-
 
 /*  1. blue = selectedParent.blue + blues
     2. h is not connected to at most K blocks (from the blue group)
@@ -94,24 +102,45 @@ func(gh *ghostdagHelper) GHOSTDAG (blockParents []*model.DomainHash) (*model.Blo
 */
 
 /* ---------------divideBluesReds--------------------- */
-func (gh *ghostdagHelper) divideBlueRed(selectedParent *model.DomainHash, desiredBlock *model.DomainHash, blues *[]*model.DomainHash, reds *[]*model.DomainHash, blueSet *[]*model.DomainHash ) {
+func (gh *ghostdagHelper) divideBlueRed(selectedParent *model.DomainHash, desiredBlock *model.DomainHash,
+	blues *[]*model.DomainHash, reds *[]*model.DomainHash, blueSet *[]*model.DomainHash) error {
 	counter := 0
 	var chain = selectedParent
 	var stop = false
 	for chain != nil { /*nil -> after genesis*/
-		if !gh.validateKCluster(chain, desiredBlock, &counter, blueSet) {/* ret false*/
+		//if !gh.validateKCluster(chain, desiredBlock, &counter, blueSet) { /* ret false*/
+		isValid, err := gh.validateKCluster(chain, desiredBlock, &counter, blueSet)
+		if err != nil {
+			return err
+		}
+		if !isValid {
 			stop = true
 			break
 		}
 		/* Check valid for the blues of the chain */
-		for _,b := range gh.dataStore.Get(gh.dbAccess, chain).MergeSetBlues{
-				if !gh.validateKCluster(b, desiredBlock, &counter, blueSet) {/* ret false*/
-					stop = true
-					break
-				}
+		blockData, err := gh.dataStore.Get(gh.dbAccess, chain)
+		if err != nil {
+			return err
 		}
-		for _,e := range *blues{
-			if !gh.validateKCluster(e, desiredBlock, &counter, blueSet) {/* ret false*/
+
+		for _, b := range blockData.MergeSetBlues {
+			//if !gh.validateKCluster(b, desiredBlock, &counter, blueSet) { /* ret false*/
+			isValid2, err2 := gh.validateKCluster(b, desiredBlock, &counter, blueSet)
+			if err2 != nil {
+				return err2
+			}
+			if !isValid2 {
+				stop = true
+				break
+			}
+		}
+		for _, e := range *blues {
+			//if !gh.validateKCluster(e, desiredBlock, &counter, blueSet) { /* ret false*/
+			isValid3, err3 := gh.validateKCluster(e, desiredBlock, &counter, blueSet)
+			if err3 != nil {
+				return err3
+			}
+			if !isValid3 {
 				stop = true
 				break
 			}
@@ -119,53 +148,87 @@ func (gh *ghostdagHelper) divideBlueRed(selectedParent *model.DomainHash, desire
 		if stop {
 			break
 		}
-		chain =gh.dataStore.Get(gh.dbAccess, chain).SelectedParent
+		//chain = gh.dataStore.Get(gh.dbAccess, chain).SelectedParent
+		blockData2, err2 := gh.dataStore.Get(gh.dbAccess, chain)
+		if err2 != nil {
+			return err2
+		}
+		chain = blockData2.SelectedParent
 	}
-	if stop{
-		*reds = append(*reds,desiredBlock )
+	if stop {
+		*reds = append(*reds, desiredBlock)
 	}
-	if !stop{
+	if !stop {
 		*blues = append(*blues, desiredBlock)
 		*blueSet = append(*blueSet, desiredBlock)
 	}
-	return
+	return nil
 }
 
 /* ---------------isAnticone-------------------------- */
-func (gh *ghostdagHelper) isAnticone(h1, h2 *model.DomainHash) bool{
+func (gh *ghostdagHelper) isAnticone(h1, h2 *model.DomainHash) (bool, error) {
 	//return !isInPast(h1, h2) && !isInPast(h1,h2)
-	return !gh.dagTopologyManager.IsAncestorOf(h1, h2) && ! gh.dagTopologyManager.IsAncestorOf(h2, h1)
+	//return !gh.dagTopologyManager.IsAncestorOf(h1, h2) && !gh.dagTopologyManager.IsAncestorOf(h2, h1)
+	isAB, err := gh.dagTopologyManager.IsAncestorOf(h1, h2)
+	if err != nil {
+		return false, err
+	}
+	isBA, err := gh.dagTopologyManager.IsAncestorOf(h1, h2)
+	if err != nil {
+		return false, err
+	}
+	return !isAB && !isBA, nil
+
 }
 
-
 /* ----------------validateKCluster------------------- */
-func(gh *ghostdagHelper) validateKCluster(chain *model.DomainHash, s1 *model.DomainHash, counter *int, blueSet *[]*model.DomainHash) bool{
+func (gh *ghostdagHelper) validateKCluster(chain *model.DomainHash, s1 *model.DomainHash, counter *int, blueSet *[]*model.DomainHash) (bool, error) {
 	var k int = int(gh.k)
-	if n := gh.isAnticone(chain, s1);n {
-		if *counter > k{
-			return false;
+	isAnt, err := gh.isAnticone(chain, s1)
+	if err != nil {
+		return false, err
+	}
+	if isAnt {
+		//if n := gh.isAnticone(chain, s1); n {
+		if *counter > k {
+			return false, nil
 		}
-		if gh.checkIfDestroy(chain, blueSet){
-			return false;
+		//if gh.checkIfDestroy(chain, blueSet) {
+		ifDes, err := gh.checkIfDestroy(chain, blueSet)
+		if err != nil {
+			return false, err
 		}
-		*counter ++
-		return true
-	} else{
-		if gh.dagTopologyManager.IsAncestorOf(s1, chain){
-			if g:= gh.dataStore.Get(gh.dbAccess, chain).MergeSetReds; contains(s1, g){
-				return false
+		if ifDes {
+			return false, nil
+		}
+		*counter++
+		return true, nil
+	} else {
+		isAnt2, err := gh.dagTopologyManager.IsAncestorOf(s1, chain)
+		if err != nil {
+			return false, err
+		}
+		//if gh.dagTopologyManager.IsAncestorOf(s1, chain) {
+		if isAnt2 {
+			dataStore, err2 := gh.BlockData(chain)
+			if err2 != nil {
+				return false, err
 			}
-		} else{
-			return true
+			if g := dataStore.MergeSetReds; contains(s1, g) {
+				//if g := gh.dataStore.Get(gh.dbAccess, chain).MergeSetReds; contains(s1, g) {
+				return false, nil
+			}
+		} else {
+			return true, nil
 		}
 	}
-	return false
+	return false, nil
 }
 
 /*----------------contains-------------------------- */
-func contains(s *model.DomainHash, g []*model.DomainHash) bool{
-	for _,r := range g{
-		if r == s{
+func contains(s *model.DomainHash, g []*model.DomainHash) bool {
+	for _, r := range g {
+		if r == s {
 			return true
 		}
 	}
@@ -174,87 +237,133 @@ func contains(s *model.DomainHash, g []*model.DomainHash) bool{
 
 /* ----------------checkIfDestroy------------------- */
 /* find number of not-connected in his blue*/
-func (gh *ghostdagHelper)checkIfDestroy(chain *model.DomainHash, blueSet *[]*model.DomainHash) bool{
+func (gh *ghostdagHelper) checkIfDestroy(chain *model.DomainHash, blueSet *[]*model.DomainHash) (bool, error) {
 	var k int = int(gh.k)
 	counter := 0
-	for _,s2 := range *blueSet{
-		if gh.isAnticone(s2, chain) {
+	for _, s2 := range *blueSet {
+		//if gh.isAnticone(s2, chain) {
+		isAnt, err := gh.isAnticone(s2, chain)
+		if err != nil {
+			return false, err
+		}
+		if isAnt {
 			counter++
 		}
-		if counter > k{
-			return false
+		if counter > k {
+			return false, nil
 		}
 	}
-	return true
+	return true, nil
 }
 
-
 /* ----------------findMergeSet------------------- */
-func (gh *ghostdagHelper)findMergeSet (h []*model.DomainHash, allMergeSet *[]*model.DomainHash, selectedParent *model.DomainHash) {
+func (gh *ghostdagHelper) findMergeSet(h []*model.DomainHash, selectedParent *model.DomainHash) ([]*model.DomainHash, error) {
 
+	allMergeSet := make([]*model.DomainHash, 0)
 	var nodeQueue = make([]*model.DomainHash, 0)
-	for _,g := range h {
+	for _, g := range h {
 		nodeQueue = append(nodeQueue, g)
 	}
-	for len(nodeQueue) > 0 {/*return boolean */
+	for len(nodeQueue) > 0 { /*return boolean */
 		ha := nodeQueue[0]
 		nodeQueue = nodeQueue[1:]
-		if selectedParent == ha{
+		if selectedParent == ha {
 			continue
 		}
 		//if isInPast(ha, selectedParent){
-		if gh.dagTopologyManager.IsAncestorOf(ha, selectedParent){
+		//if gh.dagTopologyManager.IsAncestorOf(ha, selectedParent) {
+		isanc, err := gh.dagTopologyManager.IsAncestorOf(ha, selectedParent)
+		if err != nil {
+			return nil, err
+		}
+		if isanc {
 			continue
 		}
 
-		*allMergeSet = append(*allMergeSet, ha)
-		gh.insertParent(ha, &nodeQueue)
+		allMergeSet = append(allMergeSet, ha)
+		err = gh.insertParent(ha, &nodeQueue)
+		if err != nil {
+			return nil, err
+		}
 
 	}
-
+	return allMergeSet, nil
 }
 
 /* ----------------insertParent------------------- */
 /* Insert all parents to the queue*/
-func (gh *ghostdagHelper) insertParent (h *model.DomainHash, q1 *[]*model.DomainHash) {
-	for _,v := range gh.dagTopologyManager.Parents(h){
-		if contains(v, *q1){
+func (gh *ghostdagHelper) insertParent(h *model.DomainHash, q1 *[]*model.DomainHash) error {
+	parents, err := gh.dagTopologyManager.Parents(h)
+	if err != nil {
+		return err
+	}
+	for _, v := range parents {
+		if contains(v, *q1) {
 			continue
 		}
 		*q1 = append(*q1, v)
 	}
+	return nil
 }
 
 /* ----------------findBlueSet------------------- */
-func(gh *ghostdagHelper) findBlueSet(blueSet *[]*model.DomainHash, h *model.DomainHash) {
-	for h!= nil{
-		*blueSet = append( *blueSet, h)
+func (gh *ghostdagHelper) findBlueSet(blueSet *[]*model.DomainHash, h *model.DomainHash) error {
+	for h != nil {
+		*blueSet = append(*blueSet, h)
 		//blueSet = append(gh.dataStore.Get(gh.dbAccess, h).MergeSetBlues, blueSet) //change
-		for _,v := range gh.dataStore.Get(gh.dbAccess, h).MergeSetBlues{
-			if contains(v, *blueSet){
+		//for _, v := range gh.dataStore.Get(gh.dbAccess, h).MergeSetBlues {
+		blockData, err := gh.dataStore.Get(gh.dbAccess, h)
+		if err != nil {
+			return err
+		}
+		mergeSetBlue := blockData.MergeSetBlues
+		for _, v := range mergeSetBlue {
+			if contains(v, *blueSet) {
 				continue
 			}
 			*blueSet = append(*blueSet, v)
 		}
-		h = gh.dataStore.Get(gh.dbAccess, h).SelectedParent
+		//h = gh.dataStore.Get(gh.dbAccess, h).SelectedParent
+		h = blockData.SelectedParent
 	}
+	return nil
 }
-
-
 
 /* ----------------sortByBlueScore------------------- */
-func (gh *ghostdagHelper)sortByBlueScore(arr *[]*model.DomainHash) {
-	sort.SliceStable(*arr, func(i, j int) bool{
-		isSmaller := gh.dataStore.Get(gh.dbAccess,(*arr)[i] ).BlueScore < gh.dataStore.Get(gh.dbAccess,(*arr)[j]).BlueScore
+func (gh *ghostdagHelper) sortByBlueScore(arr []*model.DomainHash) error {
+	//var err error
+	//
+	//sort.SliceStable(*arr, func(i, j int) bool {
+	//
+	//	isSmaller := gh.dataStore.Get(gh.dbAccess, (*arr)[i]).BlueScore < gh.dataStore.Get(gh.dbAccess, (*arr)[j]).BlueScore
+	//	return isSmaller
+	//})
+	//return nil
+	blockData := make([]*model.BlockGHOSTDAGData, len(arr))
+
+	for i, h := range arr {
+		var err error
+		blockData[i], err = gh.dataStore.Get(gh.dbAccess, h)
+		if err != nil {
+			return err
+		}
+	}
+	sort.SliceStable(blockData, func(i, j int) bool {
+		isSmaller := blockData[i].BlueScore < blockData[j].BlueScore
 		return isSmaller
 	})
+	return nil
 }
-
 
 /* --------------------------------------------- */
 
-func (gh *ghostdagHelper)BlockData(blockHash *model.DomainHash) *model.BlockGHOSTDAGData{
+func (gh *ghostdagHelper) BlockData(blockHash *model.DomainHash) (*model.BlockGHOSTDAGData, error) {
 	return gh.dataStore.Get(gh.dbAccess, blockHash)
 	//last
 }
 
+func (gh *ghostdagHelper) ChooseSelectedParent(
+	blockHashA *model.DomainHash, blockAGHOSTDAGData *model.BlockGHOSTDAGData,
+	blockHashB *model.DomainHash, blockBGHOSTDAGData *model.BlockGHOSTDAGData) *model.DomainHash {
+	panic("unimplemented")
+}
--- domain/consensus/processes/ghostdag_test.go
+++ domain/consensus/processes/ghostdag_test.go
@@ -6,83 +6,93 @@ import (
 	"testing"
 )
 
-func TestGHOSTDA(t *testing.T){
+func TestGHOSTDA(t *testing.T) {
 	t.Errorf("helo") //keep running
 	if false {
 		t.Fatalf("The test failed") // string - //stop
 	}
 
-	g := ghostdag2.New()
-
+	dagTopology := &DAGTopologyManagerImpl{}
+	ghostdagDataStore := &GHOSTDAGDataStoreImpl{}
+	g := ghostdag2.New(nil, dagTopology, ghostdagDataStore, 10)
 
 }
 
 type GHOSTDAGDataStoreImpl struct {
-	dagMap map[*model.DomainHash] *model.BlockGHOSTDAGData
+	dagMap map[*model.DomainHash]*model.BlockGHOSTDAGData
 }
 
-
-func (ds *GHOSTDAGDataStoreImpl) Insert(dbTx model.DBTxProxy, blockHash *model.DomainHash, blockGHOSTDAGData *model.BlockGHOSTDAGData){
+func (ds *GHOSTDAGDataStoreImpl) Insert(dbTx model.DBTxProxy, blockHash *model.DomainHash, blockGHOSTDAGData *model.BlockGHOSTDAGData) error {
 	ds.dagMap[blockHash] = blockGHOSTDAGData
-}
-func (ds *GHOSTDAGDataStoreImpl) Get(dbContext model.DBContextProxy, blockHash *model.DomainHash) *model.BlockGHOSTDAGData{
-	v, ok := ds.dagMap[blockHash]
-	if ok{
-		return v
-	}
 	return nil
 }
-
-
-
-
-
+func (ds *GHOSTDAGDataStoreImpl) Get(dbContext model.DBContextProxy, blockHash *model.DomainHash) (*model.BlockGHOSTDAGData, error) {
+	v, ok := ds.dagMap[blockHash]
+	if ok {
+		return v, nil
+	}
+	return nil, nil
+}
 
 //candidateBluesAnticoneSizes = make(map[model.DomainHash]model.KType, gm.k)
-type DAGTopologyManagerImpl struct{
+type DAGTopologyManagerImpl struct {
 	//dagMap map[*model.DomainHash] *model.BlockGHOSTDAGData
-	parentsMap map[*model.DomainHash] []*model.DomainHash
+	parentsMap map[*model.DomainHash][]*model.DomainHash
 }
 
 //Implemented//
-func (dt *DAGTopologyManagerImpl) Parents(blockHash *model.DomainHash) []*model.DomainHash{
-	v, ok:= dt.parentsMap[blockHash]
-	if !ok{
-		return make([]*model.DomainHash, 0)
-	}else{
-		return v
+func (dt *DAGTopologyManagerImpl) Parents(blockHash *model.DomainHash) ([]*model.DomainHash, error) {
+	v, ok := dt.parentsMap[blockHash]
+	if !ok {
+		return make([]*model.DomainHash, 0), nil
+	} else {
+		return v, nil
 	}
 }
 
-func (dt *DAGTopologyManagerImpl) Children(blockHash *model.DomainHash) []*model.DomainHash{
-	return nil
+func (dt *DAGTopologyManagerImpl) Children(blockHash *model.DomainHash) ([]*model.DomainHash, error) {
+	panic("unimplemented")
 }
-func (dt *DAGTopologyManagerImpl) IsParentOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) bool{
-	return true
+
+func (dt *DAGTopologyManagerImpl) IsParentOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) (bool, error) {
+	panic("unimplemented")
 }
-func (dt *DAGTopologyManagerImpl) IsChildOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) bool{
-	return true
+
+func (dt *DAGTopologyManagerImpl) IsChildOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) (bool, error) {
+	panic("unimplemented")
 }
+
 //Implemented//
-func (dt *DAGTopologyManagerImpl) IsAncestorOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) bool{
-	v, ok:= dt.parentsMap[blockHashB]
-	if !ok{
-		return false
+func (dt *DAGTopologyManagerImpl) IsAncestorOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) (bool, error) {
+	bParents, ok := dt.parentsMap[blockHashB]
+	if !ok {
+		return false, nil
 	}
-	for _, r := range v{
-		if r == blockHashA{
-			return true
+	for _, r := range bParents {
+		if r == blockHashA {
+			return true, nil
 		}
 	}
-	for _, y := range v{
-		if dt.IsAncestorOf(blockHashA, y){
-			return true
+	for _, y := range bParents {
+		isAnc, err := dt.IsAncestorOf(blockHashA, y)
+		if err != nil {
+			return false, err
+		}
+		if isAnc {
+			return true, nil
 		}
 	}
-	return false
+	return false, nil
 
 }
 
-func (dt *DAGTopologyManagerImpl) IsDescendantOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) bool{
-	return true
+func (dt *DAGTopologyManagerImpl) IsDescendantOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) (bool, error) {
+	panic("unimplemented")
+}
+
+func (gh *DAGTopologyManagerImpl) IsAncestorOfAny(blockHash *model.DomainHash, potentialDescendants []*model.DomainHash) (bool, error) {
+	panic("unimplemented")
+}
+func (gh *DAGTopologyManagerImpl) IsInSelectedParentChainOf(blockHashA *model.DomainHash, blockHashB *model.DomainHash) (bool, error) {
+	panic("unimplemented")
 }
