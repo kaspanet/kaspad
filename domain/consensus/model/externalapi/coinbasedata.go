ec4820aeaeabc7837e9cafffa5dcd5c5caa740d3
stasatdaglabs <stas@daglabs.com>
2020-10-19 17:42:11 +0300

    [NOD-1462] Add a comment above CoinbaseData.

--- domain/consensus/model/externalapi/coinbasedata.go
+++ domain/consensus/model/externalapi/coinbasedata.go
@@ -1,5 +1,7 @@
 package externalapi
 
+// CoinbaseData contains data by which a coinbase transaction
+// is built in a new block
 type CoinbaseData struct {
 	scriptPublicKey []byte
 	extraData       []byte
