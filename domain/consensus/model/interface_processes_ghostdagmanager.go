48e971ae34f3325d9050cd6c11fd36b48f3f11f1
stasatdaglabs <stas@daglabs.com>
2020-10-21 10:01:31 +0300

    [NOD-1475] Simplify ChooseSelectedParent.

--- domain/consensus/model/interface_processes_ghostdagmanager.go
+++ domain/consensus/model/interface_processes_ghostdagmanager.go
@@ -5,7 +5,5 @@ import "github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 // GHOSTDAGManager resolves and manages GHOSTDAG block data
 type GHOSTDAGManager interface {
 	GHOSTDAG(blockHash *externalapi.DomainHash) error
-	ChooseSelectedParent(
-		blockHashA *externalapi.DomainHash, blockAGHOSTDAGData *BlockGHOSTDAGData,
-		blockHashB *externalapi.DomainHash, blockBGHOSTDAGData *BlockGHOSTDAGData) *externalapi.DomainHash
+	ChooseSelectedParent(blockHashA *externalapi.DomainHash, blockHashB *externalapi.DomainHash) (*externalapi.DomainHash, error)
 }
