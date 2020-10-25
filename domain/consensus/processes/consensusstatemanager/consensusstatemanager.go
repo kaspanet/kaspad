20dabbdfa85df4a5070f7849c77ed68c9672321b
stasatdaglabs <stas@daglabs.com>
2020-10-25 15:11:28 +0200

    [NOD-1476] Remove irrelevant functions out of ConsensusStateManager.

--- domain/consensus/processes/consensusstatemanager/consensusstatemanager.go
+++ domain/consensus/processes/consensusstatemanager/consensusstatemanager.go
@@ -72,14 +72,3 @@ func (csm *consensusStateManager) AddBlockToVirtual(blockHash *externalapi.Domai
 func (csm *consensusStateManager) VirtualData() (medianTime int64, blueScore uint64, err error) {
 	return 0, 0, nil
 }
-
-// RestoreUTXOSet calculates and returns the UTXOSet of the given blockHash
-func (csm *consensusStateManager) RestorePastUTXOSet(blockHash *externalapi.DomainHash) (model.ReadOnlyUTXOSet, error) {
-	return nil, nil
-}
-
-// RestoreDiffFromVirtual restores the diff between the given virtualDiffParentHash
-// and the virtual
-func (csm *consensusStateManager) RestoreDiffFromVirtual(utxoDiff *model.UTXODiff, virtualDiffParentHash *externalapi.DomainHash) (*model.UTXODiff, error) {
-	panic("implement me")
-}
