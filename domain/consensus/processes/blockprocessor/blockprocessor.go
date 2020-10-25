676605ba6bf74d865de05b8da41d5be5c8fcb0fd
stasatdaglabs <stas@daglabs.com>
2020-10-25 15:55:52 +0200

    [NOD-1476] Fix merge errors.

--- domain/consensus/processes/blockprocessor/blockprocessor.go
+++ domain/consensus/processes/blockprocessor/blockprocessor.go
@@ -5,6 +5,8 @@ import (
 	"github.com/kaspanet/kaspad/domain/consensus/model"
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 	"github.com/kaspanet/kaspad/domain/dagconfig"
+	"github.com/kaspanet/kaspad/util/mstime"
+	"time"
 )
 
 // blockProcessor is responsible for processing incoming blocks
@@ -68,29 +70,11 @@ func New(
 func (bp *blockProcessor) BuildBlock(coinbaseData *externalapi.DomainCoinbaseData,
 	transactions []*externalapi.DomainTransaction) (*externalapi.DomainBlock, error) {
 
-	start := mstime.Now()
+	start := time.Now()
 	log.Debugf("BuildBlock start")
-
-	parents := bp.selectParentsForNewBlock()
-	transactions := transactionSelector(bp.consensusStateStore.FullUTXOSet())
-	block := bp.buildBlock(coinbaseScriptPublicKey, coinbaseExtraData, parents, transactions)
-
-	log.Debugf("BuildBlock end. Took: %s", mstime.Since(start))
-	return block
-}
-
-func (bp *BlockProcessor) selectParentsForNewBlock() []*daghash.Hash {
-	virtualParentHashes := bp.consensusStateStore.VirtualParents()
-	if len(virtualParentHashes) < appmessage.MaxBlockParents {
-		return virtualParentHashes
-	}
-	return virtualParentHashes[:appmessage.MaxBlockParents]
-}
-
-func (bp *BlockProcessor) buildBlock(coinbaseScriptPublicKey []byte, coinbaseExtraData []byte,
-	parentHashes []*daghash.Hash, transactions []*util.Tx) *appmessage.MsgBlock {
-
-	return nil, nil
+	block, err := bp.buildBlock(coinbaseData, transactions)
+	log.Debugf("BuildBlock end. Took: %s", time.Since(start))
+	return block, err
 }
 
 // ValidateAndInsertBlock validates the given block and, if valid, applies it
@@ -98,94 +82,61 @@ func (bp *BlockProcessor) buildBlock(coinbaseScriptPublicKey []byte, coinbaseExt
 func (bp *blockProcessor) ValidateAndInsertBlock(block *externalapi.DomainBlock) error {
 	start := mstime.Now()
 	log.Debugf("ValidateAndInsertBlock start")
-
-	shouldInsertBlock, blockStatus, err := bp.validateBlock(block)
-	if err != nil {
-		return err
-	}
-	if !shouldInsertBlock {
-		return nil
-	}
-	if blockStatus != model.StatusValid {
-		return bp.processNonValidBlock(block, blockStatus)
-	}
-	err = bp.insertBlock(block)
-	if err != nil {
-		return err
-	}
-
+	err := bp.validateAndInsertBlock(block)
 	log.Debugf("ValidateAndInsertBlock end. Took: %s", mstime.Since(start))
+	return err
+}
+
+func (bp *blockProcessor) buildBlock(coinbaseData *externalapi.DomainCoinbaseData,
+	transactions []*externalapi.DomainTransaction) (*externalapi.DomainBlock, error) {
+
+	return nil, nil
+}
+
+func (bp *blockProcessor) validateAndInsertBlock(block *externalapi.DomainBlock) error {
+	err := bp.blockValidator.ValidateProofOfWork(block)
+	if err != nil {
+		// If the validation failed:
+		//   Write in blockStatusStore that the block is invalid
+		// return err
+	}
+
+	err = bp.validateBlockInIsolationAndInContext(block)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (bp *BlockProcessor) validateBlock(block *appmessage.MsgBlock) (
-	shouldInsertBlock bool, blockStatus model.BlockStatus, err error) {
-
-	var validationErr *model.ValidationError
-
-	err = bp.blockValidator.ValidateProofOfWork(block)
+func (bp *blockProcessor) validateBlockInIsolationAndInContext(block *externalapi.DomainBlock) error {
+	err := bp.blockValidator.ValidateHeaderInIsolation(block.Hash)
 	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return false, model.StatusValidateFailed, nil
+		return err
 	}
 
-	err = bp.blockValidator.ValidateHeaderInIsolation(block)
+	err = bp.blockValidator.ValidateHeaderInContext(block.Hash)
 	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return true, model.StatusValidateFailed, nil
+		return err
 	}
 
-	err = bp.blockValidator.ValidateHeaderInContext(block)
+	err = bp.blockValidator.ValidateBodyInIsolation(block.Hash)
 	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return true, model.StatusValidateFailed, nil
+		return err
 	}
 
-	err = bp.blockValidator.ValidateBodyInIsolation(block)
+	err = bp.blockValidator.ValidateBodyInContext(block.Hash)
 	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return true, model.StatusValidateFailed, nil
+		return err
 	}
 
-	err = bp.blockValidator.ValidateBodyInContext(block)
-	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return true, model.StatusValidateFailed, nil
-	}
-
-	err = bp.blockValidator.ValidateAgainstPastUTXO(block)
-	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return true, model.StatusDisqualifiedFromChain, nil
-	}
-
-	err = bp.blockValidator.ValidateFinality(block)
-	if err != nil {
-		if !errors.As(err, validationErr) {
-			return false, model.StatusValidateFailed, err
-		}
-		return true, model.StatusUTXOPendingVerification, nil
-	}
-
-	return true, model.StatusValid, nil
-}
-
-func (bp *BlockProcessor) insertBlock(block *appmessage.MsgBlock) error {
 	return nil
 }
 
-func (bp *BlockProcessor) processNonValidBlock(block *appmessage.MsgBlock, blockStatus model.BlockStatus) error {
+func (bp *blockProcessor) insertBlock(block *externalapi.DomainBlock) error {
+	return nil
+}
+
+func (bp *blockProcessor) processNonValidBlock(block *externalapi.DomainBlock, blockStatus model.BlockStatus) error {
 	return nil
 }
