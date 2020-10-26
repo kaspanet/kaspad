edb8bb1733102d89c58a597a7f38a94c2d2f3681
Mike Zak <feanorr@gmail.com>
2020-10-26 09:08:34 +0200

    [NOD-1420] Convert model to externalapi in utxo_algerbra helpers

--- domain/consensus/processes/consensusstatemanager/utxoalgebra/collection_helpers.go
+++ domain/consensus/processes/consensusstatemanager/utxoalgebra/collection_helpers.go
@@ -2,10 +2,11 @@ package utxoalgebra
 
 import (
 	"github.com/kaspanet/kaspad/domain/consensus/model"
+	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 )
 
 // add adds a new UTXO entry to this collection
-func collectionAdd(collection model.UTXOCollection, outpoint model.DomainOutpoint, entry *model.model.UTXOEntry) {
+func collectionAdd(collection model.UTXOCollection, outpoint externalapi.DomainOutpoint, entry *externalapi.UTXOEntry) {
 	collection[outpoint] = entry
 }
 
@@ -17,7 +18,7 @@ func collectionAddMultiple(collection model.UTXOCollection, collectionToAdd mode
 }
 
 // remove removes a UTXO entry from this collection if it exists
-func collectionRemove(collection model.UTXOCollection, outpoint model.DomainOutpoint) {
+func collectionRemove(collection model.UTXOCollection, outpoint externalapi.DomainOutpoint) {
 	delete(collection, outpoint)
 }
 
@@ -30,20 +31,20 @@ func collectionRemoveMultiple(collection model.UTXOCollection, collectionToRemov
 
 // get returns the model.UTXOEntry represented by provided outpoint,
 // and a boolean value indicating if said model.UTXOEntry is in the set or not
-func collectionGet(collection model.UTXOCollection, outpoint model.DomainOutpoint) (*model.UTXOEntry, bool) {
+func collectionGet(collection model.UTXOCollection, outpoint externalapi.DomainOutpoint) (*externalapi.UTXOEntry, bool) {
 	entry, ok := collection[outpoint]
 	return entry, ok
 }
 
 // contains returns a boolean value indicating whether a UTXO entry is in the set
-func collectionContains(collection model.UTXOCollection, outpoint model.DomainOutpoint) bool {
+func collectionContains(collection model.UTXOCollection, outpoint externalapi.DomainOutpoint) bool {
 	_, ok := collection[outpoint]
 	return ok
 }
 
 // containsWithBlueScore returns a boolean value indicating whether a model.UTXOEntry
 // is in the set and its blue score is equal to the given blue score.
-func collectionContainsWithBlueScore(collection model.UTXOCollection, outpoint model.DomainOutpoint, blueScore uint64) bool {
+func collectionContainsWithBlueScore(collection model.UTXOCollection, outpoint externalapi.DomainOutpoint, blueScore uint64) bool {
 	entry, ok := collectionGet(collection, outpoint)
 	return ok && entry.BlockBlueScore == blueScore
 }
