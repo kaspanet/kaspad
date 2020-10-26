03c2ad5c49dd9e16efa78132052a34ec6c38f912
stasatdaglabs <stas@daglabs.com>
2020-10-26 17:13:02 +0200

    [NOD-1416] Implement validateHeaderAndProofOfWork and validateBody.

--- domain/consensus/processes/blockprocessor/validateandinsertblock.go
+++ domain/consensus/processes/blockprocessor/validateandinsertblock.go
@@ -6,7 +6,15 @@ import (
 )
 
 func (bp *blockProcessor) validateAndInsertBlock(block *externalapi.DomainBlock) error {
-	err := bp.validateBlockInIsolationAndInContext(block)
+	err := bp.validateBlock(block)
+	if err != nil {
+		bp.discardAllChanges()
+		return err
+	}
+
+	// Block validations passed, save whatever DAG data was
+	// collected so far
+	err = bp.commitAllChanges()
 	if err != nil {
 		return err
 	}
@@ -14,27 +22,51 @@ func (bp *blockProcessor) validateAndInsertBlock(block *externalapi.DomainBlock)
 	return nil
 }
 
-func (bp *blockProcessor) validateBlockInIsolationAndInContext(block *externalapi.DomainBlock) error {
+func (bp *blockProcessor) validateBlock(block *externalapi.DomainBlock) error {
+	// If either header of proof-of-work validation fails, simply
+	// return an error without writing anything in the database.
+	// This is to prevent spamming attacks.
+	err := bp.validateHeaderAndProofOfWork(block)
+	if err != nil {
+		return err
+	}
+
+	// It's now safe to store the block in the database
+	bp.blockStatusStore.Stage(block.Hash, model.StatusInvalid)
+	bp.blockStore.Stage(block.Hash, block)
+	err = bp.commitAllChanges()
+	if err != nil {
+		return err
+	}
+
+	return bp.validateBody(block.Hash)
+}
+
+func (bp *blockProcessor) validateHeaderAndProofOfWork(block *externalapi.DomainBlock) error {
 	err := bp.blockValidator.ValidateHeaderInIsolation(block.Hash)
 	if err != nil {
 		return err
 	}
-
 	err = bp.blockValidator.ValidateHeaderInContext(block.Hash)
 	if err != nil {
 		return err
 	}
-
-	err = bp.blockValidator.ValidateBodyInIsolation(block.Hash)
+	err = bp.blockValidator.ValidateProofOfWork(block.Hash)
 	if err != nil {
 		return err
 	}
+	return nil
+}
 
-	err = bp.blockValidator.ValidateBodyInContext(block.Hash)
+func (bp *blockProcessor) validateBody(blockHash *externalapi.DomainHash) error {
+	err := bp.blockValidator.ValidateBodyInIsolation(blockHash)
 	if err != nil {
 		return err
 	}
-
+	err = bp.blockValidator.ValidateBodyInContext(blockHash)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -42,6 +74,10 @@ func (bp *blockProcessor) insertBlock(block *externalapi.DomainBlock) error {
 	return nil
 }
 
-func (bp *blockProcessor) processNonValidBlock(block *externalapi.DomainBlock, blockStatus model.BlockStatus) error {
+func (bp *blockProcessor) discardAllChanges() {
+
+}
+
+func (bp *blockProcessor) commitAllChanges() error {
 	return nil
 }
