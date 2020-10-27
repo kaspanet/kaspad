e5b2f342bf627783cde1e36e63af56c913c942df
stasatdaglabs <stas@daglabs.com>
2020-10-27 16:32:40 +0200

    [NOD-1416] Add a comment about LogAndMeasureExecutionTime.

--- infrastructure/logger/utils.go
+++ infrastructure/logger/utils.go
@@ -4,6 +4,10 @@ import (
 	"time"
 )
 
+// LogAndMeasureExecutionTime logs that `functionName` has
+// started. The user is expected to defer `onEnd`, which
+// will then log that the function has ended, as well as
+// the time duration the function had ran.
 func LogAndMeasureExecutionTime(log *Logger, functionName string) (onEnd func()) {
 	start := time.Now()
 	log.Debugf("%s start", functionName)
