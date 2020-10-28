9cdfcef30f9098e714bf101b801ae7d92672b383
Ori Newman <orinewman1@gmail.com>
2020-10-28 12:38:44 +0200

    [NOD-1490] Implement database manager

---
+++ domain/consensus/dbmanager/bucket.go
@@ -0,0 +1,30 @@
+package dbmanager
+
+import (
+	"github.com/kaspanet/kaspad/domain/consensus/model"
+	"github.com/kaspanet/kaspad/infrastructure/db/database"
+)
+
+func dbBucketToDatabaseBucket(bucket model.DBBucket) *database.Bucket {
+	return database.MakeBucket(bucket.Path())
+}
+
+type dbBucket struct {
+	bucket *database.Bucket
+}
+
+func (d dbBucket) Bucket(bucketBytes []byte) model.DBBucket {
+	return newDBBucket(d.bucket.Bucket(bucketBytes))
+}
+
+func (d dbBucket) Key(suffix []byte) model.DBKey {
+	return newDBKey(d.bucket.Key(suffix))
+}
+
+func (d dbBucket) Path() []byte {
+	return d.bucket.Path()
+}
+
+func newDBBucket(bucket *database.Bucket) model.DBBucket {
+	return &dbBucket{bucket: bucket}
+}
