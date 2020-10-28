e26949e76bc8e4c9f389cc4ee76330e041aa73e5
Ori Newman <orinewman1@gmail.com>
2020-10-28 12:07:28 +0200

    [NOD-1417] Remove redundant copyright message

--- domain/consensus/processes/reachabilitymanager/log.go
+++ domain/consensus/processes/reachabilitymanager/log.go
@@ -1,8 +1,3 @@
-// Copyright (c) 2013-2017 The btcsuite developers
-// Copyright (c) 2017 The Decred developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
 package reachabilitymanager
 
 import (
