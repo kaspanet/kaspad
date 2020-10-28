4256d5948a05d0fb353007d3b3749ee59348484c
Ori Newman <orinewman1@gmail.com>
2020-10-28 11:21:43 +0200

    [NOD-1488] Fix block header store

--- domain/consensus/datastructures/blockheaderstore/blockheaderstore.go
+++ domain/consensus/datastructures/blockheaderstore/blockheaderstore.go
@@ -27,26 +27,26 @@ func (bms *blockHeaderStore) Discard() {
 	panic("implement me")
 }
 
-func (bms *blockHeaderStore) Commit(dbTx model.DBTxProxy) error {
+func (bms *blockHeaderStore) Commit(dbTx model.DBTransaction) error {
 	panic("implement me")
 }
 
 // BlockHeader gets the block header associated with the given blockHash
-func (bms *blockHeaderStore) BlockHeader(dbContext model.DBContextProxy, blockHash *externalapi.DomainHash) (*externalapi.DomainBlockHeader, error) {
+func (bms *blockHeaderStore) BlockHeader(dbContext model.DBReader, blockHash *externalapi.DomainHash) (*externalapi.DomainBlockHeader, error) {
 	panic("implement me")
 }
 
 // HasBlock returns whether a block header with a given hash exists in the store.
-func (bms *blockHeaderStore) HasBlockHeader(dbContext model.DBContextProxy, blockHash *externalapi.DomainHash) (bool, error) {
+func (bms *blockHeaderStore) HasBlockHeader(dbContext model.DBReader, blockHash *externalapi.DomainHash) (bool, error) {
 	panic("implement me")
 }
 
 // BlockHeaders gets the block headers associated with the given blockHashes
-func (bms *blockHeaderStore) BlockHeaders(dbContext model.DBContextProxy, blockHashes []*externalapi.DomainHash) ([]*externalapi.DomainBlockHeader, error) {
+func (bms *blockHeaderStore) BlockHeaders(dbContext model.DBReader, blockHashes []*externalapi.DomainHash) ([]*externalapi.DomainBlockHeader, error) {
 	panic("implement me")
 }
 
 // Delete deletes the block associated with the given blockHash
-func (bms *blockHeaderStore) Delete(dbTx model.DBTxProxy, blockHash *externalapi.DomainHash) error {
+func (bms *blockHeaderStore) Delete(dbTx model.DBTransaction, blockHash *externalapi.DomainHash) error {
 	panic("implement me")
 }
