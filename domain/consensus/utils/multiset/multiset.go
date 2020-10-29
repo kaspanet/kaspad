a9f52a8649b9d6a4216394c401d956ad5c2a97d3
Ori Newman <orinewman1@gmail.com>
2020-10-29 17:32:14 +0200

    [NOD-1493] Remove redundant functions

--- domain/consensus/utils/multiset/multiset.go
+++ domain/consensus/utils/multiset/multiset.go
@@ -1,16 +1,12 @@
 package multiset
 
 import (
-	"encoding/binary"
 	"github.com/kaspanet/go-secp256k1"
 	"github.com/kaspanet/kaspad/domain/consensus/model"
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 	"github.com/kaspanet/kaspad/domain/consensus/utils/hashes"
 	"github.com/pkg/errors"
-	"io"
 )
-
-var byteOrder = binary.LittleEndian
 
 type multiset struct {
 	ms *secp256k1.MultiSet
@@ -52,24 +48,4 @@ func FromBytes(multisetBytes []byte) (model.Multiset, error) {
 	}
 
 	return &multiset{ms: ms}, nil
-}
-
-// serializeMultiset serializes an ECMH multiset.
-func serializeMultiset(w io.Writer, ms *secp256k1.MultiSet) error {
-	serialized := ms.Serialize()
-	err := binary.Write(w, byteOrder, serialized)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// deserializeMultiset deserializes an EMCH multiset.
-func deserializeMultiset(r io.Reader) (*secp256k1.MultiSet, error) {
-	serialized := &secp256k1.SerializedMultiSet{}
-	err := binary.Read(r, byteOrder, serialized[:])
-	if err != nil {
-		return nil, err
-	}
-	return secp256k1.DeserializeMultiSet(serialized)
 }
