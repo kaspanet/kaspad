16d9faa15c0cacb2eaf5b482fa3a3baa8de2169f
stasatdaglabs <stas@daglabs.com>
2020-10-29 14:53:41 +0200

    [NOD-1493] Implement block serialization/deserialization.

---
+++ domain/consensus/database/serialization/block.go
@@ -0,0 +1,38 @@
+package serialization
+
+import (
+	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+)
+
+func DomainBlockToDbBlock(domainBlock *externalapi.DomainBlock) *DbBlock {
+	dbTransactions := make([]*DbTransaction, len(domainBlock.Transactions))
+	for i, domainTransaction := range domainBlock.Transactions {
+		dbTransactions[i] = DomainTransactionToDbTransaction(domainTransaction)
+	}
+
+	return &DbBlock{
+		Header:       DomainBlockHeaderToDbBlockHeader(domainBlock.Header),
+		Transactions: dbTransactions,
+	}
+}
+
+func DbBlockToDomainBlock(dbBlock *DbBlock) (*externalapi.DomainBlock, error) {
+	domainBlockHeader, err := DbBlockHeaderToDomainBlockHeader(dbBlock.Header)
+	if err != nil {
+		return nil, err
+	}
+
+	domainTransactions := make([]*externalapi.DomainTransaction, len(dbBlock.Transactions))
+	for i, dbTransaction := range dbBlock.Transactions {
+		var err error
+		domainTransactions[i], err = DbTransactionToDomainTransaction(dbTransaction)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &externalapi.DomainBlock{
+		Header:       domainBlockHeader,
+		Transactions: domainTransactions,
+	}, nil
+}
