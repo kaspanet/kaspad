93c12b1e23145f3c0f383671e8eda38181d959ee
stasatdaglabs <stas@daglabs.com>
2020-11-03 16:11:00 +0200

    [NOD-1501] Make go vet happy.

--- domain/consensus/model/externalapi/sync.go
+++ domain/consensus/model/externalapi/sync.go
@@ -1,17 +1,18 @@
 package externalapi
 
+// Each of the following represent one of the possible sync
+// states of the consensus
 const (
 	SyncStateRegular SyncState = iota
-
 	SyncStateHeadersFirst
-
 	SyncStateHeadersFirstAfterUTXOSet
-
 	SyncStateIBD
 )
 
+// SyncState represents the current sync state of the consensus
 type SyncState uint8
 
+// SyncInfo holds info about the current sync state of the consensus
 type SyncInfo struct {
 	State                SyncState
 	IBDRootUTXOBlockHash *DomainHash
