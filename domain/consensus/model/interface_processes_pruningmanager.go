5982632afebfa1a68865c833acb0d853c474b4c8
Elichai Turkel <elichai.turkel@gmail.com>
2020-11-03 13:19:21 +0200

    Update pruningmanager interface

--- domain/consensus/model/interface_processes_pruningmanager.go
+++ domain/consensus/model/interface_processes_pruningmanager.go
@@ -4,7 +4,7 @@ import "github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 
 // PruningManager resolves and manages the current pruning point
 type PruningManager interface {
-	FindNextPruningPoint(blockHash *externalapi.DomainHash) error
+	FindNextPruningPoint() error
 	PruningPoint() (*externalapi.DomainHash, error)
 	SerializedUTXOSet() ([]byte, error)
 }
