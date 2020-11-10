fe6c192aa87d0918555253986d4910643c092a78
Ori Newman <orinewman1@gmail.com>
2020-11-10 15:56:03 +0200

    [NOD-1525] Remove redundant methods from interface

--- app/protocol/flows/ibd/handle_request_ibd_root_utxo_set_and_block.go
+++ app/protocol/flows/ibd/handle_request_ibd_root_utxo_set_and_block.go
@@ -4,18 +4,13 @@ import (
 	"errors"
 	"github.com/kaspanet/kaspad/app/appmessage"
 	"github.com/kaspanet/kaspad/domain"
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 	"github.com/kaspanet/kaspad/domain/consensus/ruleerrors"
-	"github.com/kaspanet/kaspad/infrastructure/config"
 	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
 )
 
 // HandleRequestIBDRootUTXOSetAndBlockContext is the interface for the context needed for the HandleRequestIBDRootUTXOSetAndBlock flow.
 type HandleRequestIBDRootUTXOSetAndBlockContext interface {
 	Domain() domain.Domain
-	Config() *config.Config
-	OnNewBlock(block *externalapi.DomainBlock) error
-	FinishIBD()
 }
 
 type handleRequestIBDRootUTXOSetAndBlockFlow struct {
