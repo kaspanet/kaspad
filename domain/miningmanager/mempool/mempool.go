8ab74cc317b8f1173548a4a226d70606569abd7b
stasatdaglabs <stas@daglabs.com>
2020-11-10 16:07:17 +0200

    [NOD-1518] Set MaxTxVersion to 1.

--- domain/miningmanager/mempool/mempool.go
+++ domain/miningmanager/mempool/mempool.go
@@ -90,7 +90,7 @@ type mempool struct {
 // transactions until they are mined into a block.
 func New(consensus consensusexternalapi.Consensus) miningmanagermodel.Mempool {
 	policy := policy{
-		MaxTxVersion:    0,
+		MaxTxVersion:    1,
 		AcceptNonStd:    false,
 		MaxOrphanTxs:    5,
 		MaxOrphanTxSize: 100000,
