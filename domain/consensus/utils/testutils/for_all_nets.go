135ffbd4f29e794a2082bdbfb78b690da32f6d42
Svarog <feanorr@gmail.com>
2020-11-11 12:31:13 +0200

    [NOD-1529] Add getters + AddBlock to TestConsensus (#1025)
    * [NOD-1529] Add all stores and processes to consensus, and add access to TestConsensus
    
    * [NOD-1529] Move the getters of TestConsensus to separate file
    
    * [NOD-1529] Add AddBlock to TestConsensus
    
    * [NOD-1529] Update NewTestConsensus to be more all-encompassing
    
    * [NOD-1529] Remove test directory in teardown
    
    * [NOD-1529] Add ForAllNets function
    
    * [NOD-1529] Add comment

---
+++ domain/consensus/utils/testutils/for_all_nets.go
@@ -0,0 +1,27 @@
+package testutils
+
+import (
+	"testing"
+
+	"github.com/kaspanet/kaspad/domain/dagconfig"
+)
+
+// ForAllNets runs the passed testFunc with all available networks
+// if setDifficultyToMinumum = true - will modify the net params to have minimal difficulty, like in SimNet
+func ForAllNets(t *testing.T, setDifficultyToMinimum bool, testFunc func(*testing.T, *dagconfig.Params)) {
+	allParams := []dagconfig.Params{
+		dagconfig.MainnetParams,
+		dagconfig.TestnetParams,
+		dagconfig.SimnetParams,
+		dagconfig.DevnetParams,
+	}
+
+	for _, params := range allParams {
+		if setDifficultyToMinimum {
+			params.DisableDifficultyAdjustment = dagconfig.SimnetParams.DisableDifficultyAdjustment
+			params.TargetTimePerBlock = dagconfig.SimnetParams.TargetTimePerBlock
+		}
+
+		testFunc(t, &params)
+	}
+}
