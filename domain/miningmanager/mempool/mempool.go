4a2d04e49112b50ae2c41276e7b3f2e070ccceb6
stasatdaglabs <stas@daglabs.com>
2020-11-16 12:51:00 +0200

    [NOD-1538] Fix mempool not wrapping consensus errors.

--- domain/miningmanager/mempool/mempool.go
+++ domain/miningmanager/mempool/mempool.go
@@ -620,6 +620,9 @@ func (mp *mempool) maybeAcceptTransaction(tx *consensusexternalapi.DomainTransac
 		if errors.As(err, &missingOutpoints) {
 			return missingOutpoints.MissingOutpoints, nil, nil
 		}
+		if errors.As(err, &ruleerrors.RuleError{}) {
+			return nil, nil, txRuleError(RejectInvalid, err.Error())
+		}
 		return nil, nil, err
 	}
 
