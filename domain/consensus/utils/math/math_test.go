dd9ac849cd5af3fad68564135fe3f8169d93eb92
Ori Newman <orinewman1@gmail.com>
2020-11-16 18:57:56 +0200

    Add TestBigToCompact and TestCompactToBig

--- domain/consensus/utils/math/math_test.go
+++ domain/consensus/utils/math/math_test.go
@@ -1,6 +1,7 @@
 package math
 
 import (
+	"math/big"
 	"testing"
 )
 
@@ -30,6 +31,49 @@ func TestFastLog2Floor(t *testing.T) {
 
 		if test.expectedResult != actualResult {
 			t.Errorf("TestFastLog2Floor: %d: expected result: %d but got: %d", test.n, test.expectedResult, actualResult)
+		}
+	}
+}
+
+// TestBigToCompact ensures BigToCompact converts big integers to the expected
+// compact representation.
+func TestBigToCompact(t *testing.T) {
+	tests := []struct {
+		in  int64
+		out uint32
+	}{
+		{0, 0},
+		{-1, 25231360},
+	}
+
+	for x, test := range tests {
+		n := big.NewInt(test.in)
+		r := BigToCompact(n)
+		if r != test.out {
+			t.Errorf("TestBigToCompact test #%d failed: got %d want %d\n",
+				x, r, test.out)
+			return
+		}
+	}
+}
+
+// TestCompactToBig ensures CompactToBig converts numbers using the compact
+// representation to the expected big intergers.
+func TestCompactToBig(t *testing.T) {
+	tests := []struct {
+		in  uint32
+		out int64
+	}{
+		{10000000, 0},
+	}
+
+	for x, test := range tests {
+		n := CompactToBig(test.in)
+		want := big.NewInt(test.out)
+		if n.Cmp(want) != 0 {
+			t.Errorf("TestCompactToBig test #%d failed: got %d want %d\n",
+				x, n.Int64(), want.Int64())
+			return
 		}
 	}
 }
