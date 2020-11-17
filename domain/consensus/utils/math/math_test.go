47f6dfe4964b941f511ded0dffc0a88723c97271
Ori Newman <orinewman1@gmail.com>
2020-11-17 12:42:45 +0200

    Add tests

--- domain/consensus/utils/math/math_test.go
+++ domain/consensus/utils/math/math_test.go
@@ -1,6 +1,7 @@
 package math
 
 import (
+	"math"
 	"math/big"
 	"testing"
 )
@@ -39,15 +40,18 @@ func TestFastLog2Floor(t *testing.T) {
 // compact representation.
 func TestBigToCompact(t *testing.T) {
 	tests := []struct {
-		in  int64
+		in  string
 		out uint32
 	}{
-		{0, 0},
-		{-1, 25231360},
+		{"0", 0},
+		{"-1", 25231360},
+		{"9223372036854775807", 142606335},
+		{"922337203685477580712312312123487", 237861256},
 	}
 
 	for x, test := range tests {
-		n := big.NewInt(test.in)
+		n := new(big.Int)
+		n.SetString(test.in, 10)
 		r := BigToCompact(n)
 		if r != test.out {
 			t.Errorf("TestBigToCompact test #%d failed: got %d want %d\n",
@@ -58,21 +62,32 @@ func TestBigToCompact(t *testing.T) {
 }
 
 // TestCompactToBig ensures CompactToBig converts numbers using the compact
-// representation to the expected big intergers.
+// representation to the expected big integers.
 func TestCompactToBig(t *testing.T) {
 	tests := []struct {
 		in  uint32
-		out int64
+		out string
 	}{
-		{10000000, 0},
+		{0, "0"},
+		{10000000, "0"},
+		{math.MaxUint32, "-6311914495863998658485429352026283268468573753812676234178171506285465200675957" +
+			"87397376951158770808349115367298981082112562162319027637583517246275967980671962038665775867893645140" +
+			"22856089959012026469381002722748489975264028415685723882208353467651862351803217528553851158828320170" +
+			"89832330727351553686808317476632783024236208492771822246700842318520468733521003756809213629548010354" +
+			"33865968377930773213939300289069292503211567790599147939718451689002543278625341832829837474611074167" +
+			"86700705915281593002614032021233542099318559748885883681365573294332856023451874423425211080847063825" +
+			"199113186681992371681311588352",
+		},
+		{142606335, "9223370937343148032"},
+		{25231360, "-1"},
+		{237861256, "922337129789886856855791696084992"},
 	}
 
-	for x, test := range tests {
+	for i, test := range tests {
 		n := CompactToBig(test.in)
-		want := big.NewInt(test.out)
-		if n.Cmp(want) != 0 {
-			t.Errorf("TestCompactToBig test #%d failed: got %d want %d\n",
-				x, n.Int64(), want.Int64())
+		if n.String() != test.out {
+			t.Errorf("TestCompactToBig test #%d failed: got %s want %s",
+				i, n, test.out)
 			return
 		}
 	}
