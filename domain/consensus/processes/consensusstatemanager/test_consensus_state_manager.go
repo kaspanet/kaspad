b3a3121725da3a063e76d418c30a6ec39bb351eb
Elichai Turkel <elichai.turkel@gmail.com>
2020-11-22 12:30:27 +0200

    Add TestFinality back (#1129)
    * Add VirtualFinalityPoint to TestConsensusStateManager
    
    * Add TestFinality back

--- domain/consensus/processes/consensusstatemanager/test_consensus_state_manager.go
+++ domain/consensus/processes/consensusstatemanager/test_consensus_state_manager.go
@@ -23,3 +23,7 @@ func (csm testConsensusStateManager) AddUTXOToMultiset(
 func (csm testConsensusStateManager) ResolveBlockStatus(blockHash *externalapi.DomainHash) (externalapi.BlockStatus, error) {
 	return csm.resolveBlockStatus(blockHash)
 }
+
+func (csm testConsensusStateManager) VirtualFinalityPoint() (*externalapi.DomainHash, error) {
+	return csm.virtualFinalityPoint()
+}
