9e935c930263c34ab580e85a3d84b3ea00262bdc
stasatdaglabs <stas@daglabs.com>
2020-11-24 15:34:39 +0200

    [NOD-1566] Implement a simpler LRU cache.

---
+++ domain/consensus/utils/lrucache/lrucache.go
@@ -0,0 +1,45 @@
+package lrucache
+
+import (
+	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+)
+
+type LRUCache struct {
+	cache    map[externalapi.DomainHash]interface{}
+	capacity int
+}
+
+func New(capacity int) *LRUCache {
+	return &LRUCache{
+		cache:    make(map[externalapi.DomainHash]interface{}, capacity+1),
+		capacity: capacity,
+	}
+}
+
+func (c *LRUCache) Add(key *externalapi.DomainHash, data interface{}) {
+	c.cache[*key] = data
+
+	if len(c.cache) > c.capacity {
+		c.evictRandom()
+	}
+}
+
+func (c *LRUCache) Get(key *externalapi.DomainHash) (interface{}, bool) {
+	data, ok := c.cache[*key]
+	if !ok {
+		return nil, false
+	}
+	return data, true
+}
+
+func (c *LRUCache) Remove(key *externalapi.DomainHash) {
+	delete(c.cache, *key)
+}
+
+func (c *LRUCache) evictRandom() {
+	var keyToEvict externalapi.DomainHash
+	for key := range c.cache {
+		keyToEvict = key
+	}
+	c.Remove(&keyToEvict)
+}
