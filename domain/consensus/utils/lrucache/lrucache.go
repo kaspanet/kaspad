5f244d9bcaf7dd279dc8c3df457c0b9f23016e57
stasatdaglabs <stas@daglabs.com>
2020-11-25 10:46:42 +0200

    [NOD-1566] Make go vet happy.

--- domain/consensus/utils/lrucache/lrucache.go
+++ domain/consensus/utils/lrucache/lrucache.go
@@ -4,11 +4,14 @@ import (
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 )
 
+// LRUCache is a least-recently-used cache for any type
+// that's able to be indexed by DomainHash
 type LRUCache struct {
 	cache    map[externalapi.DomainHash]interface{}
 	capacity int
 }
 
+// New creates a new LRUCache
 func New(capacity int) *LRUCache {
 	return &LRUCache{
 		cache:    make(map[externalapi.DomainHash]interface{}, capacity+1),
@@ -16,6 +19,7 @@ func New(capacity int) *LRUCache {
 	}
 }
 
+// Add adds an entry to the LRUCache
 func (c *LRUCache) Add(key *externalapi.DomainHash, value interface{}) {
 	c.cache[*key] = value
 
@@ -24,6 +28,7 @@ func (c *LRUCache) Add(key *externalapi.DomainHash, value interface{}) {
 	}
 }
 
+// Get returns the entry for the given key, or (nil, false) otherwise
 func (c *LRUCache) Get(key *externalapi.DomainHash) (interface{}, bool) {
 	value, ok := c.cache[*key]
 	if !ok {
@@ -32,11 +37,14 @@ func (c *LRUCache) Get(key *externalapi.DomainHash) (interface{}, bool) {
 	return value, true
 }
 
+// Has returns whether the LRUCache contains the given key
 func (c *LRUCache) Has(key *externalapi.DomainHash) bool {
 	_, ok := c.cache[*key]
 	return ok
 }
 
+// Remove removes the entry for the the given key. Does nothing if
+// the entry does not exist
 func (c *LRUCache) Remove(key *externalapi.DomainHash) {
 	delete(c.cache, *key)
 }
