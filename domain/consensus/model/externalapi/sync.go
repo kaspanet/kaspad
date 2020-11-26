003a9b97c02c8cacb44300d49f1e0c52c2e81ef5
Ori Newman <orinewman1@gmail.com>
2020-11-26 15:24:02 +0200

    [NOD-1575] Add missing Equals

--- domain/consensus/model/externalapi/sync.go
+++ domain/consensus/model/externalapi/sync.go
@@ -37,6 +37,15 @@ func (s SyncState) Clone() SyncState {
 	return s
 }
 
+// If this doesn't compile, it means the type definition has been changed, so it's
+// an indication to update Equal accordingly.
+var _ SyncState = 0
+
+// Equal returns whether si equals to other
+func (s SyncState) Equal(other SyncState) bool {
+	return s == other
+}
+
 // SyncInfo holds info about the current sync state of the consensus
 type SyncInfo struct {
 	State                SyncState
@@ -53,4 +62,33 @@ func (si *SyncInfo) Clone() *SyncInfo {
 		HeaderCount:          si.HeaderCount,
 		BlockCount:           si.BlockCount,
 	}
+}
+
+// If this doesn't compile, it means the type definition has been changed, so it's
+// an indication to update Equal accordingly.
+var _ = SyncInfo{SyncState(0), &DomainHash{}, 0, 0}
+
+// Equal returns whether si equals to other
+func (si *SyncInfo) Equal(other *SyncInfo) bool {
+	if si == nil || other == nil {
+		return si == other
+	}
+
+	if !si.State.Equal(other.State) {
+		return false
+	}
+
+	if !si.IBDRootUTXOBlockHash.Equal(other.IBDRootUTXOBlockHash) {
+		return false
+	}
+
+	if si.HeaderCount != other.HeaderCount {
+		return false
+	}
+
+	if si.BlockCount != other.BlockCount {
+		return false
+	}
+
+	return true
 }
