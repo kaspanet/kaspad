3f2e1ff88420ae464dbde45bfa9872ccd8eede9a
Mike Zak <feanorr@gmail.com>
2020-12-06 10:51:40 +0200

    [NOD-1587] Add DAGParams to TestConsensus

--- domain/consensus/model/testapi/test_consensus.go
+++ domain/consensus/model/testapi/test_consensus.go
@@ -3,12 +3,14 @@ package testapi
 import (
 	"github.com/kaspanet/kaspad/domain/consensus/model"
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+	"github.com/kaspanet/kaspad/domain/dagconfig"
 )
 
 // TestConsensus wraps the Consensus interface with some methods that are needed by tests only
 type TestConsensus interface {
 	externalapi.Consensus
 
+	DAGParams() *dagconfig.Params
 	DatabaseContext() model.DBReader
 
 	BuildBlockWithParents(parentHashes []*externalapi.DomainHash, coinbaseData *externalapi.DomainCoinbaseData,
