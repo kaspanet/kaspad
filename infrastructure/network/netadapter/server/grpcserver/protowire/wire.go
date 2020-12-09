c1e5d279cc77155204fd8ba054925f60fe89d92c
stasatdaglabs <stas@daglabs.com>
2020-12-09 16:08:45 +0200

    [NOD-1596] Return an error on an unknown field.

--- infrastructure/network/netadapter/server/grpcserver/protowire/wire.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/wire.go
@@ -11,6 +11,9 @@ type converter interface {
 
 // ToAppMessage converts a KaspadMessage to its appmessage.Message representation
 func (x *KaspadMessage) ToAppMessage() (appmessage.Message, error) {
+	if len(x.unknownFields) > 0 {
+		return nil, errors.Errorf("received invalid message")
+	}
 	appMessage, err := x.Payload.(converter).toAppMessage()
 	if err != nil {
 		return nil, err
