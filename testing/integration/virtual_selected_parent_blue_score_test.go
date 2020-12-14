d62e3e0546676bc3b3c0c55bf58a4967ea0acdd1
stasatdaglabs <stas@daglabs.com>
2020-12-14 17:29:00 +0200

    [NOD-1597] Expand TestVirtualSelectedParentBlueScore.

--- testing/integration/virtual_selected_parent_blue_score_test.go
+++ testing/integration/virtual_selected_parent_blue_score_test.go
@@ -1,6 +1,9 @@
 package integration
 
-import "testing"
+import (
+	"github.com/kaspanet/kaspad/app/appmessage"
+	"testing"
+)
 
 func TestVirtualSelectedParentBlueScore(t *testing.T) {
 	// Setup a single kaspad instance
@@ -24,10 +27,27 @@ func TestVirtualSelectedParentBlueScore(t *testing.T) {
 			1, response.BlueScore)
 	}
 
-	// Mine some blocks
+	// Register to virtual selected parent blue score changes
+	onVirtualSelectedParentBlueScoreChangedChan := make(chan *appmessage.VirtualSelectedParentBlueScoreChangedNotificationMessage)
+	err = kaspad.rpcClient.RegisterForVirtualSelectedParentBlueScoreChangedNotifications(
+		func(notification *appmessage.VirtualSelectedParentBlueScoreChangedNotificationMessage) {
+			onVirtualSelectedParentBlueScoreChangedChan <- notification
+		})
+	if err != nil {
+		t.Fatalf("Failed to register for virtual selected parent "+
+			"blue score change notifications: %s", err)
+	}
+
+	// Mine some blocks and make sure that the notifications
+	// report correct blue score values
 	const blockAmountToMine = 100
 	for i := 0; i < blockAmountToMine; i++ {
 		mineNextBlock(t, kaspad)
+		notification := <-onVirtualSelectedParentBlueScoreChangedChan
+		if notification.VirtualSelectedParentBlueScore != 1+uint64(i) {
+			t.Fatalf("Unexpected virtual selected parent blue score. Want: %d, got: %d",
+				1+i, response.BlueScore)
+		}
 	}
 
 	// Make sure that the blue score after all that mining is as expected
