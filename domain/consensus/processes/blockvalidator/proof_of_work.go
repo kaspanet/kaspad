72f7aafb629255306c779c0738c3c65b70431d35
Ori Newman <orinewman1@gmail.com>
2020-12-14 15:03:45 +0200

    Fix imports

--- domain/consensus/processes/blockvalidator/proof_of_work.go
+++ domain/consensus/processes/blockvalidator/proof_of_work.go
@@ -4,8 +4,8 @@ import (
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 	"github.com/kaspanet/kaspad/domain/consensus/model/pow"
 	"github.com/kaspanet/kaspad/domain/consensus/ruleerrors"
-	"github.com/kaspanet/kaspad/infrastructure/logger"
 	"github.com/kaspanet/kaspad/domain/consensus/utils/consensushashing"
+	"github.com/kaspanet/kaspad/infrastructure/logger"
 	"github.com/kaspanet/kaspad/util"
 	"github.com/pkg/errors"
 )
