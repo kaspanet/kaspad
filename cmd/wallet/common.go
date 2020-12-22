6270d9fb99c385c4ea50980ddc24ec231b616a99
stasatdaglabs <stas@daglabs.com>
2020-12-22 17:01:25 +0200

    Fix isUTXOSpendable not checking whether a UTXO is of a coinbase transaction.

--- cmd/wallet/common.go
+++ cmd/wallet/common.go
@@ -5,6 +5,9 @@ import "github.com/kaspanet/kaspad/app/appmessage"
 const minConfirmations = 100
 
 func isUTXOSpendable(entry *appmessage.UTXOsByAddressesEntry, virtualSelectedParentBlueScore uint64) bool {
+	if !entry.UTXOEntry.IsCoinbase {
+		return true
+	}
 	blockBlueScore := entry.UTXOEntry.BlockBlueScore
 	return blockBlueScore+minConfirmations < virtualSelectedParentBlueScore
 }
