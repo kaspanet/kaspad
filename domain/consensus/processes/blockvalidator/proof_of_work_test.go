1a4683b7a35405a54d0f47cb8135d8c1b7f18e07
tal <tal@daglabs.com>
2021-01-10 15:02:46 +0200

    Delete function "solveBlock" on proof_of_work_test.go and call the function mining.SolvaBlock instead. Also, remove using of random in "solveBlockWithWrongPOW" function.

--- domain/consensus/processes/blockvalidator/proof_of_work_test.go
+++ domain/consensus/processes/blockvalidator/proof_of_work_test.go
@@ -3,9 +3,10 @@ package blockvalidator_test
 import (
 	"github.com/kaspanet/kaspad/domain/consensus/model/pow"
 	"github.com/kaspanet/kaspad/domain/consensus/ruleerrors"
+	"github.com/kaspanet/kaspad/domain/consensus/utils/mining"
 	"github.com/kaspanet/kaspad/util"
-
 	"math/rand"
+
 	"testing"
 
 	"github.com/kaspanet/kaspad/domain/consensus"
@@ -15,37 +16,38 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Set the flag "skip pow" to be false (second argument in the function)
+// Set the flag "skip pow" to be false (second argument in the function) for not skipping the check of POW and validate its correctness.
 func TestPOW(t *testing.T) {
 	testutils.ForAllNets(t, false, func(t *testing.T, params *dagconfig.Params) {
-		// First, it checks the validation of the genesis's POW.
 		factory := consensus.NewFactory()
 		tc, teardown, err := factory.NewTestConsensus(params, "TestPOW")
 		if err != nil {
 			t.Fatalf("Error setting up consensus: %+v", err)
 		}
 		defer teardown(false)
+
 		// Builds and checks block with invalid POW.
 		invalidBlockWrongPOW, _, err := tc.BuildBlockWithParents([]*externalapi.DomainHash{params.GenesisHash}, nil, nil)
-		invalidBlockWrongPOW = solveBlockWithWrongPOW(invalidBlockWrongPOW)
 		if err != nil {
 			t.Fatal(err)
 		}
+		invalidBlockWrongPOW = solveBlockWithWrongPOW(invalidBlockWrongPOW)
 		_, err = tc.ValidateAndInsertBlock(invalidBlockWrongPOW)
 		if !errors.Is(err, ruleerrors.ErrInvalidPoW) {
-			t.Fatalf("Expected block to be invalid with err: %v, instead found: %v", ruleerrors.ErrTimeTooOld, err)
+			t.Fatalf("Expected block to be invalid with err: %v, instead found: %v", ruleerrors.ErrInvalidPoW, err)
 		}
+
 		// test on a valid block.
 		validBlock, _, err := tc.BuildBlockWithParents([]*externalapi.DomainHash{params.GenesisHash}, nil, nil)
-		validBlock = solveBlock(validBlock)
 		if err != nil {
 			t.Fatal(err)
 		}
+		random := rand.New(rand.NewSource(0))
+		mining.SolveBlock(validBlock, random)
 		_, err = tc.ValidateAndInsertBlock(validBlock)
 		if err != nil {
 			t.Fatal(err)
 		}
-
 	})
 }
 
@@ -53,8 +55,8 @@ func TestPOW(t *testing.T) {
 func solveBlockWithWrongPOW(block *externalapi.DomainBlock) *externalapi.DomainBlock {
 	targetDifficulty := util.CompactToBig(block.Header.Bits())
 	headerForMining := block.Header.ToMutable()
-	initialNonce := rand.Uint64()
-	for i := initialNonce; i != initialNonce-1; i++ {
+	initialNonce := uint64(0)
+	for i := initialNonce; i < 0xFFFFFFFFFFFFFFFF; i++ {
 		headerForMining.SetNonce(i)
 		if !pow.CheckProofOfWorkWithTarget(headerForMining, targetDifficulty) {
 			block.Header = headerForMining.ToImmutable()
@@ -63,19 +65,4 @@ func solveBlockWithWrongPOW(block *externalapi.DomainBlock) *externalapi.DomainB
 	}
 
 	panic("Failed to solve block! cannot find a invalid POW for the test")
-}
-
-// solveBlock increments the given block's nonce until it gets valid POW.
-func solveBlock(block *externalapi.DomainBlock) *externalapi.DomainBlock {
-	targetDifficulty := util.CompactToBig(block.Header.Bits())
-	headerForMining := block.Header.ToMutable()
-	initialNonce := rand.Uint64()
-	for i := initialNonce; i != initialNonce-1; i++ {
-		headerForMining.SetNonce(i)
-		if pow.CheckProofOfWorkWithTarget(headerForMining, targetDifficulty) {
-			block.Header = headerForMining.ToImmutable()
-			return block
-		}
-	}
-	panic("Failed to solve block! cannot find a valid POW ")
 }
