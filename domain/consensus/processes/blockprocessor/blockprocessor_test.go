302fb557d33a4699cbfd73e5e37d468789a104d5
Anastasiia <anastasiia.orji@geoguard.com>
2021-01-12 23:47:27 +0200

    update test with original v0.8.4-dev

--- domain/consensus/processes/blockprocessor/blockprocessor_test.go
+++ domain/consensus/processes/blockprocessor/blockprocessor_test.go
@@ -4,80 +4,80 @@ import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/dagconfig"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/txscript"
 	"github.com/kaspanet/go-secp256k1"
+	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+	"github.com/kaspanet/kaspad/domain/consensus/utils/txscript"
+	"github.com/kaspanet/kaspad/domain/dagconfig"
 
-	"github.com/kaspanet/kaspad/util"
+	consensusdatabase "github.com/kaspanet/kaspad/domain/consensus/database"
 	"github.com/kaspanet/kaspad/domain/consensus/datastructures/acceptancedatastore"
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockheaderstore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockrelationstore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockstatusstore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockstore"	
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockheaderstore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockrelationstore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockstatusstore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/blockstore"
 	"github.com/kaspanet/kaspad/domain/consensus/datastructures/consensusstatestore"
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/ghostdagdatastore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/multisetstore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/pruningstore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/reachabilitydatastore"	
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/utxodiffstore"	
-	"github.com/kaspanet/kaspad/domain/consensus/model"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/blockprocessor"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/blockvalidator"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/coinbasemanager"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/consensusstatemanager"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/dagtopologymanager"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/dagtraversalmanager"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/difficultymanager"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/ghostdagmanager"		
-	"github.com/kaspanet/kaspad/domain/consensus/processes/pastmediantimemanager"
-	"github.com/kaspanet/kaspad/domain/consensus/processes/pruningmanager"		
-	"github.com/kaspanet/kaspad/domain/consensus/processes/reachabilitymanager"	
-	"github.com/kaspanet/kaspad/domain/consensus/processes/transactionvalidator"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/finalitystore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/ghostdagdatastore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/headersselectedtipstore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/multisetstore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/pruningstore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/reachabilitydatastore"
+	"github.com/kaspanet/kaspad/domain/consensus/datastructures/utxodiffstore"
+	"github.com/kaspanet/kaspad/domain/consensus/model"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/blockbuilder"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/blockprocessor"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/blockvalidator"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/coinbasemanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/consensusstatemanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/dagtopologymanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/dagtraversalmanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/difficultymanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/finalitymanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/ghostdagmanager"
 	"github.com/kaspanet/kaspad/domain/consensus/processes/headersselectedtipmanager"
 	"github.com/kaspanet/kaspad/domain/consensus/processes/mergedepthmanager"
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/finalitystore"
-	"github.com/kaspanet/kaspad/domain/consensus/processes/finalitymanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/pastmediantimemanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/pruningmanager"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/reachabilitymanager"
 	"github.com/kaspanet/kaspad/domain/consensus/processes/syncmanager"
-	"github.com/kaspanet/kaspad/domain/consensus/processes/blockbuilder"
-	"github.com/kaspanet/kaspad/domain/consensus/datastructures/headersselectedtipstore"
+	"github.com/kaspanet/kaspad/domain/consensus/processes/transactionvalidator"
 	"github.com/kaspanet/kaspad/infrastructure/db/database/ldb"
-	consensusdatabase "github.com/kaspanet/kaspad/domain/consensus/database"
-
+	"github.com/kaspanet/kaspad/util"
 )
 
 type testContext struct {
-	GenesisHash				*externalapi.DomainHash
-	DbManager				model.DBManager
-	PruningManager			model.PruningManager
-	BlockValidator			model.BlockValidator
-	DagTopologyManager		model.DAGTopologyManager
-	ReachabilityManager		model.ReachabilityManager
-	DifficultyManager		model.DifficultyManager
-	PastMedianTimeManager	model.PastMedianTimeManager
-	GhostdagManager			model.GHOSTDAGManager
-	CoinbaseManager			model.CoinbaseManager
-	HeaderTipsManager		model.HeadersSelectedTipManager
-	SyncManager				model.SyncManager
-	AcceptanceDataStore		model.AcceptanceDataStore
-	BlockStore				model.BlockStore
-	BlockStatusStore		model.BlockStatusStore
-	BlockRelationStore		model.BlockRelationStore
-	MultisetStore			model.MultisetStore
-	GhostdagDataStore		model.GHOSTDAGDataStore
-	ConsensusStateStore		model.ConsensusStateStore
-	PruningStore			model.PruningStore
-	ReachabilityDataStore	model.ReachabilityDataStore
-	UtxoDiffStore			model.UTXODiffStore
-	BlockHeaderStore		model.BlockHeaderStore
-	HeadersSelectedTipStore	model.HeaderSelectedTipStore
-	FinalityStore			model.FinalityStore
-	ConsensusStateManager	model.ConsensusStateManager
-	GenesisBlock			*externalapi.DomainBlock
+	GenesisHash             *externalapi.DomainHash
+	DbManager               model.DBManager
+	PruningManager          model.PruningManager
+	BlockValidator          model.BlockValidator
+	DagTopologyManager      model.DAGTopologyManager
+	ReachabilityManager     model.ReachabilityManager
+	DifficultyManager       model.DifficultyManager
+	PastMedianTimeManager   model.PastMedianTimeManager
+	GhostdagManager         model.GHOSTDAGManager
+	CoinbaseManager         model.CoinbaseManager
+	HeaderTipsManager       model.HeadersSelectedTipManager
+	SyncManager             model.SyncManager
+	AcceptanceDataStore     model.AcceptanceDataStore
+	BlockStore              model.BlockStore
+	BlockStatusStore        model.BlockStatusStore
+	BlockRelationStore      model.BlockRelationStore
+	MultisetStore           model.MultisetStore
+	GhostdagDataStore       model.GHOSTDAGDataStore
+	ConsensusStateStore     model.ConsensusStateStore
+	PruningStore            model.PruningStore
+	ReachabilityDataStore   model.ReachabilityDataStore
+	UtxoDiffStore           model.UTXODiffStore
+	BlockHeaderStore        model.BlockHeaderStore
+	HeadersSelectedTipStore model.HeaderSelectedTipStore
+	FinalityStore           model.FinalityStore
+	ConsensusStateManager   model.ConsensusStateManager
+	GenesisBlock            *externalapi.DomainBlock
 }
 
 func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*testContext, error) {
 	// Data Structures
+	pruningWindowSizeForCaches := int(dagParams.PruningDepth())
 	acceptanceDataStore := acceptancedatastore.New(200)
 	blockStore, err := blockstore.New(dbManager, 200)
 	if err != nil {
@@ -87,17 +87,17 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 	if err != nil {
 		return nil, err
 	}
-	blockRelationStore := blockrelationstore.New(200)
+	blockRelationStore := blockrelationstore.New(pruningWindowSizeForCaches)
 	blockStatusStore := blockstatusstore.New(200)
 	multisetStore := multisetstore.New(200)
 	pruningStore := pruningstore.New()
-	reachabilityDataStore := reachabilitydatastore.New(200)
+	reachabilityDataStore := reachabilitydatastore.New(pruningWindowSizeForCaches)
 	utxoDiffStore := utxodiffstore.New(200)
-	consensusStateStore := consensusstatestore.New()
-	ghostdagDataStore := ghostdagdatastore.New(200)
+	consensusStateStore := consensusstatestore.New(200)
+	ghostdagDataStore := ghostdagdatastore.New(pruningWindowSizeForCaches)
 	headersSelectedTipStore := headersselectedtipstore.New()
 	finalityStore := finalitystore.New(200)
-	
+
 	// Processes
 	reachabilityManager := reachabilitymanager.New(
 		dbManager,
@@ -118,6 +118,7 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		dbManager,
 		dagTopologyManager,
 		ghostdagDataStore,
+		reachabilityDataStore,
 		ghostdagManager)
 	pastMedianTimeManager := pastmediantimemanager.New(
 		dagParams.TimestampDeviationTolerance,
@@ -177,7 +178,7 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		dagParams.MaxBlockParents,
 		dagParams.TimestampDeviationTolerance,
 		dagParams.TargetTimePerBlock,
-	
+
 		dbManager,
 		difficultyManager,
 		pastMedianTimeManager,
@@ -188,7 +189,7 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		coinbaseManager,
 		mergeDepthManager,
 		reachabilityManager,
-	
+
 		pruningStore,
 		blockStore,
 		ghostdagDataStore,
@@ -203,7 +204,7 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		dagParams.MaxBlockParents,
 		dagParams.MergeSetSizeLimit,
 		genesisHash,
-	
+
 		ghostdagManager,
 		dagTopologyManager,
 		dagTraversalManager,
@@ -214,7 +215,7 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		coinbaseManager,
 		mergeDepthManager,
 		finalityManager,
-	
+
 		blockStatusStore,
 		ghostdagDataStore,
 		consensusStateStore,
@@ -243,6 +244,7 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		multisetStore,
 		acceptanceDataStore,
 		blockStore,
+		blockHeaderStore,
 		utxoDiffStore,
 		genesisHash,
 		dagParams.FinalityDepth(),
@@ -261,37 +263,37 @@ func setupTestContext(dagParams *dagconfig.Params, dbManager model.DBManager) (*
 		pruningStore)
 
 	return &testContext{
-		GenesisHash: genesisHash,
-		DbManager: dbManager,
-		ConsensusStateManager: consensusStateManager,
-		PruningManager: pruningManager,
-		BlockValidator: blockValidator,
-		DagTopologyManager: dagTopologyManager,
-		ReachabilityManager: reachabilityManager,
-		DifficultyManager: difficultyManager,
-		PastMedianTimeManager: pastMedianTimeManager,
-		GhostdagManager: ghostdagManager,
-		CoinbaseManager: coinbaseManager,
-		HeaderTipsManager: headerTipsManager,
-		SyncManager: syncManager,
-		AcceptanceDataStore: acceptanceDataStore,
-		BlockStore: blockStore,
-		BlockStatusStore: blockStatusStore,
-		BlockRelationStore: blockRelationStore,
-		MultisetStore: multisetStore,
-		GhostdagDataStore: ghostdagDataStore,
-		ConsensusStateStore: consensusStateStore,
-		PruningStore: pruningStore,
-		ReachabilityDataStore: reachabilityDataStore,
-		UtxoDiffStore: utxoDiffStore,
-		BlockHeaderStore: blockHeaderStore,
+		GenesisHash:             genesisHash,
+		DbManager:               dbManager,
+		ConsensusStateManager:   consensusStateManager,
+		PruningManager:          pruningManager,
+		BlockValidator:          blockValidator,
+		DagTopologyManager:      dagTopologyManager,
+		ReachabilityManager:     reachabilityManager,
+		DifficultyManager:       difficultyManager,
+		PastMedianTimeManager:   pastMedianTimeManager,
+		GhostdagManager:         ghostdagManager,
+		CoinbaseManager:         coinbaseManager,
+		HeaderTipsManager:       headerTipsManager,
+		SyncManager:             syncManager,
+		AcceptanceDataStore:     acceptanceDataStore,
+		BlockStore:              blockStore,
+		BlockStatusStore:        blockStatusStore,
+		BlockRelationStore:      blockRelationStore,
+		MultisetStore:           multisetStore,
+		GhostdagDataStore:       ghostdagDataStore,
+		ConsensusStateStore:     consensusStateStore,
+		PruningStore:            pruningStore,
+		ReachabilityDataStore:   reachabilityDataStore,
+		UtxoDiffStore:           utxoDiffStore,
+		BlockHeaderStore:        blockHeaderStore,
 		HeadersSelectedTipStore: headersSelectedTipStore,
-		FinalityStore: finalityStore,
-		GenesisBlock: dagParams.GenesisBlock,
+		FinalityStore:           finalityStore,
+		GenesisBlock:            dagParams.GenesisBlock,
 	}, nil
 }
 
-func SetupBlockBuilder(testContext *testContext) (model.BlockBuilder) {
+func SetupBlockBuilder(testContext *testContext) model.BlockBuilder {
 	blockBuilder := blockbuilder.New(
 		testContext.DbManager,
 		testContext.DifficultyManager,
@@ -337,13 +339,12 @@ func SetupBlockProcessor(testContext *testContext) (model.BlockProcessor, error)
 		testContext.HeadersSelectedTipStore,
 		testContext.FinalityStore)
 
-		_, err := blockProcessor.ValidateAndInsertBlock(testContext.GenesisBlock)
-		if err != nil {
-			return nil, err
-		}
+	_, err := blockProcessor.ValidateAndInsertBlock(testContext.GenesisBlock)
+	if err != nil {
+		return nil, err
+	}
 	return blockProcessor, nil
 }
-
 
 func SetupLDB(testName string) (*ldb.LevelDB, func(), error) {
 	// Create a temp db to run tests against
@@ -381,7 +382,7 @@ func generatePubKey() (*secp256k1.SchnorrKeyPair, *secp256k1.SchnorrPublicKey, e
 	return privKey, publicKey, nil
 }
 
-func generateScriptPublicKey(publicKey *secp256k1.SchnorrPublicKey) ([]byte, error) {
+func generateScriptPublicKey(publicKey *secp256k1.SchnorrPublicKey) (*externalapi.ScriptPublicKey, error) {
 	serializedPubKey, err := publicKey.Serialize()
 	if err != nil {
 		return nil, err
@@ -397,8 +398,8 @@ func generateScriptPublicKey(publicKey *secp256k1.SchnorrPublicKey) ([]byte, err
 	return scriptPublicKey, nil
 }
 
-func NewCoinbaseData(scriptPublicKey []byte) (*externalapi.DomainCoinbaseData, error) {
-	return &externalapi.DomainCoinbaseData {
+func NewCoinbaseData(scriptPublicKey *externalapi.ScriptPublicKey) (*externalapi.DomainCoinbaseData, error) {
+	return &externalapi.DomainCoinbaseData{
 		ScriptPublicKey: scriptPublicKey,
 	}, nil
 }
@@ -414,7 +415,7 @@ func SetupCoinbaseData() (*externalapi.DomainCoinbaseData, error) {
 	}
 	return NewCoinbaseData(scriptPublicKey)
 }
-											
+
 func TestBlockProcessor(t *testing.T) {
 	createChain := func(t *testing.T, numOfBlocks int) (model.BlockProcessor, []*externalapi.DomainBlock, func()) {
 		params := &dagconfig.TestnetParams
@@ -433,12 +434,14 @@ func TestBlockProcessor(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error setting up BlockProcessor: %+v", err)
 		}
-		blockProcessor = SetupBlockProcessor(dbManager, &dagconfig.SimnetParams)
 
-		coinbaseData := externalapi.DomainCoinbaseData{}
+		coinbaseData, _ := SetupCoinbaseData()
+		if err != nil {
+			t.Fatalf("error in SetupCoinbaseData: %+v", err)
+		}
 		blocks := make([]*externalapi.DomainBlock, numOfBlocks)
 		for i := range blocks {
-			block, err := blockBuilder.BuildBlock(&coinbaseData, nil)
+			block, err := blockBuilder.BuildBlock(coinbaseData, nil)
 			if err != nil {
 				t.Fatalf("error in BuildBlock: %+v", err)
 			}
@@ -467,23 +470,25 @@ func TestBlockProcessor(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error setting up BlockProcessor: %+v", err)
 		}
-		blockProcessor := SetupBlockProcessor(dbManager, &dagconfig.SimnetParams)
 		defer teardownFunc()
-		
+
 		// create block
-		coinbaseData := &externalapi.DomainCoinbaseData{}
+		coinbaseData, _ := SetupCoinbaseData()
+		if err != nil {
+			t.Fatalf("error in SetupCoinbaseData: %+v", err)
+		}
 		block, err := blockBuilder.BuildBlock(coinbaseData, nil)
 		if err != nil {
 			t.Fatalf("error in BuildBlock: %+v", err)
 		}
-		
+
 		// process block
 		_, err = blockProcessor.ValidateAndInsertBlock(block)
 		if err != nil {
 			t.Fatalf("error in ValidateAndInsertBlock: %+v", err)
 		}
 	})
-	
+
 	t.Run("Test create and process 11 blocks", func(t *testing.T) {
 		params := &dagconfig.TestnetParams
 		params.SkipProofOfWork = true
@@ -500,13 +505,15 @@ func TestBlockProcessor(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error setting up BlockProcessor: %+v", err)
 		}
-		blockProcessor := SetupBlockProcessor(dbManager, &dagconfig.SimnetParams)
 		defer teardownFunc()
 
 		// create 11 blocks
 		blocks := make([]*externalapi.DomainBlock, 11)
 		for i := range blocks {
-			coinbaseData , _ := SetupCoinbaseData()
+			coinbaseData, _ := SetupCoinbaseData()
+			if err != nil {
+				t.Fatalf("error in SetupCoinbaseData: %+v", err)
+			}
 			block, err := blockBuilder.BuildBlock(coinbaseData, nil)
 			if err != nil {
 				t.Fatalf("error in BuildBlock: %+v", err)
@@ -522,7 +529,7 @@ func TestBlockProcessor(t *testing.T) {
 			}
 		}
 	})
-	
+
 	t.Run("Test create and double process same block", func(t *testing.T) {
 		params := &dagconfig.TestnetParams
 		params.SkipProofOfWork = true
@@ -539,10 +546,12 @@ func TestBlockProcessor(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error setting up BlockProcessor: %+v", err)
 		}
-		blockProcessor := SetupBlockProcessor(dbManager, &dagconfig.SimnetParams)
 		defer teardownFunc()
 
-		coinbaseData := &externalapi.DomainCoinbaseData{}
+		coinbaseData, _ := SetupCoinbaseData()
+		if err != nil {
+			t.Fatalf("error in SetupCoinbaseData: %+v", err)
+		}
 		block, err := blockBuilder.BuildBlock(coinbaseData, nil)
 		if err != nil {
 			t.Fatalf("error in BuildBlock: %+v", err)
@@ -562,11 +571,11 @@ func TestBlockProcessor(t *testing.T) {
 		_, chainABlocks, teardownChainAFunc := createChain(t, 50)
 		// destory chain A
 		teardownChainAFunc()
-		
+
 		// create a chain B of 49 blocks
 		blockProcessor, _, teardownChainBFunc := createChain(t, 49)
 		defer teardownChainBFunc()
-		
+
 		// add chain A blocks to chain B
 		for i := range chainABlocks {
 			block := chainABlocks[i]
@@ -576,17 +585,17 @@ func TestBlockProcessor(t *testing.T) {
 			}
 		}
 	})
-	
+
 	t.Run("Test add a chain of 100 blocks to a chain of 100 blocks", func(t *testing.T) { //TODO: timeout
 		// create a chain A of 100 blocks
 		_, chainABlocks, teardownChainAFunc := createChain(t, 100)
 		// destory chain A
 		teardownChainAFunc()
-		
+
 		// create a chain B of 100 blocks
 		blockProcessor, _, teardownChainBFunc := createChain(t, 100)
 		defer teardownChainBFunc()
-		
+
 		// add chain A blocks to chain B
 		for i := range chainABlocks {
 			block := chainABlocks[i]
