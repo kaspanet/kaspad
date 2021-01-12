db05ff0ca3c4687a0c144b88d9072805830f0e53
Elichai Turkel <elichai.turkel@gmail.com>
2021-01-12 18:24:40 +0200

    Add a test for GetHashrateString

--- util/math_test.go
+++ util/math_test.go
@@ -1,6 +1,9 @@
-package util
+package util_test
 
 import (
+	"github.com/kaspanet/kaspad/domain/consensus/utils/testutils"
+	"github.com/kaspanet/kaspad/domain/dagconfig"
+	"github.com/kaspanet/kaspad/util"
 	"testing"
 )
 
@@ -26,10 +29,26 @@ func TestFastLog2Floor(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		actualResult := FastLog2Floor(test.n)
+		actualResult := util.FastLog2Floor(test.n)
 
 		if test.expectedResult != actualResult {
 			t.Errorf("TestFastLog2Floor: %d: expected result: %d but got: %d", test.n, test.expectedResult, actualResult)
 		}
 	}
+}
+
+func TestGetHashrateString(t *testing.T) {
+	var results = map[string]string{
+		"kaspa-mainnet": "2 H/s",
+		"kaspa-testnet": "131.07 KH/s",
+		"kaspa-devnet":  "131.07 KH/s",
+		"kaspa-simnet":  "2.00 KH/s",
+	}
+	testutils.ForAllNets(t, false, func(t *testing.T, params *dagconfig.Params) {
+		targetGenesis := util.CompactToBig(params.GenesisBlock.Header.Bits())
+		hashrate := util.GetHashrateString(targetGenesis, params.TargetTimePerBlock)
+		if hashrate != results[params.Name] {
+			t.Errorf("Expected %s, found %s", results[params.Name], hashrate)
+		}
+	})
 }
