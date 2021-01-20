067191b2f1ba6ff33882d37030328264dbe3de29
stasatdaglabs <stas@daglabs.com>
2021-01-20 16:07:58 +0200

    Split handleRequestIBDRootUTXOSetAndBlockFlow into smaller methods.

--- app/protocol/flows/blockrelay/handle_request_ibd_root_utxo_set_and_block.go
+++ app/protocol/flows/blockrelay/handle_request_ibd_root_utxo_set_and_block.go
@@ -36,85 +36,102 @@ func HandleRequestIBDRootUTXOSetAndBlock(context HandleRequestIBDRootUTXOSetAndB
 
 func (flow *handleRequestIBDRootUTXOSetAndBlockFlow) start() error {
 	for {
-		message, err := flow.incomingRoute.Dequeue()
+		msgRequestIBDRootUTXOSetAndBlock, err := flow.waitForRequestIBDRootUTXOSetAndBlockMessages()
 		if err != nil {
 			return err
-		}
-		msgRequestIBDRootUTXOSetAndBlock, ok := message.(*appmessage.MsgRequestIBDRootUTXOSetAndBlock)
-		if !ok {
-			return protocolerrors.Errorf(true, "received unexpected message type. "+
-				"expected: %s, got: %s", appmessage.CmdRequestIBDRootUTXOSetAndBlock, message.Command())
 		}
 
 		finishMeasuring := logger.LogAndMeasureExecutionTime(log, "handleRequestIBDRootUTXOSetAndBlockFlow")
 		log.Debugf("Got request for IBDRoot UTXOSet and Block")
 
-		block, err := flow.Domain().Consensus().GetBlock(msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
-		if err != nil {
-			return err
-		}
-		log.Debugf("Retrieved pruning block %s", msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
-
-		err = flow.outgoingRoute.Enqueue(appmessage.NewMsgIBDBlock(appmessage.DomainBlockToMsgBlock(block)))
+		err = flow.sendIBDRootBlock(msgRequestIBDRootUTXOSetAndBlock)
 		if err != nil {
 			return err
 		}
 
-		// Send the UTXO set in `step`-sized chunks
-		const step = 1000
-		offset := 0
-		chunksSent := 0
-		for {
-			pruningPointUTXOs, err := flow.Domain().Consensus().GetPruningPointUTXOs(
-				msgRequestIBDRootUTXOSetAndBlock.IBDRoot, offset, step)
-			if err != nil {
-				if errors.Is(err, ruleerrors.ErrWrongPruningPointHash) {
-					err = flow.outgoingRoute.Enqueue(appmessage.NewMsgIBDRootNotFound())
-					if err != nil {
-						return err
-					}
-					break
-				}
-			}
-
-			log.Debugf("Retrieved %d UTXOs for pruning block %s",
-				len(pruningPointUTXOs), msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
-
-			outpointAndUTXOEntryPairs :=
-				appmessage.DomainOutpointAndUTXOEntryPairsToOutpointAndUTXOEntryPairs(pruningPointUTXOs)
-			err = flow.outgoingRoute.Enqueue(appmessage.NewMsgIBDRootUTXOSetChunk(outpointAndUTXOEntryPairs))
-			if err != nil {
-				return err
-			}
-
-			if len(pruningPointUTXOs) < step {
-				log.Debugf("Finished sending UTXOs for pruning block %s",
-					msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
-
-				err = flow.outgoingRoute.Enqueue(appmessage.NewMsgDoneIBDRootUTXOSetChunks())
-				if err != nil {
-					return err
-				}
-				break
-			}
-
-			offset += step
-			chunksSent++
-
-			// Wait for the peer to request more chunks every `ibdBatchSize` chunks
-			if chunksSent%ibdBatchSize == 0 {
-				message, err := flow.incomingRoute.DequeueWithTimeout(common.DefaultTimeout)
-				if err != nil {
-					return err
-				}
-				_, ok := message.(*appmessage.MsgRequestNextIBDRootUTXOSetChunk)
-				if !ok {
-					return protocolerrors.Errorf(true, "received unexpected message type. "+
-						"expected: %s, got: %s", appmessage.CmdRequestNextIBDRootUTXOSetChunk, message.Command())
-				}
-			}
+		err = flow.sendIBDRootUTXOSet(msgRequestIBDRootUTXOSetAndBlock)
+		if err != nil {
+			return err
 		}
 
 		finishMeasuring()
+	}
+}
+
+func (flow *handleRequestIBDRootUTXOSetAndBlockFlow) waitForRequestIBDRootUTXOSetAndBlockMessages() (
+	*appmessage.MsgRequestIBDRootUTXOSetAndBlock, error) {
+
+	message, err := flow.incomingRoute.Dequeue()
+	if err != nil {
+		return nil, err
+	}
+	msgRequestIBDRootUTXOSetAndBlock, ok := message.(*appmessage.MsgRequestIBDRootUTXOSetAndBlock)
+	if !ok {
+		return nil, protocolerrors.Errorf(true, "received unexpected message type. "+
+			"expected: %s, got: %s", appmessage.CmdRequestIBDRootUTXOSetAndBlock, message.Command())
+	}
+	return msgRequestIBDRootUTXOSetAndBlock, nil
+}
+
+func (flow *handleRequestIBDRootUTXOSetAndBlockFlow) sendIBDRootBlock(
+	msgRequestIBDRootUTXOSetAndBlock *appmessage.MsgRequestIBDRootUTXOSetAndBlock) error {
+
+	block, err := flow.Domain().Consensus().GetBlock(msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
+	if err != nil {
+		return err
+	}
+	log.Debugf("Retrieved pruning block %s", msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
+
+	return flow.outgoingRoute.Enqueue(appmessage.NewMsgIBDBlock(appmessage.DomainBlockToMsgBlock(block)))
+}
+
+func (flow *handleRequestIBDRootUTXOSetAndBlockFlow) sendIBDRootUTXOSet(
+	msgRequestIBDRootUTXOSetAndBlock *appmessage.MsgRequestIBDRootUTXOSetAndBlock) error {
+
+	// Send the UTXO set in `step`-sized chunks
+	const step = 1000
+	offset := 0
+	chunksSent := 0
+	for {
+		pruningPointUTXOs, err := flow.Domain().Consensus().GetPruningPointUTXOs(
+			msgRequestIBDRootUTXOSetAndBlock.IBDRoot, offset, step)
+		if err != nil {
+			if errors.Is(err, ruleerrors.ErrWrongPruningPointHash) {
+				return flow.outgoingRoute.Enqueue(appmessage.NewMsgIBDRootNotFound())
+			}
+		}
+
+		log.Debugf("Retrieved %d UTXOs for pruning block %s",
+			len(pruningPointUTXOs), msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
+
+		outpointAndUTXOEntryPairs :=
+			appmessage.DomainOutpointAndUTXOEntryPairsToOutpointAndUTXOEntryPairs(pruningPointUTXOs)
+		err = flow.outgoingRoute.Enqueue(appmessage.NewMsgIBDRootUTXOSetChunk(outpointAndUTXOEntryPairs))
+		if err != nil {
+			return err
+		}
+
+		if len(pruningPointUTXOs) < step {
+			log.Debugf("Finished sending UTXOs for pruning block %s",
+				msgRequestIBDRootUTXOSetAndBlock.IBDRoot)
+
+			return flow.outgoingRoute.Enqueue(appmessage.NewMsgDoneIBDRootUTXOSetChunks())
+		}
+
+		offset += step
+		chunksSent++
+
+		// Wait for the peer to request more chunks every `ibdBatchSize` chunks
+		if chunksSent%ibdBatchSize == 0 {
+			message, err := flow.incomingRoute.DequeueWithTimeout(common.DefaultTimeout)
+			if err != nil {
+				return err
+			}
+			_, ok := message.(*appmessage.MsgRequestNextIBDRootUTXOSetChunk)
+			if !ok {
+				return protocolerrors.Errorf(true, "received unexpected message type. "+
+					"expected: %s, got: %s", appmessage.CmdRequestNextIBDRootUTXOSetChunk, message.Command())
+			}
+		}
 	}
 }
