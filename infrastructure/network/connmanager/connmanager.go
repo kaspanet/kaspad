9ebce1ab1f3319acd99489fa560c504b99cc0508
Elichai Turkel <elichai.turkel@gmail.com>
2021-02-01 13:15:36 +0200

    Fix a data race in ConnectionManager by locking the mutex

--- infrastructure/network/connmanager/connmanager.go
+++ infrastructure/network/connmanager/connmanager.go
@@ -147,28 +147,15 @@ func (c *ConnectionManager) IsBanned(netConnection *netadapter.NetConnection) (b
 func (c *ConnectionManager) waitTillNextIteration() {
 	select {
 	case <-c.resetLoopChan:
-		c.loopTicker.Reset(connectionsLoopInterval)
+		c.loopTicker.Stop()
+		c.loopTicker = time.NewTicker(connectionsLoopInterval)
 	case <-c.loopTicker.C:
 	}
 }
 
-func (c *ConnectionManager) connectionExists(addressString string) bool {
-	if _, ok := c.activeRequested[addressString]; ok {
-		return true
-	}
-
-	if _, ok := c.activeOutgoing[addressString]; ok {
-		return true
-	}
-
-	if _, ok := c.activeIncoming[addressString]; ok {
-		return true
-	}
-
-	return false
-}
-
 func (c *ConnectionManager) isPermanent(addressString string) bool {
+	c.connectionRequestsLock.Lock()
+	defer c.connectionRequestsLock.Unlock()
 	if conn, ok := c.activeRequested[addressString]; ok {
 		return conn.isPermanent
 	}
