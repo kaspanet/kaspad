e04b5bae328e0cfccebed0c5b76d99b126bf59aa
Elichai Turkel <elichai.turkel@gmail.com>
2021-02-02 16:45:14 +0200

    Add comments explaining the dummy go files

--- app/protocol/flows/testing/testing.go
+++ app/protocol/flows/testing/testing.go
@@ -1,1 +1,4 @@
 package testing
+
+// Because of a bug in Go coverage fails if you have packages with test files only. See https://github.com/golang/go/issues/27333
+// So this is a dummy non-test go file in the package.
