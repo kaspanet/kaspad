fef44610653615a51ec156dbdd0475d45c3b0348
tal <tal@daglabs.com>
2021-02-08 12:54:54 +0200

    Changes according to the review - more detailed error messages, added constants, changed to 3 independent graphs (instead of extending), and changes all the abbreviations.

--- domain/consensus/processes/dagtraversalmanager/dagtraversalmanager_test.go
+++ domain/consensus/processes/dagtraversalmanager/dagtraversalmanager_test.go
@@ -9,100 +9,104 @@ import (
 	"testing"
 )
 
+const sizeOfTheChainThatCommonToAllGraphs int = 5
+
 func TestBlockAtDepth(t *testing.T) {
 	testutils.ForAllNets(t, true, func(t *testing.T, params *dagconfig.Params) {
+
 		factory := consensus.NewFactory()
 		tc, tearDown, err := factory.NewTestConsensus(params, false,
 			"TestBlockAtDepth")
 		if err != nil {
-			t.Fatalf("NewTestConsensus: %s", err)
+			t.Fatalf("Failed creating a NewTestConsensus: %s", err)
 		}
 		defer tearDown(false)
 
-		// "checkExpectedBlockChainDag" - compare BlockAtDepth to a loop on the SelectedChainIterator on a single chain DAG.
-		checkExpectedBlockChainDag := func(highHash *externalapi.DomainHash, depth uint64) {
-			currentBlock := highHash
-			currentBlockData, err := tc.GHOSTDAGDataStore().Get(tc.DatabaseContext(), currentBlock)
-			if err != nil {
-				t.Fatalf("GHOSTDAGDataStore().Get: %+v", err)
-			}
-			for i := uint64(0); i <= depth; i++ {
-				if currentBlockData.SelectedParent() == nil {
-					break
-				}
-				currentBlock = currentBlockData.SelectedParent()
-				currentBlockData, err = tc.GHOSTDAGDataStore().Get(tc.DatabaseContext(), currentBlock)
-				if err != nil {
-					t.Fatalf("GHOSTDAGDataStore().Get: %+v", err)
-				}
-			}
-			expected := currentBlock
-			blockHash, err := tc.DAGTraversalManager().BlockAtDepth(highHash, depth)
-			if err != nil {
-				t.Fatalf("BlockAtDepth: %+v", err)
-			}
-
-			if !blockHash.Equal(expected) {
-				t.Fatalf("Expected block %s but got %s", expected, blockHash)
-			}
-		}
-
-		// "checkExpectedBlockTwoChildrenSameSelectedParent" checks blockAtDepth on 2 children with the same selectedParent.
-		checkExpectedBlockTwoChildrenSameSelectedParent := func(firstChild, secondChild *externalapi.DomainHash, depth uint64) {
-			blockHash, err := tc.DAGTraversalManager().BlockAtDepth(firstChild, depth)
-			if err != nil {
-				t.Fatalf("BlockAtDepth: %+v", err)
-			}
-			expectedSameHash, err := tc.DAGTraversalManager().BlockAtDepth(secondChild, depth)
-			if err != nil {
-				t.Fatalf("BlockAtDepth: %+v", err)
-			}
-			if !blockHash.Equal(expectedSameHash) {
-				t.Fatalf("Expected block %s but got %s", expectedSameHash, blockHash)
-			}
-		}
-
-		// "checkExpectedBlockTwoChildrenSameSelectedParent" checks on different chains, on the same DAG, that they merge at the correct point.
-		checkExpectedBlockOnTwoDiffChain := func(firstChild, secondChild *externalapi.DomainHash, firstDepth, secondDepth uint64) {
-			blockHash, err := tc.DAGTraversalManager().BlockAtDepth(firstChild, firstDepth)
-			if err != nil {
-				t.Fatalf("BlockAtDepth: %+v", err)
-			}
-			expectedSameHash, err := tc.DAGTraversalManager().BlockAtDepth(secondChild, secondDepth)
-			if err != nil {
-				t.Fatalf("BlockAtDepth: %+v", err)
-			}
-			if !blockHash.Equal(expectedSameHash) {
-				t.Fatalf("Expected block %s but got %s", expectedSameHash, blockHash)
-			}
-			expectedDiffHash, err := tc.DAGTraversalManager().BlockAtDepth(secondChild, secondDepth-1)
-			if err != nil {
-				t.Fatalf("BlockAtDepth: %+v", err)
-			}
-			if blockHash.Equal(expectedDiffHash) {
-				t.Fatalf("Expected to differente block")
-			}
-
-		}
-
-		highHash, err := createAChainDag(params.GenesisHash, tc)
+		// The first test compares the result of BlockAtDepth to the result of looping over the SelectedChain on a single chain DAG.
+		const depth uint64 = 2
+		highHash, err := createAChainDAG(params.GenesisHash, tc)
 		if err != nil {
-			t.Fatalf("createAChainDagInBlockAtDepth: %+v", err)
+			t.Fatalf("Failed creating a Chain DAG In BlockAtDepthTEST: %+v", err)
 		}
-		checkExpectedBlockChainDag(highHash, 2)
-
-		firstChild, secondChild, err := expendDagToTwoChildrenSameSelectedParent(highHash, tc)
+		currentBlockHash := highHash
+		currentBlockData, err := tc.GHOSTDAGDataStore().Get(tc.DatabaseContext(), currentBlockHash)
 		if err != nil {
-			t.Fatalf("createADagInBlockAtDepth: %+v", err)
+			t.Fatalf("Failed getting GHOSTDAGData for block with hash %s: %+v", currentBlockHash.String(), err)
 		}
-		checkExpectedBlockTwoChildrenSameSelectedParent(firstChild, secondChild, 2)
 
-		firstChild, secondChild, err = expendDagToDiffChains(firstChild, secondChild, tc)
+		for i := uint64(0); i <= depth; i++ {
+			if currentBlockData.SelectedParent() == nil {
+				break
+			}
+			currentBlockHash = currentBlockData.SelectedParent()
+			currentBlockData, err = tc.GHOSTDAGDataStore().Get(tc.DatabaseContext(), currentBlockHash)
+			if err != nil {
+				t.Fatalf("Failed getting GHOSTDAGData for block with hash %s: %+v", currentBlockHash.String(), err)
+			}
+		}
+		expectedBlockHash := currentBlockHash
+		actualBlockHash, err := tc.DAGTraversalManager().BlockAtDepth(highHash, depth)
 		if err != nil {
-			t.Fatalf("createADagInBlockAtDepth: %+v", err)
+			t.Fatalf("Failed on BlockAtDepth: %+v", err)
 		}
-		checkExpectedBlockOnTwoDiffChain(firstChild, secondChild, 3, 2)
+		if !actualBlockHash.Equal(expectedBlockHash) {
+			t.Fatalf("Expected block %s but got %s", expectedBlockHash, actualBlockHash)
+		}
 
+		// The second test compares the results of BlockAtDepth of 2 children that have the same selectedParent.
+		tc, tearDown, err = factory.NewTestConsensus(params, false,
+			"TestBlockAtDepth")
+		if err != nil {
+			t.Fatalf("Failed creating a NewTestConsensus: %s", err)
+		}
+		firstChild, secondChild, err := createADAGTwoChildrenWithSameSelectedParent(params.GenesisHash, tc)
+		if err != nil {
+			t.Fatalf("Failed creating a DAG InBlockAtDepthTEST: %+v", err)
+		}
+		actualBlockHash, err = tc.DAGTraversalManager().BlockAtDepth(firstChild, depth)
+		if err != nil {
+			t.Fatalf("Failed at BlockAtDepth: %+v", err)
+		}
+		expectedSameHash, err := tc.DAGTraversalManager().BlockAtDepth(secondChild, depth)
+		if err != nil {
+			t.Fatalf("Failed in BlockAtDepth: %+v", err)
+		}
+		if !actualBlockHash.Equal(expectedSameHash) {
+			t.Fatalf("Expected block %s but got %s", expectedSameHash, actualBlockHash)
+		}
+
+		// The third test compares results of BlockAtDepth on two different chains, on the same DAG, and validates they merge at the correct point.
+		const firstDepth uint64 = 3
+		const secondDepth uint64 = 2
+		tc, tearDown, err = factory.NewTestConsensus(params, false,
+			"TestBlockAtDepth")
+		if err != nil {
+			t.Fatalf("Failed creating a NewTestConsensus: %s", err)
+		}
+		firstChild, secondChild, err = createADAGWithTwoDifferentChains(params.GenesisHash, tc)
+		if err != nil {
+			t.Fatalf("Failed creating a DAG in lockAtDepthTEST: %+v", err)
+		}
+
+		actualBlockHash, err = tc.DAGTraversalManager().BlockAtDepth(firstChild, firstDepth)
+		if err != nil {
+			t.Fatalf("Failed in BlockAtDepth: %+v", err)
+		}
+		expectedSameHash, err = tc.DAGTraversalManager().BlockAtDepth(secondChild, secondDepth)
+		if err != nil {
+			t.Fatalf("Failed in BlockAtDepth: %+v", err)
+		}
+
+		if !actualBlockHash.Equal(expectedSameHash) {
+			t.Fatalf("Expected block %s but got %s", expectedSameHash, actualBlockHash)
+		}
+		expectedDiffHash, err := tc.DAGTraversalManager().BlockAtDepth(secondChild, secondDepth-1)
+		if err != nil {
+			t.Fatalf("Failed in BlockAtDepth: %+v", err)
+		}
+		if actualBlockHash.Equal(expectedDiffHash) {
+			t.Fatalf("Expected to a differente block")
+		}
 	})
 }
 
@@ -209,36 +213,60 @@ func TestLowestChainBlockAboveOrEqualToBlueScore(t *testing.T) {
 	})
 }
 
-func createAChainDag(genesisHash *externalapi.DomainHash, tc testapi.TestConsensus) (*externalapi.DomainHash, error) {
+func createAChainDAG(genesisHash *externalapi.DomainHash, tc testapi.TestConsensus) (*externalapi.DomainHash, error) {
 	block := genesisHash
 	var err error
-	for i := 0; i < 5; i++ {
+	for i := 0; i < sizeOfTheChainThatCommonToAllGraphs; i++ {
 		block, _, err = tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
 		if err != nil {
 			return nil, err
 		}
 	}
 	return block, nil
-
 }
-func expendDagToTwoChildrenSameSelectedParent(parentHash *externalapi.DomainHash, tc testapi.TestConsensus) (*externalapi.DomainHash, *externalapi.DomainHash, error) {
 
-	firstChild, _, err := tc.AddBlock([]*externalapi.DomainHash{parentHash}, nil, nil)
+func createADAGTwoChildrenWithSameSelectedParent(genesisHash *externalapi.DomainHash, tc testapi.TestConsensus) (*externalapi.DomainHash, *externalapi.DomainHash, error) {
+	block := genesisHash
+	var err error
+	for i := 0; i < sizeOfTheChainThatCommonToAllGraphs; i++ {
+		block, _, err = tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	firstChild, _, err := tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
 	if err != nil {
 		return nil, nil, err
 	}
-	secondChild, _, err := tc.AddBlock([]*externalapi.DomainHash{parentHash}, nil, nil)
+	secondChild, _, err := tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
 	if err != nil {
 		return nil, nil, err
 	}
 	return firstChild, secondChild, nil
-
 }
 
-func expendDagToDiffChains(firstChainHash, secondChainHash *externalapi.DomainHash, tc testapi.TestConsensus) (*externalapi.DomainHash, *externalapi.DomainHash, error) {
-	block := firstChainHash
+func createADAGWithTwoDifferentChains(genesisHash *externalapi.DomainHash, tc testapi.TestConsensus) (*externalapi.DomainHash, *externalapi.DomainHash, error) {
+	const sizeOfTheFirstChildSubChainDAG int = 3
+	const sizeOfTheSecondChildSubChainDAG int = 2
+	block := genesisHash
 	var err error
-	for i := 0; i < 3; i++ {
+	for i := 0; i < sizeOfTheChainThatCommonToAllGraphs; i++ {
+		block, _, err = tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	firstChainHash, _, err := tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	secondChainHash, _, err := tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	block = firstChainHash
+	for i := 0; i < sizeOfTheFirstChildSubChainDAG; i++ {
 		block, _, err = tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
 		if err != nil {
 			return nil, nil, err
@@ -247,7 +275,7 @@ func expendDagToDiffChains(firstChainHash, secondChainHash *externalapi.DomainHa
 	firstChainHash = block
 
 	block = secondChainHash
-	for i := 0; i < 2; i++ {
+	for i := 0; i < sizeOfTheSecondChildSubChainDAG; i++ {
 		block, _, err = tc.AddBlock([]*externalapi.DomainHash{block}, nil, nil)
 		if err != nil {
 			return nil, nil, err
@@ -255,5 +283,4 @@ func expendDagToDiffChains(firstChainHash, secondChainHash *externalapi.DomainHa
 	}
 	secondChainHash = block
 	return firstChainHash, secondChainHash, nil
-
 }
