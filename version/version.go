09545b94b71b92c1ab044ade17f973a450cafe1f
Mike Zak <feanorr@gmail.com>
2021-02-10 18:36:19 +0200

    Update to version 0.8.8

--- version/version.go
+++ version/version.go
@@ -10,8 +10,8 @@ const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 
 const (
 	appMajor uint = 0
-	appMinor uint = 9
-	appPatch uint = 0
+	appMinor uint = 8
+	appPatch uint = 8
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
