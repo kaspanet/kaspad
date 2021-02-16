6fb59915ea27f0d18652dc4d70a6eceefd4221c8
tal <tal@daglabs.com>
2021-02-16 10:54:34 +0200

    Adds a comment to type MEDIAN.

--- domain/consensus/constructors.go
+++ domain/consensus/constructors.go
@@ -4,4 +4,6 @@ import "github.com/kaspanet/kaspad/domain/consensus/model"
 
 // GHOSTDAGManagerConstructor is the function signature for a constructor of a type implementing model.GHOSTDAGManager
 type GHOSTDAGManagerConstructor func(model.DBReader, model.DAGTopologyManager, model.GHOSTDAGDataStore, model.BlockHeaderStore, model.KType) model.GHOSTDAGManager
+
+// MEDIAN is the function signature for a constructor of a type implementing model.PastMedianTimeManager
 type MEDIAN func(int, model.DBReader, model.DAGTraversalManager, model.BlockHeaderStore, model.GHOSTDAGDataStore) model.PastMedianTimeManager
