17b359ec56b7a43b6030c2079da43951771c8e1a
tal <tal@daglabs.com>
2021-02-21 15:53:10 +0200

    Fix comment.

--- domain/consensus/constructors.go
+++ domain/consensus/constructors.go
@@ -5,5 +5,5 @@ import "github.com/kaspanet/kaspad/domain/consensus/model"
 // GHOSTDAGManagerConstructor is the function signature for a constructor of a type implementing model.GHOSTDAGManager
 type GHOSTDAGManagerConstructor func(model.DBReader, model.DAGTopologyManager, model.GHOSTDAGDataStore, model.BlockHeaderStore, model.KType) model.GHOSTDAGManager
 
-// MEDIAN is the function signature for a constructor of a type implementing model.PastMedianTimeManager
+// PastMedianTimeManagerConstructor is the function signature for a constructor of a type implementing model.PastMedianTimeManager
 type PastMedianTimeManagerConstructor func(int, model.DBReader, model.DAGTraversalManager, model.BlockHeaderStore, model.GHOSTDAGDataStore) model.PastMedianTimeManager
