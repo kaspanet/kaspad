29744253591f6efec721d110189a7a3b3fb8f20a
Elichai Turkel <elichai.turkel@gmail.com>
2021-02-25 16:19:31 +0200

    Remove DomainSubnetworkID reversal

--- domain/consensus/utils/subnetworks/compare.go
+++ domain/consensus/utils/subnetworks/compare.go
@@ -1,23 +1,11 @@
 package subnetworks
 
 import (
+	"bytes"
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 )
 
-func cmp(a, b externalapi.DomainSubnetworkID) int {
-	// We compare the hashes backwards because Hash is stored as a little endian byte array.
-	for i := externalapi.DomainSubnetworkIDSize - 1; i >= 0; i-- {
-		switch {
-		case a[i] < b[i]:
-			return -1
-		case a[i] > b[i]:
-			return 1
-		}
-	}
-	return 0
-}
-
 // Less returns true iff id a is less than id b
 func Less(a, b externalapi.DomainSubnetworkID) bool {
-	return cmp(a, b) < 0
+	return bytes.Compare(a[:], b[:]) < 0
 }
