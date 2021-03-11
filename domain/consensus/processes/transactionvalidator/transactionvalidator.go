e2d017a3d5a469211880e780ff59594f0992fa8c
Ori Newman <orinewman1@gmail.com>
2021-03-11 18:18:31 +0200

    Use DAA score for sequence lock

--- domain/consensus/processes/transactionvalidator/transactionvalidator.go
+++ domain/consensus/processes/transactionvalidator/transactionvalidator.go
@@ -32,7 +32,9 @@ func New(blockCoinbaseMaturity uint64,
 	maxCoinbasePayloadLength uint64,
 	databaseContext model.DBReader,
 	pastMedianTimeManager model.PastMedianTimeManager,
-	ghostdagDataStore model.GHOSTDAGDataStore) model.TransactionValidator {
+	ghostdagDataStore model.GHOSTDAGDataStore,
+	daaBlocksStore model.DAABlocksStore) model.TransactionValidator {
+
 	return &transactionValidator{
 		blockCoinbaseMaturity:      blockCoinbaseMaturity,
 		enableNonNativeSubnetworks: enableNonNativeSubnetworks,
@@ -43,6 +45,7 @@ func New(blockCoinbaseMaturity uint64,
 		databaseContext:            databaseContext,
 		pastMedianTimeManager:      pastMedianTimeManager,
 		ghostdagDataStore:          ghostdagDataStore,
+		daaBlocksStore:             daaBlocksStore,
 		sigCache:                   txscript.NewSigCache(sigCacheSize),
 	}
 }
