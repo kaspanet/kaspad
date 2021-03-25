9dbd8cbbe89cb25e1ee8deabd84b70491f10368c
Mike Zak <feanorr@gmail.com>
2021-03-25 10:19:57 +0200

    Add comment to StagingArea

--- domain/consensus/model/staging_area.go
+++ domain/consensus/model/staging_area.go
@@ -2,15 +2,25 @@ package model
 
 import "github.com/pkg/errors"
 
+// StagingShard is an interface that enables every store to have it's own Commit logic
+// See StagingArea for more details
 type StagingShard interface {
 	Commit(dbTx DBTransaction) error
 }
 
+// StagingArea is single changeset inside the consensus database, similar to a transaction in a classic database.
+// Each StagingArea consists of multiple StagingShards, one for each dataStore that has any changes within it.
+// To enable maximum flexibility for all stores, each has to define it's own Commit method, and pass it to the
+// StagingArea through the relevant StagingShard.
+//
+// When the StagingArea is being Committed, it goes over all it's shards, and commits those one-by-one.
+// Since Commit happens in a DatabaseTransaction, a StagingArea is atomic.
 type StagingArea struct {
 	shards     map[string]StagingShard
 	isCommited bool
 }
 
+// NewStagingArea creates a new, empty staging area.
 func NewStagingArea() *StagingArea {
 	return &StagingArea{
 		shards:     map[string]StagingShard{},
@@ -18,6 +28,8 @@ func NewStagingArea() *StagingArea {
 	}
 }
 
+// GetOrCreateShard attempts to retrieve a shard with the given name.
+// If it does not exist - a new shard is created using `createFunc`.
 func (sa *StagingArea) GetOrCreateShard(shardName string, createFunc func() StagingShard) StagingShard {
 	if _, ok := sa.shards[shardName]; !ok {
 		sa.shards[shardName] = createFunc()
@@ -26,6 +38,8 @@ func (sa *StagingArea) GetOrCreateShard(shardName string, createFunc func() Stag
 	return sa.shards[shardName]
 }
 
+// Commit goes over all the Shards in the StagingArea and commits them, inside the provided database transaction.
+// Note: the transaction itself is not committed, this is the callers responsibility to commit it.
 func (sa *StagingArea) Commit(dbTx DBTransaction) error {
 	if sa.isCommited {
 		return errors.New("Attempt to call Commit on already committed stagingArea")
