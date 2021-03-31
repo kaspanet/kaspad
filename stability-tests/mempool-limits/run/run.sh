a4aca7095a6d5863308c862c74a2be502d091dc8
stasatdaglabs <stas@daglabs.com>
2021-03-31 17:34:45 +0300

    Copy some boilerplate from the other stability tests.

---
+++ stability-tests/mempool-limits/run/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mempool-limits --command-list-file ./commands-list --profile=7000
+TEST_EXIT_CODE=$?
+
+echo "Exit code: $TEST_EXIT_CODE"
+
+if [ $TEST_EXIT_CODE -eq 0 ]; then
+  echo "mempool-limits test: PASSED"
+  exit 0
+fi
+echo "mempool-limits test: FAILED"
+exit 1
