c396f7d52492e7f3316596f8b3a2498b82db29be
stasatdaglabs <stas@daglabs.com>
2021-03-31 18:09:17 +0300

    Run kaspad in the background.

--- stability-tests/mempool-limits/main.go
+++ stability-tests/mempool-limits/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/kaspanet/kaspad/infrastructure/logger"
 	"github.com/kaspanet/kaspad/stability-tests/common"
-	"github.com/kaspanet/kaspad/util/profiling"
-
 	"github.com/kaspanet/kaspad/util/panics"
+	"github.com/kaspanet/kaspad/util/profiling"
 	"github.com/pkg/errors"
+	"os/exec"
 )
 
 func main() {
@@ -22,5 +24,26 @@ func main() {
 		profiling.Start(cfg.Profile, log)
 	}
 
+	rpcPort := 29587
+	kaspadErrChan := runKaspad(rpcPort)
+
+	select {
+	case err := <-kaspadErrChan:
+		log.Errorf("Kaspad closed unexpectedly: %s", err)
+	}
+
 	log.Infof("All tests have passed")
+}
+
+func runKaspad(rpcPort int) chan error {
+	cmd := exec.Command("kaspad", "--devnet", fmt.Sprintf("--rpclisten=0.0.0.0:%d", rpcPort))
+	cmd.Stdout = common.NewLogWriter(log, logger.LevelTrace, "KASPAD-STDOUT")
+	cmd.Stderr = common.NewLogWriter(log, logger.LevelWarn, "KASPAD-STDERR")
+
+	log.Infof("Running `%s`", cmd)
+	errChan := make(chan error)
+	spawn("kaspad-run", func() {
+		errChan <- cmd.Run()
+	})
+	return errChan
 }
