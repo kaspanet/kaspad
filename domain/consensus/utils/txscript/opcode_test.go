155378622d798d8b493fa1c9fd37f2b41942cd42
Ori Newman <orinewman1@gmail.com>
2021-04-06 15:03:25 +0300

    Add OpCheckMultiSigECDSA

--- domain/consensus/utils/txscript/opcode_test.go
+++ domain/consensus/utils/txscript/opcode_test.go
@@ -71,7 +71,7 @@ func TestOpcodeDisasm(t *testing.T) {
 		0x9f: "OP_LESSTHAN", 0xa0: "OP_GREATERTHAN",
 		0xa1: "OP_LESSTHANOREQUAL", 0xa2: "OP_GREATERTHANOREQUAL",
 		0xa3: "OP_MIN", 0xa4: "OP_MAX", 0xa5: "OP_WITHIN",
-		0xa8: "OP_SHA256",
+		0xa8: "OP_SHA256", 0xa9: "OP_CHECKMULTISIGECDSA",
 		0xaa: "OP_BLAKE2B",
 		0xab: "OP_CHECKSIGECDSA", 0xac: "OP_CHECKSIG", 0xad: "OP_CHECKSIGVERIFY",
 		0xae: "OP_CHECKMULTISIG", 0xaf: "OP_CHECKMULTISIGVERIFY",
@@ -187,7 +187,7 @@ func TestOpcodeDisasm(t *testing.T) {
 
 func isOpUnknown(opcodeVal int) bool {
 	return opcodeVal >= 0xba && opcodeVal <= 0xf9 || opcodeVal == 0xfc ||
-		opcodeVal == 0xa6 || opcodeVal == 0xa7 || opcodeVal == 0xa9
+		opcodeVal == 0xa6 || opcodeVal == 0xa7
 }
 
 func isNumberedNop(opcodeVal int) bool {
