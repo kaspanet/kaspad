465e4f0cfcc428b736d94f7bde5dab8beffadace
Mike Zak <feanorr@gmail.com>
2021-04-14 09:53:44 +0300

    Remove redundant code

--- infrastructure/db/database/database.go
+++ infrastructure/db/database/database.go
@@ -16,7 +16,4 @@ type Database interface {
 
 	// Close closes the database.
 	Close() error
-
-	// Version returns the version of this database, as specified in the version file
-	//	Version() int
 }
