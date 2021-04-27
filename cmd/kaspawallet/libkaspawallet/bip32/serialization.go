396f8be1957bbfa7bd89f9335b40162723d06b7f
Ori Newman <orinewman1@gmail.com>
2021-04-27 18:08:09 +0300

    Copy base58 library to kaspad

--- cmd/kaspawallet/libkaspawallet/bip32/serialization.go
+++ cmd/kaspawallet/libkaspawallet/bip32/serialization.go
@@ -2,8 +2,8 @@ package bip32
 
 import (
 	"encoding/binary"
-	"github.com/btcsuite/btcutil/base58"
 	"github.com/kaspanet/go-secp256k1"
+	"github.com/kaspanet/kaspad/cmd/kaspawallet/libkaspawallet/bip32/base58"
 	"github.com/pkg/errors"
 )
 
