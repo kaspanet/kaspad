4125188686d9792ff87b9952d733482c04a5a9e3
tal <tal@daglabs.com>
2021-05-19 11:53:50 +0300

    Create a file

---
+++ domain/consensus/utils/lockTimeAndSequence_test.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"github.com/kaspanet/kaspad/domain/consensus"
+	"github.com/kaspanet/kaspad/domain/consensus/utils/testutils"
+	"testing"
+)
+
+func TestPruningDepth(t *testing.T) {
+	testutils.ForAllNets(t, true, func(t *testing.T, consensusConfig *consensus.Config) {
+
+	})
+}
