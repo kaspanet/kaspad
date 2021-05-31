363494ef7afa5545828d2b1ffee3a3eca86fe55f
stasatdaglabs <39559713+stasatdaglabs@users.noreply.github.com>
2021-05-31 18:11:08 +0300

    Implement NotifyVirtualDaaScoreChanged (#1737)
    * Add notifyVirtualDaaScoreChanged to protowire.
    
    * Add notifyVirtualDaaScoreChanged to the rest of kaspad.
    
    * Add notifyVirtualDaaScoreChanged to the rest of kaspad.
    
    * Test the DAA score notification in TestVirtualSelectedParentBlueScore.
    
    * Rename TestVirtualSelectedParentBlueScore to TestVirtualSelectedParentBlueScoreAndVirtualDAAScore.
    
    (cherry picked from commit 83e631548f206aaf2da5b2103681c715bc250c75)

---
+++ infrastructure/network/netadapter/server/grpcserver/protowire/rpc_notify_virtual_daa_score_changed.go
@@ -0,0 +1,73 @@
+package protowire
+
+import (
+	"github.com/kaspanet/kaspad/app/appmessage"
+	"github.com/pkg/errors"
+)
+
+func (x *KaspadMessage_NotifyVirtualDaaScoreChangedRequest) toAppMessage() (appmessage.Message, error) {
+	if x == nil {
+		return nil, errors.Wrapf(errorNil, "KaspadMessage_NotifyVirtualDaaScoreChangedRequest is nil")
+	}
+	return &appmessage.NotifyVirtualDaaScoreChangedRequestMessage{}, nil
+}
+
+func (x *KaspadMessage_NotifyVirtualDaaScoreChangedRequest) fromAppMessage(_ *appmessage.NotifyVirtualDaaScoreChangedRequestMessage) error {
+	x.NotifyVirtualDaaScoreChangedRequest = &NotifyVirtualDaaScoreChangedRequestMessage{}
+	return nil
+}
+
+func (x *KaspadMessage_NotifyVirtualDaaScoreChangedResponse) toAppMessage() (appmessage.Message, error) {
+	if x == nil {
+		return nil, errors.Wrapf(errorNil, "KaspadMessage_NotifyVirtualDaaScoreChangedResponse is nil")
+	}
+	return x.NotifyVirtualDaaScoreChangedResponse.toAppMessage()
+}
+
+func (x *KaspadMessage_NotifyVirtualDaaScoreChangedResponse) fromAppMessage(message *appmessage.NotifyVirtualDaaScoreChangedResponseMessage) error {
+	var err *RPCError
+	if message.Error != nil {
+		err = &RPCError{Message: message.Error.Message}
+	}
+	x.NotifyVirtualDaaScoreChangedResponse = &NotifyVirtualDaaScoreChangedResponseMessage{
+		Error: err,
+	}
+	return nil
+}
+
+func (x *NotifyVirtualDaaScoreChangedResponseMessage) toAppMessage() (appmessage.Message, error) {
+	if x == nil {
+		return nil, errors.Wrapf(errorNil, "NotifyVirtualDaaScoreChangedResponseMessage is nil")
+	}
+	rpcErr, err := x.Error.toAppMessage()
+	// Error is an optional field
+	if err != nil && !errors.Is(err, errorNil) {
+		return nil, err
+	}
+	return &appmessage.NotifyVirtualDaaScoreChangedResponseMessage{
+		Error: rpcErr,
+	}, nil
+}
+
+func (x *KaspadMessage_VirtualDaaScoreChangedNotification) toAppMessage() (appmessage.Message, error) {
+	if x == nil {
+		return nil, errors.Wrapf(errorNil, "KaspadMessage_VirtualDaaScoreChangedNotification is nil")
+	}
+	return x.VirtualDaaScoreChangedNotification.toAppMessage()
+}
+
+func (x *KaspadMessage_VirtualDaaScoreChangedNotification) fromAppMessage(message *appmessage.VirtualDaaScoreChangedNotificationMessage) error {
+	x.VirtualDaaScoreChangedNotification = &VirtualDaaScoreChangedNotificationMessage{
+		VirtualDaaScore: message.VirtualDaaScore,
+	}
+	return nil
+}
+
+func (x *VirtualDaaScoreChangedNotificationMessage) toAppMessage() (appmessage.Message, error) {
+	if x == nil {
+		return nil, errors.Wrapf(errorNil, "VirtualDaaScoreChangedNotificationMessage is nil")
+	}
+	return &appmessage.VirtualDaaScoreChangedNotificationMessage{
+		VirtualDaaScore: x.VirtualDaaScore,
+	}, nil
+}
--- testing/integration/virtual_selected_parent_blue_score_test.go
+++ testing/integration/virtual_selected_parent_blue_score_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestVirtualSelectedParentBlueScore(t *testing.T) {
+func TestVirtualSelectedParentBlueScoreAndVirtualDAAScore(t *testing.T) {
 	// Setup a single kaspad instance
 	harnessParams := &harnessParams{
 		p2pAddress:              p2pAddress1,
@@ -38,15 +38,30 @@ func TestVirtualSelectedParentBlueScore(t *testing.T) {
 			"blue score change notifications: %s", err)
 	}
 
+	// Register to virtual DAA score changes
+	onVirtualDaaScoreChangedChan := make(chan *appmessage.VirtualDaaScoreChangedNotificationMessage)
+	err = kaspad.rpcClient.RegisterForVirtualDaaScoreChangedNotifications(
+		func(notification *appmessage.VirtualDaaScoreChangedNotificationMessage) {
+			onVirtualDaaScoreChangedChan <- notification
+		})
+	if err != nil {
+		t.Fatalf("Failed to register for virtual DAA score change notifications: %s", err)
+	}
+
 	// Mine some blocks and make sure that the notifications
-	// report correct blue score values
+	// report correct values
 	const blockAmountToMine = 100
 	for i := 0; i < blockAmountToMine; i++ {
 		mineNextBlock(t, kaspad)
-		notification := <-onVirtualSelectedParentBlueScoreChangedChan
-		if notification.VirtualSelectedParentBlueScore != 1+uint64(i) {
+		blueScoreChangedNotification := <-onVirtualSelectedParentBlueScoreChangedChan
+		if blueScoreChangedNotification.VirtualSelectedParentBlueScore != 1+uint64(i) {
 			t.Fatalf("Unexpected virtual selected parent blue score. Want: %d, got: %d",
-				1+uint64(i), notification.VirtualSelectedParentBlueScore)
+				1+uint64(i), blueScoreChangedNotification.VirtualSelectedParentBlueScore)
+		}
+		daaScoreChangedNotification := <-onVirtualDaaScoreChangedChan
+		if daaScoreChangedNotification.VirtualDaaScore > 1+uint64(i) {
+			t.Fatalf("Unexpected virtual DAA score. Want: %d, got: %d",
+				1+uint64(i), daaScoreChangedNotification.VirtualDaaScore)
 		}
 	}
 
