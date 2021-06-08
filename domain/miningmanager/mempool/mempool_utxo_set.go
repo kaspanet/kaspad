d88fe305c53b98a8360e5d49b9651c3ab257c86c
Mike Zak <feanorr@gmail.com>
2021-06-08 10:27:06 +0300

    Implemented mempoolUTXOSet.addTransaction

--- domain/miningmanager/mempool/mempool_utxo_set.go
+++ domain/miningmanager/mempool/mempool_utxo_set.go
@@ -2,7 +2,9 @@ package mempool
 
 import (
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+	"github.com/kaspanet/kaspad/domain/consensus/utils/utxo"
 	"github.com/kaspanet/kaspad/domain/miningmanager/mempool/model"
+	"github.com/pkg/errors"
 )
 
 type mempoolUTXOSet struct {
@@ -37,7 +39,34 @@ func (mpus *mempoolUTXOSet) getParentsInPool(transaction *model.MempoolTransacti
 }
 
 func (mpus *mempoolUTXOSet) addTransaction(transaction *model.MempoolTransaction) error {
-	panic("mempoolUTXOSet.addTransaction not implemented") // TODO (Mike)
+	outpoint := &externalapi.DomainOutpoint{TransactionID: *transaction.TransactionID()}
+
+	for i, input := range transaction.Transaction.Inputs {
+		outpoint.Index = uint32(i)
+
+		// Sanity check for consistency
+		if existingTx, exists := mpus.transactionsByPreviousOutpoint[input.PreviousOutpoint]; exists {
+			return errors.Errorf("outpoint %s is already used by %s",
+				input.PreviousOutpoint, existingTx.TransactionID())
+		}
+
+		delete(mpus.poolUnspentOutputs, *outpoint)
+		mpus.transactionsByPreviousOutpoint[input.PreviousOutpoint] = transaction
+	}
+
+	for i, output := range transaction.Transaction.Outputs {
+		outpoint := externalapi.DomainOutpoint{TransactionID: *transaction.TransactionID(), Index: uint32(i)}
+
+		// Sanity check for consistency
+		if _, exists := mpus.poolUnspentOutputs[outpoint]; exists {
+			return errors.Errorf("outpoint %s already exists", outpoint)
+		}
+
+		mpus.poolUnspentOutputs[outpoint] =
+			utxo.NewUTXOEntry(output.Value, output.ScriptPublicKey, false, model.UnacceptedDAAScore)
+	}
+
+	return nil
 }
 
 func (mpus *mempoolUTXOSet) removeTransaction(transactionID *externalapi.DomainTransactionID) error {
