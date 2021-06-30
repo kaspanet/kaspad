172fc981413775e15fe95d99f7605a8e544996ae
Mike Zak <feanorr@gmail.com>
2021-06-30 09:50:42 +0300

    Update comments on constants

--- domain/consensus/utils/constants/constants.go
+++ domain/consensus/utils/constants/constants.go
@@ -29,8 +29,8 @@ const (
 	SequenceLockTimeDisabled uint64 = 1 << 63
 
 	// SequenceLockTimeIsSeconds is a flag that if set on a transaction
-	// input's sequence number, the relative locktime has units of 524,2788
-	// milliseconds
+	// input's sequence number, the relative locktime has units of 1 second.
+	// If the flag is not set, the relative lockatime is according to DAA score.
 	SequenceLockTimeIsSeconds uint64 = 1 << 62
 
 	// SequenceLockTimeMask is a mask that extracts the relative locktime
@@ -40,13 +40,10 @@ const (
 	// SequenceLockTimeGranularity is the defined time based granularity
 	// for milliseconds-based relative time locks. When converting from milliseconds
 	// to a sequence number, the value is multiplied by this amount,
-	// therefore the granularity of relative time locks 1000 milliseconds.
-	// Enforced relative lock times are multiples of 524288 milliseconds.
+	// therefore the granularity of relative time locks is 1000 milliseconds or 1 second.
 	SequenceLockTimeGranularity = 1000
 
 	// LockTimeThreshold is the number below which a lock time is
-	// interpreted to be a block number. Since an average of one block
-	// is generated per 10 minutes, this allows blocks for about 9,512
-	// years.
+	// interpreted to be a block number.
 	LockTimeThreshold = 5e8 // Tue Nov 5 00:53:20 1985 UTC
 )
