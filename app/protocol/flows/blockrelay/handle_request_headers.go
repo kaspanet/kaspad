630804bbc126eadbafef1ed281502a19307e7083
cbitensky <constantinebitensky@gmail.com>
2021-06-30 15:07:57 +0300

    Make use of maxBlocks instead of maxBlueScoreDifference in antiPastHashesBetween

--- app/protocol/flows/blockrelay/handle_request_headers.go
+++ app/protocol/flows/blockrelay/handle_request_headers.go
@@ -49,7 +49,7 @@ func (flow *handleRequestHeadersFlow) start() error {
 
 			// GetHashesBetween is a relatively heavy operation so we limit it
 			// in order to avoid locking the consensus for too long
-			const maxBlocks = 1 << 13
+			const maxBlocks = 1 << 10
 			blockHashes, _, err := flow.Domain().Consensus().GetHashesBetween(lowHash, highHash, maxBlocks)
 			if err != nil {
 				return err
