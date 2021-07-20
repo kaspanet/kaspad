1a222031e4c38c396e470081638c82d25d14e570
stasatdaglabs <stas@daglabs.com>
2021-07-20 08:37:51 +0300

    Try to figure out what's inside github.event.

--- .github/workflows/notify_discord.sh
+++ .github/workflows/notify_discord.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+echo "${EVENT}"
+
+JOB_TYPE="commit"
+if [ "${EVENT_NAME}" = "pull_request" ]; then
+  JOB_TYPE="pull request"
+fi
+
 MESSAGE="**${MESSAGE_TITLE}**:
-Job **FAILED** in ${REPOSITORY_NAME} for branch '${BRANCH_NAME}' authored by ${COMMITER}"
+Job **FAILED** in ${REPOSITORY_NAME} for ${JOB_TYPE} authored by ${COMMITER}"
 
 echo "${MESSAGE}"
