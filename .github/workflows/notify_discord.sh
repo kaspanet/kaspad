6c92623c2f836122f387674d2f6313798b6a9611
stasatdaglabs <stas@daglabs.com>
2021-07-20 09:48:48 +0300

    Add URLs to the message.

--- .github/workflows/notify_discord.sh
+++ .github/workflows/notify_discord.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "${JOB_JSON}"
-echo "aaaaa"
-
 ACTOR_NAME=$(jq -r .actor <<< "${GITHUB_JSON}")
 JOB_NAME=$(jq -r .job <<< "${GITHUB_JSON}")
 REPOSITORY_NAME=$(jq -r .repository <<< "${GITHUB_JSON}")
@@ -13,7 +10,12 @@ Job '${JOB_NAME}' failed in ${REPOSITORY_NAME}"
 
 if [ "${EVENT_NAME}" = "pull_request" ]; then
   PULL_REQUEST_TITLE=$(jq -r .event.pull_request.title <<< "${GITHUB_JSON}")
-  MESSAGE="${MESSAGE} for pull request '${PULL_REQUEST_TITLE}'"
+  PULL_REQUEST_URL=$(jq -r .event.pull_request._links.html.href <<< "${GITHUB_JSON}")
+  RUN_ID=$(jq -r .run_id <<< "${GITHUB_JSON}")
+  RUN_URL="${PULL_REQUEST_URL}/checks?check_run_id=${RUN_ID}"
+
+  MESSAGE="${MESSAGE} for pull request '${PULL_REQUEST_TITLE}'
+  [Pull Request](${PULL_REQUEST_URL})  [JOB](${RUN_URL})"
 fi
 
 echo "${MESSAGE}"
