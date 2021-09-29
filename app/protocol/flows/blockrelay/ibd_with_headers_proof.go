9c624cb3c59e4c0fe492e31cca5ff2e39f687f71
Ori Newman <orinewman1@gmail.com>
2021-09-29 20:29:01 +0300

    Ban for rule errors on pruning proof

--- app/protocol/flows/blockrelay/ibd_with_headers_proof.go
+++ app/protocol/flows/blockrelay/ibd_with_headers_proof.go
@@ -90,7 +90,7 @@ func (flow *handleRelayInvsFlow) syncAndValidatePruningPointProof() (*externalap
 	pruningPointProof := appmessage.MsgPruningPointProofToDomainPruningPointProof(pruningPointProofMessage)
 	err = flow.Domain().Consensus().ValidatePruningPointProof(pruningPointProof)
 	if err != nil {
-		if !errors.As(err, &ruleerrors.RuleError{}) {
+		if errors.As(err, &ruleerrors.RuleError{}) {
 			return nil, protocolerrors.Wrapf(true, err, "pruning point proof validation failed")
 		}
 		return nil, err
