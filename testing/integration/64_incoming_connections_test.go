ccf60ae711bdda9eb92404cdaabe62645ffa4d2f
Ori Newman <orinewman1@gmail.com>
2021-10-14 12:22:15 +0300

    numBullies->16

--- testing/integration/64_incoming_connections_test.go
+++ testing/integration/64_incoming_connections_test.go
@@ -11,7 +11,7 @@ import (
 
 func Test64IncomingConnections(t *testing.T) {
 	// Much more than 32 hosts creates a risk of running out of available file descriptors for leveldb
-	const numBullies = 64
+	const numBullies = 16
 	harnessesParams := make([]*harnessParams, numBullies+1)
 	for i := 0; i < numBullies+1; i++ {
 		harnessesParams[i] = &harnessParams{
