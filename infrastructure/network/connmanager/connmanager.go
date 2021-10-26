2a761e1b214d04ccd1b64a51fd45243578c66871
Ori Newman <orinewman1@gmail.com>
2021-10-26 09:08:09 +0300

    Fix comment

--- infrastructure/network/connmanager/connmanager.go
+++ infrastructure/network/connmanager/connmanager.go
@@ -91,7 +91,7 @@ func (c *ConnectionManager) Stop() {
 	}
 
 	c.loopTicker.Stop()
-	// Force the next iteration so the connection loop will stop immediately and not after `connectionsLoopInterval`ErrPruningProofMissesBlocksBelowPruningPoint.
+	// Force the next iteration so the connection loop will stop immediately and not after `connectionsLoopInterval`.
 	c.run()
 }
 
