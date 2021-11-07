e9d8f0b975a1b28e5506268e55132768023a5649
Elichai Turkel <elichai.turkel@gmail.com>
2021-11-07 12:00:41 +0200

    Add benchmarks to hash writers to compare blake2b to the CSHAKE

--- domain/consensus/utils/hashes/writers_test.go
+++ domain/consensus/utils/hashes/writers_test.go
@@ -1,1 +1,50 @@
 package hashes
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func BenchmarkNewBlockHashWriterSmall(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	var someBytes [32]byte
+	r.Read(someBytes[:])
+	for i := 0; i < b.N; i++ {
+		hasher := NewBlockHashWriter()
+		hasher.InfallibleWrite(someBytes[:])
+		hasher.Finalize()
+	}
+}
+
+func BenchmarkNewBlockHashWriterBig(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	var someBytes [1024]byte
+	r.Read(someBytes[:])
+	for i := 0; i < b.N; i++ {
+		hasher := NewBlockHashWriter()
+		hasher.InfallibleWrite(someBytes[:])
+		hasher.Finalize()
+	}
+}
+
+func BenchmarkNewHeavyHashWriterSmall(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	var someBytes [32]byte
+	r.Read(someBytes[:])
+	for i := 0; i < b.N; i++ {
+		hasher := NewHeavyHashWriter()
+		hasher.InfallibleWrite(someBytes[:])
+		hasher.Finalize()
+	}
+}
+
+func BenchmarkNewHeavyHashWriterBig(b *testing.B) {
+	r := rand.New(rand.NewSource(0))
+	var someBytes [1024]byte
+	r.Read(someBytes[:])
+	for i := 0; i < b.N; i++ {
+		hasher := NewHeavyHashWriter()
+		hasher.InfallibleWrite(someBytes[:])
+		hasher.Finalize()
+	}
+}
