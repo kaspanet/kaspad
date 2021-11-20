d96498a9cbae3f1de1bc094af9ba79ac3353185b
Ori Newman <orinewman1@gmail.com>
2021-11-21 01:22:39 +0200

    Add --allow-submit-block-when-not-synced in stability tests

--- stability-tests/run/run-fast.sh
+++ stability-tests/run/run-fast.sh
@@ -10,33 +10,33 @@ failedTests=()
 # cd "${PROJECT_ROOT}/application-level-garbage/run" && ./run.sh || failedTests+=("application-level-garbage")
 # echo "Done running application-level-garbage"
 
-echo "Running infra-level-garbage"
-cd "${PROJECT_ROOT}/infra-level-garbage/run" && ./run.sh || failedTests+=("infra-level-garbage")
-echo "Done running infra-level-garbage"
+#echo "Running infra-level-garbage"
+#cd "${PROJECT_ROOT}/infra-level-garbage/run" && ./run.sh || failedTests+=("infra-level-garbage")
+#echo "Done running infra-level-garbage"
+#
+#echo "Running kaspadsanity"
+#cd "${PROJECT_ROOT}/kaspadsanity/run" && ./run.sh || failedTests+=("kaspadsanity")
+#echo "Done running kaspadsanity"
+#
+#echo "Running rpc-stability"
+#cd "${PROJECT_ROOT}/rpc-stability/run" && ./run.sh || failedTests+=("rpc-stability")
+#echo "Done running rpc-stability"
+#
+#echo "Running rpc-idle-clients"
+#cd "${PROJECT_ROOT}/rpc-idle-clients/run" && ./run.sh || failedTests+=("rpc-idle-clients")
+#echo "Done running rpc-idle-clients"
 
-echo "Running kaspadsanity"
-cd "${PROJECT_ROOT}/kaspadsanity/run" && ./run.sh || failedTests+=("kaspadsanity")
-echo "Done running kaspadsanity"
+#echo "Running simple-sync"
+#cd "${PROJECT_ROOT}/simple-sync/run" && ./run.sh || failedTests+=("simple-sync")
+#echo "Done running simple-sync"
 
-echo "Running rpc-stability"
-cd "${PROJECT_ROOT}/rpc-stability/run" && ./run.sh || failedTests+=("rpc-stability")
-echo "Done running rpc-stability"
-
-echo "Running rpc-idle-clients"
-cd "${PROJECT_ROOT}/rpc-idle-clients/run" && ./run.sh || failedTests+=("rpc-idle-clients")
-echo "Done running rpc-idle-clients"
-
-echo "Running simple-sync"
-cd "${PROJECT_ROOT}/simple-sync/run" && ./run.sh || failedTests+=("simple-sync")
-echo "Done running simple-sync"
-
-echo "Running orphans"
-cd "${PROJECT_ROOT}/orphans/run" && ./run.sh || failedTests+=("orphans")
-echo "Done running orphans"
-
-echo "Running reorg"
-cd "${PROJECT_ROOT}/reorg/run" && ./run.sh || failedTests+=("reorg")
-echo "Done running reorg"
+#echo "Running orphans"
+#cd "${PROJECT_ROOT}/orphans/run" && ./run.sh || failedTests+=("orphans")
+#echo "Done running orphans"
+#
+#echo "Running reorg"
+#cd "${PROJECT_ROOT}/reorg/run" && ./run.sh || failedTests+=("reorg")
+#echo "Done running reorg"
 
 echo "Running many-tips"
 cd "${PROJECT_ROOT}/many-tips/run" && ./run.sh || failedTests+=("many-tips")
