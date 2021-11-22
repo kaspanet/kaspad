b04292c97ad618fe58f833b934e994106010e261
Ori Newman <orinewman1@gmail.com>
2021-11-22 09:56:30 +0200

    Don't server parallel PruningPointAndItsAnticoneRequests

--- app/protocol/flows/blockrelay/handle_pruning_point_and_its_anticone_requests.go
+++ app/protocol/flows/blockrelay/handle_pruning_point_and_its_anticone_requests.go
@@ -3,9 +3,12 @@ package blockrelay
 import (
 	"github.com/kaspanet/kaspad/app/appmessage"
 	peerpkg "github.com/kaspanet/kaspad/app/protocol/peer"
+	"github.com/kaspanet/kaspad/app/protocol/protocolerrors"
 	"github.com/kaspanet/kaspad/domain"
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
+	"runtime"
+	"sync/atomic"
 )
 
 // PruningPointAndItsAnticoneRequestsContext is the interface for the context needed for the HandlePruningPointAndItsAnticoneRequests flow.
@@ -13,52 +16,65 @@ type PruningPointAndItsAnticoneRequestsContext interface {
 	Domain() domain.Domain
 }
 
+var isBusy uint32
+
 // HandlePruningPointAndItsAnticoneRequests listens to appmessage.MsgRequestPruningPointAndItsAnticone messages and sends
 // the pruning point and its anticone to the requesting peer.
 func HandlePruningPointAndItsAnticoneRequests(context PruningPointAndItsAnticoneRequestsContext, incomingRoute *router.Route,
 	outgoingRoute *router.Route, peer *peerpkg.Peer) error {
 
 	for {
-		_, err := incomingRoute.Dequeue()
-		if err != nil {
-			return err
-		}
-
-		log.Debugf("Got request for pruning point and its anticone from %s", peer)
-
-		pruningPointHeaders, err := context.Domain().Consensus().PruningPointHeaders()
-		if err != nil {
-			return err
-		}
-
-		msgPruningPointHeaders := make([]*appmessage.MsgBlockHeader, len(pruningPointHeaders))
-		for i, header := range pruningPointHeaders {
-			msgPruningPointHeaders[i] = appmessage.DomainBlockHeaderToBlockHeader(header)
-		}
-
-		err = outgoingRoute.Enqueue(appmessage.NewMsgPruningPoints(msgPruningPointHeaders))
-		if err != nil {
-			return err
-		}
-
-		pointAndItsAnticone, err := context.Domain().Consensus().PruningPointAndItsAnticone()
-		if err != nil {
-			return err
-		}
-
-		for _, blockHash := range pointAndItsAnticone {
-			err := sendBlockWithTrustedData(context, outgoingRoute, blockHash)
+		err := func() error {
+			_, err := incomingRoute.Dequeue()
 			if err != nil {
 				return err
 			}
-		}
 
-		err = outgoingRoute.Enqueue(appmessage.NewMsgDoneBlocksWithTrustedData())
+			if !atomic.CompareAndSwapUint32(&isBusy, 0, 1) {
+				return protocolerrors.Errorf(false, "node is busy with other pruning point anticone requests")
+			}
+			defer atomic.StoreUint32(&isBusy, 0)
+
+			log.Debugf("Got request for pruning point and its anticone from %s", peer)
+
+			pruningPointHeaders, err := context.Domain().Consensus().PruningPointHeaders()
+			if err != nil {
+				return err
+			}
+
+			msgPruningPointHeaders := make([]*appmessage.MsgBlockHeader, len(pruningPointHeaders))
+			for i, header := range pruningPointHeaders {
+				msgPruningPointHeaders[i] = appmessage.DomainBlockHeaderToBlockHeader(header)
+			}
+
+			err = outgoingRoute.Enqueue(appmessage.NewMsgPruningPoints(msgPruningPointHeaders))
+			if err != nil {
+				return err
+			}
+
+			pointAndItsAnticone, err := context.Domain().Consensus().PruningPointAndItsAnticone()
+			if err != nil {
+				return err
+			}
+
+			for _, blockHash := range pointAndItsAnticone {
+				err := sendBlockWithTrustedData(context, outgoingRoute, blockHash)
+				if err != nil {
+					return err
+				}
+			}
+
+			err = outgoingRoute.Enqueue(appmessage.NewMsgDoneBlocksWithTrustedData())
+			if err != nil {
+				return err
+			}
+
+			log.Debugf("Sent pruning point and its anticone to %s", peer)
+			return nil
+		}()
 		if err != nil {
 			return err
 		}
-
-		log.Debugf("Sent pruning point and its anticone to %s", peer)
 	}
 }
 
@@ -72,6 +88,8 @@ func sendBlockWithTrustedData(context PruningPointAndItsAnticoneRequestsContext,
 	if err != nil {
 		return err
 	}
+
+	runtime.GC()
 
 	return nil
 }
