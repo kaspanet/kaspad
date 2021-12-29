3a323ea1fb1567b732367853722b0cf8bb6c12dc
msutton <mikisiton2@gmail.com>
2021-12-29 14:20:31 +0200

    Fix protobuf generation for PR #1885

--- infrastructure/network/netadapter/server/grpcserver/protowire/rpc_get_balance_by_address.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/rpc_get_balance_by_address.go
@@ -5,15 +5,15 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (x *KaspadMessage_GetBalanceByAddressRequestMessage) toAppMessage() (appmessage.Message, error) {
+func (x *KaspadMessage_GetBalanceByAddressRequest) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
 		return nil, errors.Wrapf(errorNil, "KaspadMessage_GetBalanceByAddressRequest is nil")
 	}
-	return x.GetBalanceByAddressRequestMessage.toAppMessage()
+	return x.GetBalanceByAddressRequest.toAppMessage()
 }
 
-func (x *KaspadMessage_GetBalanceByAddressRequestMessage) fromAppMessage(message *appmessage.GetBalanceByAddressRequestMessage) error {
-	x.GetBalanceByAddressRequestMessage = &GetBalanceByAddressRequestMessage{
+func (x *KaspadMessage_GetBalanceByAddressRequest) fromAppMessage(message *appmessage.GetBalanceByAddressRequestMessage) error {
+	x.GetBalanceByAddressRequest = &GetBalanceByAddressRequestMessage{
 		Address: message.Address,
 	}
 	return nil
@@ -28,19 +28,19 @@ func (x *GetBalanceByAddressRequestMessage) toAppMessage() (appmessage.Message, 
 	}, nil
 }
 
-func (x *KaspadMessage_GetBalanceByAddressResponseMessage) toAppMessage() (appmessage.Message, error) {
+func (x *KaspadMessage_GetBalanceByAddressResponse) toAppMessage() (appmessage.Message, error) {
 	if x == nil {
 		return nil, errors.Wrapf(errorNil, "GetBalanceByAddressResponse is nil")
 	}
-	return x.GetBalanceByAddressResponseMessage.toAppMessage()
+	return x.GetBalanceByAddressResponse.toAppMessage()
 }
 
-func (x *KaspadMessage_GetBalanceByAddressResponseMessage) fromAppMessage(message *appmessage.GetBalanceByAddressResponseMessage) error {
+func (x *KaspadMessage_GetBalanceByAddressResponse) fromAppMessage(message *appmessage.GetBalanceByAddressResponseMessage) error {
 	var err *RPCError
 	if message.Error != nil {
 		err = &RPCError{Message: message.Error.Message}
 	}
-	x.GetBalanceByAddressResponseMessage = &GetBalanceByAddressResponseMessage{
+	x.GetBalanceByAddressResponse = &GetBalanceByAddressResponseMessage{
 		Balance: message.Balance,
 
 		Error: err,
