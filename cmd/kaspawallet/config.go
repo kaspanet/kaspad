4c335d8726e24e39cab437b5e9d98d87118168c6
stasatdaglabs <stas@daglabs.com>
2022-01-16 11:48:14 +0200

    Add --transaction-file to the sign wallet subcommand.

--- cmd/kaspawallet/config.go
+++ cmd/kaspawallet/config.go
@@ -65,9 +65,10 @@ type createUnsignedTransactionConfig struct {
 }
 
 type signConfig struct {
-	KeysFile    string `long:"keys-file" short:"f" description:"Keys file location (default: ~/.kaspawallet/keys.json (*nix), %USERPROFILE%\\AppData\\Local\\Kaspawallet\\key.json (Windows))"`
-	Password    string `long:"password" short:"p" description:"Wallet password"`
-	Transaction string `long:"transaction" short:"t" description:"The unsigned transaction to sign on (encoded in hex)" required:"true"`
+	KeysFile        string `long:"keys-file" short:"f" description:"Keys file location (default: ~/.kaspawallet/keys.json (*nix), %USERPROFILE%\\AppData\\Local\\Kaspawallet\\key.json (Windows))"`
+	Password        string `long:"password" short:"p" description:"Wallet password"`
+	Transaction     string `long:"transaction" short:"t" description:"The unsigned transaction to sign on (encoded in hex)"`
+	TransactionFile string `long:"transaction-file" short:"tf" description:"The file containing the unsigned transaction to sign on (encoded in hex)"`
 	config.NetworkFlags
 }
 
--- cmd/kaspawallet/sign.go
+++ cmd/kaspawallet/sign.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/kaspanet/kaspad/cmd/kaspawallet/keys"
 	"github.com/kaspanet/kaspad/cmd/kaspawallet/libkaspawallet"
+	"github.com/pkg/errors"
+	"io/ioutil"
 )
 
 func sign(conf *signConfig) error {
@@ -13,7 +15,23 @@ func sign(conf *signConfig) error {
 		return err
 	}
 
-	partiallySignedTransaction, err := hex.DecodeString(conf.Transaction)
+	if conf.Transaction == "" && conf.TransactionFile == "" {
+		return errors.Errorf("Either --transaction or --transaction-file is required")
+	}
+	if conf.Transaction != "" && conf.TransactionFile != "" {
+		return errors.Errorf("Both --transaction and --transaction-file cannot be passed at the same time")
+	}
+
+	transactionHex := conf.Transaction
+	if conf.TransactionFile != "" {
+		transactionHexBytes, err := ioutil.ReadFile(conf.TransactionFile)
+		if err != nil {
+			return errors.Wrapf(err, "Could not read hex from %s", conf.TransactionFile)
+		}
+		transactionHex = string(transactionHexBytes)
+	}
+
+	partiallySignedTransaction, err := hex.DecodeString(transactionHex)
 	if err != nil {
 		return err
 	}
