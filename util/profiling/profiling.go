8898a6c338b0e81eb4d889c962c4d99824c162ed
msutton <mikisiton2@gmail.com>
2022-01-27 01:45:08 +0200

    Add monitoring of heap and save heap profile if size is over some limit

--- util/profiling/profiling.go
+++ util/profiling/profiling.go
@@ -4,11 +4,16 @@ import (
 	"github.com/kaspanet/kaspad/infrastructure/logger"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	// Required for profiling
 	_ "net/http/pprof"
 
 	"github.com/kaspanet/kaspad/util/panics"
+	"runtime"
+	"runtime/pprof"
 )
 
 // Start starts the profiling server
@@ -21,4 +26,42 @@ func Start(port string, log *logger.Logger) {
 		http.Handle("/", profileRedirect)
 		log.Error(http.ListenAndServe(listenAddr, nil))
 	})
+}
+
+func TrackHeap(appDir string, log *logger.Logger) {
+	spawn := panics.GoroutineWrapperFunc(log)
+	spawn("profiling.TrackHeap", func() {
+		dumpFolder := filepath.Join(appDir, "dumps")
+		err := os.MkdirAll(dumpFolder, 0700)
+		if err != nil {
+			log.Errorf("Could not create heap dumps folder at %s", dumpFolder)
+			return
+		}
+		const limitInGigabytes = 8
+		trackHeapSize(limitInGigabytes*1024*1024*1024, dumpFolder, log)
+	})
+}
+
+func trackHeapSize(heapLimit uint64, dumpFolder string, log *logger.Logger) {
+	ticker := time.NewTicker(30 * time.Second) // Should be 10? How much time is expected from memory peak to crash?
+	defer ticker.Stop()
+	for range ticker.C {
+		memStats := &runtime.MemStats{}
+		runtime.ReadMemStats(memStats)
+		// If we passed the expected heap limit, dump the heap report to a file
+		if memStats.HeapAlloc > heapLimit {
+			heapFile := filepath.Join(dumpFolder, "heap.out") // Should we keep a few recent files or override each time?
+			log.Infof("Saving heap statistics into %s (HeapAlloc=%d > %d=heapLimit)", heapFile, memStats.HeapAlloc, heapLimit)
+			f, err := os.Create(heapFile)
+			defer f.Close()
+			if err != nil {
+				log.Infof("Could not create heap profile: %s", err)
+				continue // or should we break?
+			}
+			//runtime.GC() // get up-to-date statistics
+			if err := pprof.WriteHeapProfile(f); err != nil {
+				log.Infof("Could not write heap profile: %s", err)
+			}
+		}
+	}
 }
