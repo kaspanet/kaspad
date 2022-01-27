832133bc375d4fb1e3895fac857005d8f1226000
msutton <mikisiton2@gmail.com>
2022-01-27 16:47:24 +0200

    Fix for rare consensus bug: daa window min-time-block was not deterministic when timestamps are equal

--- domain/consensus/processes/difficultymanager/blockwindow.go
+++ domain/consensus/processes/difficultymanager/blockwindow.go
@@ -1,18 +1,18 @@
 package difficultymanager
 
 import (
-	"math"
-	"math/big"
-
 	"github.com/kaspanet/kaspad/domain/consensus/model"
-
 	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
 	"github.com/kaspanet/kaspad/util/difficulty"
+	"math"
+	"math/big"
 )
 
 type difficultyBlock struct {
 	timeInMilliseconds int64
 	Bits               uint32
+	hash               *externalapi.DomainHash
+	blueWork           *big.Int
 }
 
 type blockWindow []difficultyBlock
@@ -27,6 +27,8 @@ func (dm *difficultyManager) getDifficultyBlock(
 	return difficultyBlock{
 		timeInMilliseconds: header.TimeInMilliseconds(),
 		Bits:               header.Bits(),
+		hash:               blockHash,
+		blueWork:           header.BlueWork(),
 	}, nil
 }
 
@@ -53,19 +55,20 @@ func (dm *difficultyManager) blockWindow(stagingArea *model.StagingArea, startin
 	return window, windowHashes, nil
 }
 
-func (window blockWindow) minMaxTimestamps() (min, max int64, minIndex, maxIndex int) {
+func (window blockWindow) minMaxTimestamps() (min, max int64, minIndex int) {
 	min = math.MaxInt64
-	minIndex = math.MaxInt64
+	minIndex = 0
 	max = 0
-	maxIndex = 0
 	for i, block := range window {
-		if block.timeInMilliseconds < min {
+
+		if block.timeInMilliseconds < min ||
+			(block.timeInMilliseconds == min && block.blueWork.Cmp(window[minIndex].blueWork) < 0) ||
+			(block.timeInMilliseconds == min && block.blueWork.Cmp(window[minIndex].blueWork) == 0 && block.hash.Less(window[minIndex].hash)) {
 			min = block.timeInMilliseconds
 			minIndex = i
 		}
 		if block.timeInMilliseconds > max {
 			max = block.timeInMilliseconds
-			maxIndex = i
 		}
 	}
 	return
