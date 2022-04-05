72f046348a09f7d7bd6d5e22e99d4ff79b26244f
Mike Zak <feanorr@gmail.com>
2022-04-05 16:45:01 +0300

    Implement Sign

--- cmd/kaspawallet/daemon/server/sign.go
+++ cmd/kaspawallet/daemon/server/sign.go
@@ -3,11 +3,34 @@ package server
 import (
 	"context"
 
+	"github.com/kaspanet/kaspad/cmd/kaspawallet/libkaspawallet"
+
 	"github.com/kaspanet/kaspad/cmd/kaspawallet/daemon/pb"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *server) Sign(_ context.Context, request *pb.SignRequest) (*pb.SignResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Sign not implemented")
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	signedTransactions, err := s.signTransactions(request.UnsignedTransactions, request.Password)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.SignResponse{SignedTransactions: signedTransactions}, nil
+}
+
+func (s *server) signTransactions(unsignedTransactions [][]byte, password string) ([][]byte, error) {
+	mnemonics, err := s.keysFile.DecryptMnemonics(password)
+	if err != nil {
+		return nil, err
+	}
+	signedTransactions := make([][]byte, len(unsignedTransactions))
+	for i, unsignedTransaction := range unsignedTransactions {
+		signedTransaction, err := libkaspawallet.Sign(s.params, mnemonics, unsignedTransaction, s.keysFile.ECDSA)
+		if err != nil {
+			return nil, err
+		}
+		signedTransactions[i] = signedTransaction
+	}
+	return signedTransactions, nil
 }
