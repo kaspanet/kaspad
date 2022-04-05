556898b22dc73c83fbfc8913b202f4fa04451930
msutton <mikisiton2@gmail.com>
2022-04-05 16:03:03 +0300

    Print full json of rule violating header

--- app/rpc/rpchandlers/submit_block.go
+++ app/rpc/rpchandlers/submit_block.go
@@ -1,6 +1,7 @@
 package rpchandlers
 
 import (
+	"encoding/json"
 	"github.com/kaspanet/kaspad/app/appmessage"
 	"github.com/kaspanet/kaspad/app/protocol/protocolerrors"
 	"github.com/kaspanet/kaspad/app/rpc/rpccontext"
@@ -56,6 +57,12 @@ func HandleSubmitBlock(context *rpccontext.Context, _ *router.Router, request ap
 		isProtocolOrRuleError := errors.As(err, &ruleerrors.RuleError{}) || errors.As(err, &protocolerrors.ProtocolError{})
 		if !isProtocolOrRuleError {
 			return nil, err
+		}
+
+		jsonBytes, _ := json.MarshalIndent(submitBlockRequest.Block.Header, "", "    ")
+		if jsonBytes != nil {
+			log.Warnf("The RPC submitted block triggered a rule/protocol error (%s), printing "+
+				"the full header for debug purposes: %s", err, string(jsonBytes))
 		}
 
 		return &appmessage.SubmitBlockResponseMessage{
