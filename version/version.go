67259026639a0f2a744b80f64e150782e5b4a35c
Ori Newman <orinewman1@gmail.com>
2022-04-06 22:06:57 +0300

    Bump version and update changelog (#2011)

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 const (
 	appMajor uint = 0
 	appMinor uint = 11
-	appPatch uint = 16
+	appPatch uint = 17
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
