696e6515d612f74cfcabc4f163d52e89297cd046
Mike Zak <feanorr@gmail.com>
2022-05-01 12:23:47 +0300

    Fix test to not expect empty notifications

--- testing/integration/selected_parent_chain_test.go
+++ testing/integration/selected_parent_chain_test.go
@@ -65,36 +65,10 @@ func TestVirtualSelectedParentChain(t *testing.T) {
 	chain2TipHashString := chain2TipHash.String()
 
 	// For the first `blockAmountToMine - 1` blocks we don't expect
-	// the chain to change at all
-	for i := 0; i < blockAmountToMine-1; i++ {
-		notification := <-onVirtualSelectedParentChainChangedChan
-		if len(notification.RemovedChainBlockHashes) > 0 {
-			t.Fatalf("RemovedChainBlockHashes is unexpectedly not empty")
-		}
-		if len(notification.AddedChainBlockHashes) > 0 {
-			t.Fatalf("AddedChainBlockHashes is unexpectedly not empty")
-		}
-	}
+	// the chain to change at all, thus there will be no notifications
 
-	// Either the next block could cause a reorg or the one
-	// after it
-	potentialReorgNotification1 := <-onVirtualSelectedParentChainChangedChan
-	potentialReorgNotification2 := <-onVirtualSelectedParentChainChangedChan
-	var reorgNotification *appmessage.VirtualSelectedParentChainChangedNotificationMessage
-	var nonReorgNotification *appmessage.VirtualSelectedParentChainChangedNotificationMessage
-	if len(potentialReorgNotification1.RemovedChainBlockHashes) > 0 {
-		reorgNotification = potentialReorgNotification1
-		nonReorgNotification = potentialReorgNotification2
-	} else {
-		reorgNotification = potentialReorgNotification2
-		nonReorgNotification = potentialReorgNotification1
-	}
-
-	// Make sure that the non-reorg notification has nothing
-	// in `removed`
-	if len(nonReorgNotification.RemovedChainBlockHashes) > 0 {
-		t.Fatalf("nonReorgNotification.RemovedChainBlockHashes is unexpectedly not empty")
-	}
+	// Either the next block or the one after it will cause a reorg
+	reorgNotification := <-onVirtualSelectedParentChainChangedChan
 
 	// Make sure that the reorg notification contains exactly
 	// `blockAmountToMine` blocks in its `removed`
