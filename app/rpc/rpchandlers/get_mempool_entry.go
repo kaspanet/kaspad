4a9dc3806802f9c8e6816f86a374d24e391baf06
D-Stacks <doransmail@gmail.com>
2022-06-05 17:47:36 +0200

    update and simplyfiy get_mempool_entry handler

--- app/rpc/rpchandlers/get_mempool_entry.go
+++ app/rpc/rpchandlers/get_mempool_entry.go
@@ -12,7 +12,7 @@ import (
 func HandleGetMempoolEntry(context *rpccontext.Context, _ *router.Router, request appmessage.Message) (appmessage.Message, error) {
 
 	transaction := &externalapi.DomainTransaction{}
-	var ok bool
+	var found bool
 	var isOrphan bool
 
 	getMempoolEntryRequest := request.(*appmessage.GetMempoolEntryRequestMessage)
@@ -24,43 +24,21 @@ func HandleGetMempoolEntry(context *rpccontext.Context, _ *router.Router, reques
 		return errorMessage, nil
 	}
 
-	if getMempoolEntryRequest.FilterTransactionPool && getMempoolEntryRequest.IncludeOrphanPool { //both true
-
-		transaction, ok = context.Domain.MiningManager().GetTransaction(transactionID)
-		if !ok {
-			transaction, ok = context.Domain.MiningManager().GetOrphanTransaction(transactionID)
-			if !ok {
-				errorMessage := &appmessage.GetMempoolEntryResponseMessage{}
-				errorMessage.Error = appmessage.RPCErrorf("Transaction %s was not found", transactionID)
-				return errorMessage, nil
-			}
-			isOrphan = true
-		}
-		isOrphan = false
-
-	} else if getMempoolEntryRequest.FilterTransactionPool && !getMempoolEntryRequest.IncludeOrphanPool { //only transactions
-		transaction, ok = context.Domain.MiningManager().GetTransaction(transactionID)
-		if !ok {
-			errorMessage := &appmessage.GetMempoolEntryResponseMessage{}
-			errorMessage.Error = appmessage.RPCErrorf("Transaction %s was not found", transactionID)
-			return errorMessage, nil
-		}
-		isOrphan = true
-
-	} else if !getMempoolEntryRequest.FilterTransactionPool && getMempoolEntryRequest.IncludeOrphanPool { //only orphans
-		transaction, ok = context.Domain.MiningManager().GetOrphanTransaction(transactionID)
-		if !ok {
-			errorMessage := &appmessage.GetMempoolEntryResponseMessage{}
-			errorMessage.Error = appmessage.RPCErrorf("Transaction %s was not found", transactionID)
-			return errorMessage, nil
-		}
-		isOrphan = false
-	} else if !(getMempoolEntryRequest.FilterTransactionPool || getMempoolEntryRequest.IncludeOrphanPool) {
-		errorMessage := &appmessage.GetMempoolEntryResponseMessage{}
-		errorMessage.Error = appmessage.RPCErrorf("Request is not querying any mempool pools")
-		return errorMessage, nil
-
+	if !getMempoolEntryRequest.FilterTransactionPool {
+		transaction, found = context.Domain.MiningManager().GetTransaction(transactionID)
 	}
+
+	if getMempoolEntryRequest.IncludeOrphanPool && !found {
+		transaction, found = context.Domain.MiningManager().GetOrphanTransaction(transactionID)
+		isOrphan = true
+	}
+
+	if !found {
+		errorMessage := &appmessage.GetMempoolEntryResponseMessage{}
+		errorMessage.Error = appmessage.RPCErrorf("Transaction %s was not found", transactionID)
+		return errorMessage, nil
+	}
+
 	rpcTransaction := appmessage.DomainTransactionToRPCTransaction(transaction)
 	err = context.PopulateTransactionWithVerboseData(rpcTransaction, nil)
 	if err != nil {
