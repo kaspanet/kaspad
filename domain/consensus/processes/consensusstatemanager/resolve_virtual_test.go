733c2eda7f30664de2c1df2cba488e4b84ca1733
msutton <mikisiton2@gmail.com>
2022-07-14 15:45:09 +0300

    Improve and clarify resolve virtual tests

--- domain/consensus/processes/consensusstatemanager/resolve_virtual_test.go
+++ domain/consensus/processes/consensusstatemanager/resolve_virtual_test.go
@@ -24,11 +24,14 @@ func TestAddBlockBetweenResolveVirtualCalls(t *testing.T) {
 		}
 		defer teardown(false)
 
+		hashes := []*externalapi.DomainHash{consensusConfig.GenesisHash}
+
 		// Create a chain of blocks
 		const initialChainLength = 10
 		previousBlockHash := consensusConfig.GenesisHash
 		for i := 0; i < initialChainLength; i++ {
 			previousBlockHash, _, err = tc.AddBlock([]*externalapi.DomainHash{previousBlockHash}, nil, nil)
+			hashes = append(hashes, previousBlockHash)
 			if err != nil {
 				t.Fatalf("Error mining block no. %d in initial chain: %+v", i, err)
 			}
@@ -43,6 +46,7 @@ func TestAddBlockBetweenResolveVirtualCalls(t *testing.T) {
 				t.Fatalf("Error mining block no. %d in re-org chain: %+v", i, err)
 			}
 			previousBlockHash = consensushashing.BlockHash(previousBlock)
+			hashes = append(hashes, previousBlockHash)
 
 			// Do not UTXO validate in order to resolve virtual later
 			err = tc.ValidateAndInsertBlock(previousBlock, false)
@@ -81,6 +85,7 @@ func TestAddBlockBetweenResolveVirtualCalls(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error mining block during virtual resolution of reorg: %+v", err)
 		}
+		hashes = append(hashes, consensushashing.BlockHash(blockTemplate.Block))
 
 		// Complete resolving virtual
 		for !isCompletelyResolved {
@@ -89,6 +94,8 @@ func TestAddBlockBetweenResolveVirtualCalls(t *testing.T) {
 				t.Fatalf("Error resolving virtual in re-org chain: %+v", err)
 			}
 		}
+
+		verifyUtxoDiffPaths(t, tc, hashes)
 	})
 }
 
@@ -103,11 +110,14 @@ func TestAddGenesisChildAfterOneResolveVirtualCall(t *testing.T) {
 		}
 		defer teardown(false)
 
+		hashes := []*externalapi.DomainHash{consensusConfig.GenesisHash}
+
 		// Create a chain of blocks
 		const initialChainLength = 6
 		previousBlockHash := consensusConfig.GenesisHash
 		for i := 0; i < initialChainLength; i++ {
 			previousBlockHash, _, err = tc.AddBlock([]*externalapi.DomainHash{previousBlockHash}, nil, nil)
+			hashes = append(hashes, previousBlockHash)
 			if err != nil {
 				t.Fatalf("Error mining block no. %d in initial chain: %+v", i, err)
 			}
@@ -122,6 +132,7 @@ func TestAddGenesisChildAfterOneResolveVirtualCall(t *testing.T) {
 				t.Fatalf("Error mining block no. %d in re-org chain: %+v", i, err)
 			}
 			previousBlockHash = consensushashing.BlockHash(previousBlock)
+			hashes = append(hashes, previousBlockHash)
 
 			// Do not UTXO validate in order to resolve virtual later
 			err = tc.ValidateAndInsertBlock(previousBlock, false)
@@ -148,6 +159,8 @@ func TestAddGenesisChildAfterOneResolveVirtualCall(t *testing.T) {
 				t.Fatalf("Error resolving virtual in re-org chain: %+v", err)
 			}
 		}
+
+		verifyUtxoDiffPaths(t, tc, hashes)
 	})
 }
 
@@ -162,11 +175,14 @@ func TestAddGenesisChildAfterTwoResolveVirtualCalls(t *testing.T) {
 		}
 		defer teardown(false)
 
+		hashes := []*externalapi.DomainHash{consensusConfig.GenesisHash}
+
 		// Create a chain of blocks
 		const initialChainLength = 6
 		previousBlockHash := consensusConfig.GenesisHash
 		for i := 0; i < initialChainLength; i++ {
 			previousBlockHash, _, err = tc.AddBlock([]*externalapi.DomainHash{previousBlockHash}, nil, nil)
+			hashes = append(hashes, previousBlockHash)
 			if err != nil {
 				t.Fatalf("Error mining block no. %d in initial chain: %+v", i, err)
 			}
@@ -181,6 +197,7 @@ func TestAddGenesisChildAfterTwoResolveVirtualCalls(t *testing.T) {
 				t.Fatalf("Error mining block no. %d in re-org chain: %+v", i, err)
 			}
 			previousBlockHash = consensushashing.BlockHash(previousBlock)
+			hashes = append(hashes, previousBlockHash)
 
 			// Do not UTXO validate in order to resolve virtual later
 			err = tc.ValidateAndInsertBlock(previousBlock, false)
@@ -213,10 +230,12 @@ func TestAddGenesisChildAfterTwoResolveVirtualCalls(t *testing.T) {
 				t.Fatalf("Error resolving virtual in re-org chain: %+v", err)
 			}
 		}
+
+		verifyUtxoDiffPaths(t, tc, hashes)
 	})
 }
 
-func TestResolveVirtualMess(t *testing.T) {
+func TestResolveVirtualBackAndForthReorgs(t *testing.T) {
 
 	testutils.ForAllNets(t, true, func(t *testing.T, consensusConfig *consensus.Config) {
 		factory := consensus.NewFactory()
@@ -231,7 +250,7 @@ func TestResolveVirtualMess(t *testing.T) {
 		blocks := make(map[externalapi.DomainHash]string)
 		blocks[*consensusConfig.GenesisHash] = "g"
 		blocks[*model.VirtualBlockHash] = "v"
-		//fmt.Printf("%s\n\n", consensusConfig.GenesisHash)
+		printfDebug("%s\n\n", consensusConfig.GenesisHash)
 
 		// Create a chain of blocks
 		const initialChainLength = 6
@@ -240,17 +259,20 @@ func TestResolveVirtualMess(t *testing.T) {
 			previousBlockHash, _, err = tc.AddBlock([]*externalapi.DomainHash{previousBlockHash}, nil, nil)
 			blocks[*previousBlockHash] = fmt.Sprintf("A_%d", i)
 			hashes = append(hashes, previousBlockHash)
-			//fmt.Printf("A_%d: %s\n", i, previousBlockHash)
+			printfDebug("A_%d: %s\n", i, previousBlockHash)
 
 			if err != nil {
 				t.Fatalf("Error mining block no. %d in initial chain: %+v", i, err)
 			}
 		}
 
-		//fmt.Printf("\n")
+		printfDebug("\n")
+		verifyUtxoDiffPaths(t, tc, hashes)
+
+		firstChainTip := previousBlockHash
 
 		// Mine a chain with more blocks, to re-organize the DAG
-		const reorgChainLength = 20 // initialChainLength + 1
+		const reorgChainLength = 12 // initialChainLength + 1
 		previousBlockHash = consensusConfig.GenesisHash
 		for i := 0; i < reorgChainLength; i++ {
 			previousBlock, _, err := tc.BuildBlockWithParents([]*externalapi.DomainHash{previousBlockHash}, nil, nil)
@@ -260,7 +282,7 @@ func TestResolveVirtualMess(t *testing.T) {
 			previousBlockHash = consensushashing.BlockHash(previousBlock)
 			blocks[*previousBlockHash] = fmt.Sprintf("B_%d", i)
 			hashes = append(hashes, previousBlockHash)
-			//fmt.Printf("B_%d: %s\n", i, previousBlockHash)
+			printfDebug("B_%d: %s\n", i, previousBlockHash)
 
 			// Do not UTXO validate in order to resolve virtual later
 			err = tc.ValidateAndInsertBlock(previousBlock, false)
@@ -269,14 +291,15 @@ func TestResolveVirtualMess(t *testing.T) {
 			}
 		}
 
-		//fmt.Printf("\n")
+		printfDebug("\n")
 
-		//printUtxoDiffChildren(t, hashes, tc, blocks)
+		printUtxoDiffChildren(t, tc, hashes, blocks)
+		verifyUtxoDiffPaths(t, tc, hashes)
 
 		// Resolve one step
 		_, err = tc.ResolveVirtualWithMaxParam(3)
 		if err != nil {
-			printUtxoDiffChildren(t, hashes, tc, blocks)
+			printUtxoDiffChildren(t, tc, hashes, blocks)
 			t.Fatalf("Error resolving virtual in re-org chain: %+v", err)
 		}
 
@@ -294,12 +317,90 @@ func TestResolveVirtualMess(t *testing.T) {
 			}
 		}
 
-		//printUtxoDiffChildren(t, hashes, tc, blocks)
+		printUtxoDiffChildren(t, tc, hashes, blocks)
+		verifyUtxoDiffPaths(t, tc, hashes)
+
+		// Now get the first chain back to the wining position
+		previousBlockHash = firstChainTip
+		for i := 0; i < reorgChainLength; i++ {
+			previousBlockHash, _, err = tc.AddBlock([]*externalapi.DomainHash{previousBlockHash}, nil, nil)
+			blocks[*previousBlockHash] = fmt.Sprintf("A_%d", initialChainLength+i)
+			hashes = append(hashes, previousBlockHash)
+			printfDebug("A_%d: %s\n", initialChainLength+i, previousBlockHash)
+
+			if err != nil {
+				t.Fatalf("Error mining block no. %d in initial chain: %+v", initialChainLength+i, err)
+			}
+		}
+
+		printfDebug("\n")
+
+		printUtxoDiffChildren(t, tc, hashes, blocks)
+		verifyUtxoDiffPaths(t, tc, hashes)
 	})
 }
 
-func printUtxoDiffChildren(t *testing.T, hashes []*externalapi.DomainHash, tc testapi.TestConsensus, blocks map[externalapi.DomainHash]string) {
-	fmt.Printf("\n===============================\nBlock\t\tDiff child\n")
+func verifyUtxoDiffPathToRoot(t *testing.T, tc testapi.TestConsensus, stagingArea *model.StagingArea, block, utxoDiffRoot *externalapi.DomainHash) {
+	current := block
+	for !current.Equal(utxoDiffRoot) {
+		hasUTXODiffChild, err := tc.UTXODiffStore().HasUTXODiffChild(tc.DatabaseContext(), stagingArea, current)
+		if err != nil {
+			t.Fatalf("Error while reading utxo diff store: %+v", err)
+		}
+		if !hasUTXODiffChild {
+			t.Fatalf("%s is expected to have a UTXO diff child", current)
+		}
+		current, err = tc.UTXODiffStore().UTXODiffChild(tc.DatabaseContext(), stagingArea, current)
+		if err != nil {
+			t.Fatalf("Error while reading utxo diff store: %+v", err)
+		}
+	}
+}
+
+func verifyUtxoDiffPaths(t *testing.T, tc testapi.TestConsensus, hashes []*externalapi.DomainHash) {
+	stagingArea := model.NewStagingArea()
+
+	virtualGHOSTDAGData, err := tc.GHOSTDAGDataStore().Get(tc.DatabaseContext(), stagingArea, model.VirtualBlockHash, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	utxoDiffRoot := virtualGHOSTDAGData.SelectedParent()
+	hasUTXODiffChild, err := tc.UTXODiffStore().HasUTXODiffChild(tc.DatabaseContext(), stagingArea, utxoDiffRoot)
+	if err != nil {
+		t.Fatalf("Error while reading utxo diff store: %+v", err)
+	}
+	if hasUTXODiffChild {
+		t.Fatalf("Virtual selected parent is not expected to have an explicit diff child")
+	}
+	_, err = tc.UTXODiffStore().UTXODiff(tc.DatabaseContext(), stagingArea, utxoDiffRoot)
+	if err != nil {
+		t.Fatalf("Virtual selected parent is expected to have a utxo diff: %+v", err)
+	}
+
+	for _, block := range hashes {
+		hasUTXODiffChild, err = tc.UTXODiffStore().HasUTXODiffChild(tc.DatabaseContext(), stagingArea, block)
+		if err != nil {
+			t.Fatalf("Error while reading utxo diff store: %+v", err)
+		}
+		isOnVirtualSelectedChain, err := tc.DAGTopologyManager().IsInSelectedParentChainOf(stagingArea, block, utxoDiffRoot)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// We expect a valid path to root in both cases: (i) block has a diff child, (ii) block is on the virtual selected chain
+		if hasUTXODiffChild || isOnVirtualSelectedChain {
+			verifyUtxoDiffPathToRoot(t, tc, stagingArea, block, utxoDiffRoot)
+		}
+	}
+}
+
+func printfDebug(format string, a ...any) {
+	// Uncomment below when debugging the test
+	//fmt.Printf(format, a...)
+}
+
+func printUtxoDiffChildren(t *testing.T, tc testapi.TestConsensus, hashes []*externalapi.DomainHash, blocks map[externalapi.DomainHash]string) {
+	printfDebug("\n===============================\nBlock\t\tDiff child\n")
 	stagingArea := model.NewStagingArea()
 	for _, block := range hashes {
 		hasUTXODiffChild, err := tc.UTXODiffStore().HasUTXODiffChild(tc.DatabaseContext(), stagingArea, block)
@@ -311,10 +412,10 @@ func printUtxoDiffChildren(t *testing.T, hashes []*externalapi.DomainHash, tc te
 			if err != nil {
 				t.Fatalf("Error while reading utxo diff store: %+v", err)
 			}
-			fmt.Printf("%s\t\t\t%s\n", blocks[*block], blocks[*utxoDiffChild])
+			printfDebug("%s\t\t\t%s\n", blocks[*block], blocks[*utxoDiffChild])
 		} else {
-			fmt.Printf("%s\n", blocks[*block])
+			printfDebug("%s\n", blocks[*block])
 		}
 	}
-	fmt.Printf("\n===============================\n")
+	printfDebug("\n===============================\n")
 }
