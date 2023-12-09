ad3f83a3bce3f951c627b156756c996c89dba5e4
Lazzeruz <larsvdh@gmail.com>
2023-12-10 00:05:31 +0100

    fixed capitalization of ZoomydApp
    title says it

--- app/app.go
+++ app/app.go
@@ -55,7 +55,7 @@ func StartApp() error {
 	defer logger.BackendLog.Close()
 	defer panics.HandlePanic(log, "MAIN", nil)
 
-	app := &zoomydApp{cfg: cfg}
+	app := &ZoomydApp{cfg: cfg}
 
 	// Call serviceMain on Windows to handle running as a service. When
 	// the return isService flag is true, exit now since we ran as a
@@ -73,7 +73,7 @@ func StartApp() error {
 	return app.main(nil)
 }
 
-func (app *zoomydApp) main(startedChan chan<- struct{}) error {
+func (app *ZoomydApp) main(startedChan chan<- struct{}) error {
 	// Get a channel that will be closed when a shutdown signal has been
 	// triggered either from an OS signal such as SIGINT (Ctrl+C) or from
 	// another subsystem such as the RPC server.
