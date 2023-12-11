71f5677854619d1a2c0cf4cb2a5ac73203845f37
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 03:38:52 +0100

    Update errors.go

--- app/protocol/flowcontext/errors.go
+++ app/protocol/flowcontext/errors.go
@@ -5,9 +5,9 @@ import (
 	"strings"
 	"sync/atomic"
 
-	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
+	"github.com/fabbez/topiad/infrastructure/network/netadapter/router"
 
-	"github.com/kaspanet/kaspad/app/protocol/protocolerrors"
+	"github.com/fabbez/topiad/app/protocol/protocolerrors"
 )
 
 var (
