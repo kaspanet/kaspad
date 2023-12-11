bab22f535ea083e62554153f7074183b0d3ed368
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 10:13:55 +0100

    Update log.go

--- app/log.go
+++ app/log.go
@@ -6,7 +6,7 @@
 package app
 
 import (
-	"github.com/kaspanet/kaspad/infrastructure/logger"
+	"github.com/fabbez/topiad/infrastructure/logger"
 )
 
-var log = logger.RegisterSubSystem("KASD")
+var log = logger.RegisterSubSystem("TOPD")
