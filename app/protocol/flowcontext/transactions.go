3793b2d528a9440c10e3b8c38313df21c3f6d25c
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 03:51:34 +0100

    Update transactions.go

--- app/protocol/flowcontext/transactions.go
+++ app/protocol/flowcontext/transactions.go
@@ -3,9 +3,9 @@ package flowcontext
 import (
 	"time"
 
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/consensushashing"
+	"github.com/fabbez/topiad/app/appmessage"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/utils/consensushashing"
 )
 
 // TransactionIDPropagationInterval is the interval between transaction IDs propagations
