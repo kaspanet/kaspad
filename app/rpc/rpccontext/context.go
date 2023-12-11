3554b3a16554209b0828d55091f68fd0249dc17b
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 04:11:54 +0100

    Update context.go

--- app/rpc/rpccontext/context.go
+++ app/rpc/rpccontext/context.go
@@ -1,13 +1,13 @@
 package rpccontext
 
 import (
-	"github.com/kaspanet/kaspad/app/protocol"
-	"github.com/kaspanet/kaspad/domain"
-	"github.com/kaspanet/kaspad/domain/utxoindex"
-	"github.com/kaspanet/kaspad/infrastructure/config"
-	"github.com/kaspanet/kaspad/infrastructure/network/addressmanager"
-	"github.com/kaspanet/kaspad/infrastructure/network/connmanager"
-	"github.com/kaspanet/kaspad/infrastructure/network/netadapter"
+	"github.com/fabbez/topiad/app/protocol"
+	"github.com/fabbez/topiad/domain"
+	"github.com/fabbez/topiad/domain/utxoindex"
+	"github.com/fabbez/topiad/infrastructure/config"
+	"github.com/fabbez/topiad/infrastructure/network/addressmanager"
+	"github.com/fabbez/topiad/infrastructure/network/connmanager"
+	"github.com/fabbez/topiad/infrastructure/network/netadapter"
 )
 
 // Context represents the RPC context
