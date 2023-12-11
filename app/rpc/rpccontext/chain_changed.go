07ff6451bb4b85699a72c226abaae0bdf314e726
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 04:09:19 +0100

    Update chain_changed.go

--- app/rpc/rpccontext/chain_changed.go
+++ app/rpc/rpccontext/chain_changed.go
@@ -1,9 +1,9 @@
 package rpccontext
 
 import (
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/consensushashing"
+	"github.com/fabbez/topiad/app/appmessage"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/utils/consensushashing"
 )
 
 // ConvertVirtualSelectedParentChainChangesToChainChangedNotificationMessage converts
