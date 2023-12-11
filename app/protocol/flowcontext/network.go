0a805f153ffcb2f9c910817b57fbfc4cb3b9f9af
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 03:47:59 +0100

    Update network.go

--- app/protocol/flowcontext/network.go
+++ app/protocol/flowcontext/network.go
@@ -1,11 +1,11 @@
 package flowcontext
 
 import (
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/app/protocol/common"
-	peerpkg "github.com/kaspanet/kaspad/app/protocol/peer"
-	"github.com/kaspanet/kaspad/infrastructure/network/connmanager"
-	"github.com/kaspanet/kaspad/infrastructure/network/netadapter"
+	"github.com/fabbez/topiad/app/appmessage"
+	"github.com/fabbez/topiad/app/protocol/common"
+	peerpkg "github.com/topiad/kaspad/app/protocol/peer"
+	"github.com/fabbez/topiad/infrastructure/network/connmanager"
+	"github.com/fabbez/topiad/infrastructure/network/netadapter"
 	"github.com/pkg/errors"
 )
 
