12aeaa153bfcae62f947fb7cd5a1b48f196f89b5
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 11:39:24 +0100

    Update sendversion.go

--- app/protocol/flows/handshake/sendversion.go
+++ app/protocol/flows/handshake/sendversion.go
@@ -1,22 +1,22 @@
 package handshake
 
 import (
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/app/protocol/common"
-	peerpkg "github.com/kaspanet/kaspad/app/protocol/peer"
-	"github.com/kaspanet/kaspad/infrastructure/logger"
-	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
-	"github.com/kaspanet/kaspad/version"
+	"github.com/fabbez/topiad/app/appmessage"
+	"github.com/fabbez/topiad/app/protocol/common"
+	peerpkg "github.com/fabbez/topiad/app/protocol/peer"
+	"github.com/fabbez/topiad/infrastructure/logger"
+	"github.com/fabbez/topiad/infrastructure/network/netadapter/router"
+	"github.com/fabbez/topiad/version"
 	"github.com/pkg/errors"
 )
 
 var (
 	// userAgentName is the user agent name and is used to help identify
-	// ourselves to other kaspa peers.
-	userAgentName = "kaspad"
+	// ourselves to other topia peers.
+	userAgentName = "topiad"
 
 	// userAgentVersion is the user agent version and is used to help
-	// identify ourselves to other kaspa peers.
+	// identify ourselves to other topia peers.
 	userAgentVersion = version.Version()
 
 	// defaultServices describes the default services that are supported by
