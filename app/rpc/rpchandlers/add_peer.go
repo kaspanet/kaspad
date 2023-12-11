d45103816bb9fda76a51c812dbc39382929e2f5d
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-11 04:21:22 +0100

    Update add_peer.go

--- app/rpc/rpchandlers/add_peer.go
+++ app/rpc/rpchandlers/add_peer.go
@@ -1,10 +1,10 @@
 package rpchandlers
 
 import (
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/app/rpc/rpccontext"
-	"github.com/kaspanet/kaspad/infrastructure/network/netadapter/router"
-	"github.com/kaspanet/kaspad/util/network"
+	"github.com/fabbez/topiad/app/appmessage"
+	"github.com/fabbez/topiad/app/rpc/rpccontext"
+	"github.com/fabbez/topiad/infrastructure/network/netadapter/router"
+	"github.com/fabbez/topiad/util/network"
 )
 
 // HandleAddPeer handles the respectively named RPC command
