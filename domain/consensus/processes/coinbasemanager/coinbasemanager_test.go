d162aa2667e72f7b08cfe14e4ee05cea8eb021e1
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-13 20:10:56 +0100

    Update coinbasemanager_test.go

--- domain/consensus/processes/coinbasemanager/coinbasemanager_test.go
+++ domain/consensus/processes/coinbasemanager/coinbasemanager_test.go
@@ -1,9 +1,9 @@
 package coinbasemanager
 
 import (
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/constants"
-	"github.com/kaspanet/kaspad/domain/dagconfig"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/utils/constants"
+	"github.com/fabbez/topiad/domain/dagconfig"
 	"strconv"
 	"testing"
 )
