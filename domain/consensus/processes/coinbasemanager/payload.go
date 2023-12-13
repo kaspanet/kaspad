c8f0dbda5b51b18e0fdcdea8788cd7a9a647d5bc
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-13 20:08:51 +0100

    Update payload.go

--- domain/consensus/processes/coinbasemanager/payload.go
+++ domain/consensus/processes/coinbasemanager/payload.go
@@ -3,8 +3,8 @@ package coinbasemanager
 import (
 	"encoding/binary"
 
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/ruleerrors"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/ruleerrors"
 	"github.com/pkg/errors"
 )
 
