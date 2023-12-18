77ef6fba2fe9077bbf03aaed85a6b1571c751090
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-19 00:00:51 +0100

    Update calculate_signature_hash_test.go

--- domain/consensus/utils/consensushashing/calculate_signature_hash_test.go
+++ domain/consensus/utils/consensushashing/calculate_signature_hash_test.go
@@ -3,18 +3,18 @@ package consensushashing_test
 import (
 	"encoding/hex"
 	"fmt"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/subnetworks"
+	"github.com/fabbez/topiad/domain/consensus/utils/subnetworks"
 	"testing"
 
-	"github.com/kaspanet/go-secp256k1"
+	"github.com/fabbez/go-secp256k1"
 
-	"github.com/kaspanet/kaspad/domain/consensus/utils/consensushashing"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/txscript"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/utxo"
-	"github.com/kaspanet/kaspad/domain/dagconfig"
-	"github.com/kaspanet/kaspad/util"
+	"github.com/fabbez/topiad/domain/consensus/utils/consensushashing"
+	"github.com/fabbez/topiad/domain/consensus/utils/txscript"
+	"github.com/fabbez/topiad/domain/consensus/utils/utxo"
+	"github.com/fabbez/topiad/domain/dagconfig"
+	"github.com/fabbez/topiad/util"
 
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
 )
 
 // shortened versions of SigHash types to fit in single line of test case
