b073006ec51d4b55e02217bffe17bbb5999e90d0
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-18 23:55:06 +0100

    Update block.go

--- domain/consensus/utils/consensushashing/block.go
+++ domain/consensus/utils/consensushashing/block.go
@@ -3,10 +3,10 @@ package consensushashing
 import (
 	"io"
 
-	"github.com/kaspanet/kaspad/domain/consensus/utils/serialization"
+	"github.com/fabbez/topiad/domain/consensus/utils/serialization"
 
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/hashes"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/utils/hashes"
 	"github.com/pkg/errors"
 )
 
