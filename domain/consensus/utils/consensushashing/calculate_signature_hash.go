56dd9396478d60f2852c9b4c610544978f6c29ee
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-18 23:56:30 +0100

    Update calculate_signature_hash.go

--- domain/consensus/utils/consensushashing/calculate_signature_hash.go
+++ domain/consensus/utils/consensushashing/calculate_signature_hash.go
@@ -1,10 +1,10 @@
 package consensushashing
 
 import (
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/hashes"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/serialization"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/subnetworks"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/utils/hashes"
+	"github.com/fabbez/topiad/domain/consensus/utils/serialization"
+	"github.com/fabbez/topiad/domain/consensus/utils/subnetworks"
 	"github.com/pkg/errors"
 )
 
