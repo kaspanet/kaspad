5cabbf46de29dbd8889a9045a3bfb1cabc022a5a
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-21 21:00:44 +0100

    Update hash.go

--- domain/consensus/database/binaryserialization/hash.go
+++ domain/consensus/database/binaryserialization/hash.go
@@ -1,7 +1,7 @@
 package binaryserialization
 
 import (
-	"github.com/kaspanet/kaspad/domain/consensus/model/externalapi"
+	"github.com/fabbez/topiad/domain/consensus/model/externalapi"
 	"github.com/pkg/errors"
 )
 
