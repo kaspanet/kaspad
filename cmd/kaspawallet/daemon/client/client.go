fc4c2d4c19f1939ab435cd56887cca7f8f2a67f8
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-23 02:14:27 +0100

    Update client.go

--- cmd/kaspawallet/daemon/client/client.go
+++ cmd/kaspawallet/daemon/client/client.go
@@ -2,17 +2,17 @@ package client
 
 import (
 	"context"
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/daemon/server"
+	"github.com/fabbez/topiad/cmd/topiawallet/daemon/server"
 	"time"
 
 	"github.com/pkg/errors"
 
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/daemon/pb"
+	"github.com/fabbez/topiad/cmd/topiawallet/daemon/pb"
 	"google.golang.org/grpc"
 )
 
-// Connect connects to the kaspawalletd server, and returns the client instance
-func Connect(address string) (pb.KaspawalletdClient, func(), error) {
+// Connect connects to the topiawalletd server, and returns the client instance
+func Connect(address string) (pb.topiawalletdClient, func(), error) {
 	// Connection is local, so 1 second timeout is sufficient
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -20,12 +20,12 @@ func Connect(address string) (pb.KaspawalletdClient, func(), error) {
 	conn, err := grpc.DialContext(ctx, address, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(server.MaxDaemonSendMsgSize)))
 	if err != nil {
 		if errors.Is(err, context.DeadlineExceeded) {
-			return nil, nil, errors.New("kaspawallet daemon is not running, start it with `kaspawallet start-daemon`")
+			return nil, nil, errors.New("topiawallet daemon is not running, start it with `topiawallet start-daemon`")
 		}
 		return nil, nil, err
 	}
 
-	return pb.NewKaspawalletdClient(conn), func() {
+	return pb.NewtopiawalletdClient(conn), func() {
 		conn.Close()
 	}, nil
 }
