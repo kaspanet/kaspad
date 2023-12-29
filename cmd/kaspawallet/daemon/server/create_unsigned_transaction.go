d35b9303fe96307f25fa339b345a6d950ff94ecc
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 11:33:26 +0100

    Update create_unsigned_transaction.go

--- cmd/kaspawallet/daemon/server/create_unsigned_transaction.go
+++ cmd/kaspawallet/daemon/server/create_unsigned_transaction.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/daemon/pb"
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/libkaspawallet"
-	"github.com/kaspanet/kaspad/domain/consensus/utils/constants"
-	"github.com/kaspanet/kaspad/util"
+	"github.com/fabbez/topiad/cmd/kaspawallet/daemon/pb"
+	"github.com/fabbez/topiad/cmd/kaspawallet/libkaspawallet"
+	"github.com/fabbez/topiad/domain/consensus/utils/constants"
+	"github.com/fabbez/topiad/util"
 	"github.com/pkg/errors"
 	"golang.org/x/exp/slices"
 )
