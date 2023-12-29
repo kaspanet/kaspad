533102868b2eb85efa37831bd4e07874d3506c4a
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 11:19:44 +0100

    Update client.go

--- cmd/kaspaminer/client.go
+++ cmd/kaspaminer/client.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/kaspanet/kaspad/app/appmessage"
-	"github.com/kaspanet/kaspad/infrastructure/logger"
-	"github.com/kaspanet/kaspad/infrastructure/network/rpcclient"
+	"github.com/fabbez/topiad/app/appmessage"
+	"github.com/fabbez/topiad/infrastructure/logger"
+	"github.com/fabbez/topiad/infrastructure/network/rpcclient"
 	"github.com/pkg/errors"
 	"time"
 )
