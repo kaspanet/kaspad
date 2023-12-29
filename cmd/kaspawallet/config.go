1a766393c38f2b760eb5bd8f614f6d1d3d444bdb
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 12:31:11 +0100

    Update config.go

--- cmd/kaspawallet/config.go
+++ cmd/kaspawallet/config.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/kaspanet/kaspad/infrastructure/config"
+	"github.com/fabbez/topiad/infrastructure/config"
 	"github.com/pkg/errors"
 
 	"github.com/jessevdk/go-flags"
