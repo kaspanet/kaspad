3c0733633d520e00d02df648b3d8e8e5003d2aad
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 11:35:53 +0100

    Update rpc.go

--- cmd/kaspawallet/daemon/server/rpc.go
+++ cmd/kaspawallet/daemon/server/rpc.go
@@ -3,8 +3,8 @@ package server
 import (
 	"time"
 
-	"github.com/kaspanet/kaspad/domain/dagconfig"
-	"github.com/kaspanet/kaspad/infrastructure/network/rpcclient"
+	"github.com/fabbez/topiad/domain/dagconfig"
+	"github.com/fabbez/topiad/infrastructure/network/rpcclient"
 )
 
 func connectToRPC(params *dagconfig.Params, rpcServer string, timeout uint32) (*rpcclient.RPCClient, error) {
