34fd89632f3d1f6bf61b94c6fd89ab9db019203b
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 12:32:52 +0100

    Update create.go

--- cmd/kaspawallet/create.go
+++ cmd/kaspawallet/create.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/libkaspawallet"
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/libkaspawallet/bip32"
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/utils"
+	"github.com/fabbez/topiad/cmd/kaspawallet/libkaspawallet"
+	"github.com/fabbez/topiad/cmd/kaspawallet/libkaspawallet/bip32"
+	"github.com/fabbez/topiad/cmd/kaspawallet/utils"
 	"github.com/pkg/errors"
 
-	"github.com/kaspanet/kaspad/cmd/kaspawallet/keys"
+	"github.com/fabbez/topiad/cmd/kaspawallet/keys"
 )
 
 func create(conf *createConfig) error {
