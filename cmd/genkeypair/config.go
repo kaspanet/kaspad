941f8d98479c36f6c0e935c538ea317bd00ba2a2
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 11:10:22 +0100

    Update config.go

--- cmd/genkeypair/config.go
+++ cmd/genkeypair/config.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/jessevdk/go-flags"
-	"github.com/kaspanet/kaspad/infrastructure/config"
+	"github.com/fabbez/topiad/infrastructure/config"
 )
 
 type configFlags struct {
