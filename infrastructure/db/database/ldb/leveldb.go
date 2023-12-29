66dbb976ea2e648ea463c7dd7f77b917cb60aa17
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 13:26:11 +0100

    Update leveldb.go

--- infrastructure/db/database/ldb/leveldb.go
+++ infrastructure/db/database/ldb/leveldb.go
@@ -1,7 +1,7 @@
 package ldb
 
 import (
-	"github.com/kaspanet/kaspad/infrastructure/db/database"
+	"github.com/fabbez/topiad/infrastructure/db/database"
 	"github.com/pkg/errors"
 	"github.com/syndtr/goleveldb/leveldb"
 	ldbErrors "github.com/syndtr/goleveldb/leveldb/errors"
