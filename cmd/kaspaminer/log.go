130f798622ce39d889ac75a2b55cf6c50adf7b11
topianet <130279998+fabbez@users.noreply.github.com>
2023-12-29 11:22:11 +0100

    Update log.go

--- cmd/kaspaminer/log.go
+++ cmd/kaspaminer/log.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/kaspanet/kaspad/infrastructure/logger"
-	"github.com/kaspanet/kaspad/util/panics"
+	"github.com/fabbez/topiad/infrastructure/logger"
+	"github.com/fabbez/topiad/util/panics"
 	"os"
 )
 
